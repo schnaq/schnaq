c41cbb59b65492477d74f89d8b90fa15ee93f8aa
Mike Birkhoff <michael.birkhoff@hhu.de>
2020-07-15 10:09:41 +0200

    Add navbar and bootstrap
    Bootstrap.js is included in the html file, this should be fixed in the future ...

--- src/meetly/meeting/interface/views.cljs
+++ src/meetly/meeting/interface/views.cljs
@@ -26,19 +26,46 @@
    [:h2 "Startpage"]
    (navigation-button :routes/startpage "--> Startpage")])
 
+
+(defn header []
+  ;; collapsable navbar
+  [:nav.navbar.navbar-expand-lg.py-3.navbar-light.bg-light
+   ;; logo
+   [:div.container
+    [:a.navbar-brand {:href "#/startpage"}
+     [:img.d-inline-block.align-middle.mr-2 {:src "imgs/logo.png" :width "140" :alt ""}]]
+    ;; hamburger
+    [:button.navbar-toggler
+     {:type "button" :data-toggle "collapse" :data-target "#navbarSupportedContent"
+      :aria-controls "navbarSupportedContent" :aria-expanded "false" :aria-label "Toggle navigation"}
+     [:span.navbar-toggler-icon]]
+    ;; menu items
+    [:div
+     {:id "navbarSupportedContent"
+      :class "collapse navbar-collapse"}
+     [:ul.navbar-nav.ml-auto
+      ;[:li.nav-item.active [:a.nav-link {:href "#"} "Home" [:span.sr-only "(current)"]]]
+      [:a.nav-link {:href "#/startpage"} "Home" [:span.sr-only "(current)"]]
+      [:li.nav-item [:a.nav-link {:href "#/clock"} "Examples"]]
+      [:li.nav-item [:a.nav-link {:href "#/meetings/"} "Show Meetings"]]
+      [:li.nav-item [:a.nav-link {:href "#/meetings/create"} "Create Meeting"]]
+      [:li.nav-item [:a.nav-link {:href "#/meetings/agenda"} "Create Agenda"]]
+      [:li.nav-item [:a.nav-link {:href "#"} "Overview"]]]]]])
+
 (defn base-page
   []
   (let [current-route @(rf/subscribe [:current-route])
         errors @(rf/subscribe [:error-occurred])
         ajax-error (:ajax errors)]
-    [:div.container
-     [:link {:href "bootstrap/css/bootstrap.css" :rel "stylesheet"}]
-     [:script {:src "bootstrap/js/bootstrap.js"}]
+    [:div
 
-     (when ajax-error
-       [:h1 "Error: " ajax-error])
-     (when current-route
-       [(-> current-route :data :view)])]))
+     [header]
+     [:div.container
+
+      (when ajax-error
+        [:h1 "Error: " ajax-error])
+      (when current-route
+        [(-> current-route :data :view)])]]))
 
 (defn root []
   [:div#root
