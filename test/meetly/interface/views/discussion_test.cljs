ae8da9e6b5bfb9e5b190e61eaab1283a80d8cb09
Christian Meter <cmeter@googlemail.com>
2020-07-29 17:24:58 +0200

    Remove always-true-test

--- test/meetly/interface/views/discussion_test.cljs
+++ test/meetly/interface/views/discussion_test.cljs
@@ -1,6 +1,3 @@
 (ns meetly.interface.views.discussion-test
   (:require [cljs.test :refer [deftest is testing run-tests]]
             [meetly.meeting.interface.views.discussion :as d]))
-
-(deftest test-nothing
-  (is (= 1 1)))
