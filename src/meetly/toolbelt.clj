942f651f0117b126ba237d755f0e4018e39d5f87
Christian Meter <cmeter@googlemail.com>
2020-08-03 11:37:10 +0200

    Add fn to create subfolder for dev-local

--- src/meetly/toolbelt.clj
+++ src/meetly/toolbelt.clj
@@ -1,10 +1,16 @@
 (ns meetly.toolbelt
   "Utility functions supporting the backend."
+  (:require [ghostwheel.core :refer [>defn]])
   (:import (java.io File)))
 
-(defn create-storage-directory!
-  "Locally creates a file to store datomic data."
-  []
-  (let [dir (File. ".datomic/dev-local/data")]
-    (.mkdir dir)
-    (.getAbsolutePath dir)))
+(>defn create-storage-directory!
+  "Locally creates a file to store datomic data. Optionally takes a sub-path for
+  folder-creation."
+  ([]
+   [:ret string?]
+   (create-storage-directory! "data"))
+  ([sub-path]
+   [string? :ret string?]
+   (let [dir (File. (format ".datomic/dev-local/%s" sub-path))]
+     (.mkdir dir)
+     (.getAbsolutePath dir))))
