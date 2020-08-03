4fae72822ed50b178b1ab27e61521d197f3f7bff
Alexander Schneider <alexander@schneider.gg>
2020-08-03 14:29:07 +0200

    Fix test of down and upvotes

--- test/meetly/meeting/database_test.clj
+++ test/meetly/meeting/database_test.clj
@@ -8,15 +8,10 @@
 
 (use-fixtures :each meetly-toolbelt/init-db-test-fixture)
 
-
-#_(deftest agenda-by-meeting-hash-and-discussion-id-test
-    (testing "If discussion/agenda-id belongs to a meeting, it should return the agenda."))
-;; (db/agenda-by-meeting-hash-and-discussion-id "2d2d3a83-2296-4b81-b544-1e7c4a607cdd" 17592186045433)))
-
 (deftest up-and-downvotes-test
   (testing "Tests whether setting up and downvotes works properly."
-    (let [cat-or-dog (:db/id (ddb/all-discussions-by-title "Cat or Dog?"))
-          some-statements (map #(get-in % [:argument/premises first :db/id])
+    (let [cat-or-dog (:db/id (first (ddb/all-discussions-by-title "Cat or Dog?")))
+          some-statements (map #(-> % :argument/premises first :db/id)
                                (ddb/all-arguments-for-discussion cat-or-dog))
           author-1 "Test-1"
           author-2 "Test-2"]
