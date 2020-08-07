e9b1ecf4cf3a2674e1275ec603b62290cd7ceda5
Christian Meter <cmeter@googlemail.com>
2020-08-07 12:51:08 +0200

    Recursively add directories

--- src/meetly/toolbelt.clj
+++ src/meetly/toolbelt.clj
@@ -6,10 +6,10 @@
 (>defn create-directory!
   "Creates a directory in the project's path. Returns the absolut path of the
   directory."
-  ([^String path]
-   [string? :ret string?]
-   (when-not (or (.startsWith path "/")
-                 (.startsWith path ".."))
-     (let [dir (File. path)]
-       (.mkdir dir)
-       (.getAbsolutePath dir)))))
+  [^String path]
+  [string? :ret string?]
+  (when-not (or (.startsWith path "/")
+                (.startsWith path ".."))
+    (let [dir (File. path)]
+      (.mkdirs dir)
+      (.getAbsolutePath dir))))
