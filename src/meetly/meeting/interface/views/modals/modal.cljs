abd06a347f374b2dbf505359bcac95b9e48ee0bd
Christian Meter <cmeter@googlemail.com>
2020-08-08 15:58:40 +0200

    Generify modals

--- src/meetly/meeting/interface/views/modals/modal.cljs
+++ src/meetly/meeting/interface/views/modals/modal.cljs
@@ -2,9 +2,9 @@
   (:require [re-frame.core :as rf]
             [meetly.meeting.interface.views.modals.events]
             [meetly.meeting.interface.views.modals.subs]
-            [oops.core :refer [oget]]
-            [meetly.meeting.interface.text.display-data :refer [labels]]))
-
+            [meetly.meeting.interface.text.display-data :refer [labels]]
+            [ghostwheel.core :refer [>defn]]
+            [oops.core :refer [oget]]))
 
 (defn modal-panel
   [{:keys [child show?]}]
@@ -25,9 +25,25 @@
        (when (:show? @modal)
          [modal-panel @modal])])))
 
-
 (defn- close-modal []
   (rf/dispatch [:modal {:show? false :child nil}]))
+
+(>defn modal-template
+  "Generic modal template."
+  [header body]
+  [string? vector? :ret vector?]
+  [:div.modal-content
+   [:div.modal-header
+    [:h5.modal-title header]
+    [:button.close {:type "button" :data-dismiss "modal" :aria-label "Close"}
+     [:span {:aria-hidden "true"
+             :on-click close-modal}
+      "x"]]]
+   [:div.modal-body body]])
+
+
+;; -----------------------------------------------------------------------------
+;; Enter Name Modal
 
 (defn- modal-name-input
   "An input, where the user can set their name. Happens automatically by typing."
@@ -50,13 +66,8 @@
                              :value "Set Name"}]]])
 
 (defn enter-name-modal []
-  [:div.modal-content
-   [:div.modal-header
-    [:h5.modal-title (labels :modals/enter-name-header)]
-    [:button.close {:type "button" :data-dismiss "modal" :aria-label "Close"}
-     [:span {:aria-hidden "true"
-             :on-click close-modal}
-      "x"]]]
-   [:div.modal-body
-    [:p (labels :modals/enter-name-primer)]
-    [modal-name-input "Ihr Name"]]])
+  (modal-template
+    (labels :modals/enter-name-header)
+    [:div
+     [:p (labels :modals/enter-name-primer)]
+     [modal-name-input "Ihr Name"]]))
