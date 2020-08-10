7566d3ad77160c5714afc3a6881bfddb21c60d84
Mike Birkhoff <michael.birkhoff@hhu.de>
2020-08-10 10:27:46 +0200

    Move avatar name next to icon

--- src/meetly/meeting/interface/views/common.cljs
+++ src/meetly/meeting/interface/views/common.cljs
@@ -6,7 +6,8 @@
   "Create an image based on the nickname."
   [display-name size]
   [string? number? :ret vector?]
-  [:div.avatar.text-center
+  ;[:div.avatar.text-center
+  [::div.d-flex.flex-row
+   [:div.avatar-name.mr-4 display-name]
    [:div.avatar-image.img-thumbnail.rounded-circle
-    {:dangerouslySetInnerHTML {:__html (jdenticon/toSvg display-name size)}}]
-   [:div.avatar-name display-name]])
+    {:dangerouslySetInnerHTML {:__html (jdenticon/toSvg display-name size)}}]])
