b87f22bb41b454fcf516ff66c672e6729683f18d
Christian Meter <cmeter@googlemail.com>
2020-08-15 17:47:00 +0200

    Add build-hash of current build to project

--- src/meetly/interface/config.cljs
+++ src/meetly/interface/config.cljs
@@ -3,6 +3,7 @@
 ;; Third parameter is a default value
 (goog-define rest-api-url "http://localhost:3000")
 (goog-define environment "development")
+(goog-define build-hash "dev")
 
 (def config
   {:rest-backend rest-api-url
