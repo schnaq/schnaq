9cb278e699e01ce5a0ca76389c5a818e16752375
Christian Meter <cmeter@googlemail.com>
2020-08-24 17:40:33 +0200

    Remove error-alert from base page

--- src/meetly/interface/views.cljs
+++ src/meetly/interface/views.cljs
@@ -1,7 +1,6 @@
 (ns meetly.interface.views
   (:require [meetly.interface.views.feedback :as feedback]
             [meetly.interface.views.base :as base]
-            [meetly.interface.views.errors :as errors]
             [meetly.interface.views.modals.modal :as modal]
             [meetly.interface.views.notifications :as notifications]
             [re-frame.core :as rf]
@@ -9,13 +8,8 @@
 
 (defn- base-page
   []
-  (let [current-route @(rf/subscribe [:navigation/current-route])
-        errors @(rf/subscribe [:error-occurred])
-        ajax-error (:ajax errors)]
+  (let [current-route @(rf/subscribe [:navigation/current-route])]
     [:div#display-content
-     ;[header]
-     [:div#error-display.container
-      [errors/upper-error-box ajax-error]]
      (when current-route
        [:div
         [modal/modal]
