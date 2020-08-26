589dca089afa6cf465a3c8bfb05243c0db3925b2
Alexander Schneider <alexander@schneider.gg>
2020-08-26 16:36:31 +0200

    Remove outer padding on small devices

--- src/main/meetly/interface/views/discussion/discussion.cljs
+++ src/main/meetly/interface/views/discussion/discussion.cljs
@@ -81,7 +81,7 @@
   [:div
    [base/nav-header]
    [view/discussion-header-no-subtitle meeting]
-   [:div.container.py-4
+   [:div.container.py-4.sm-pad-0
     [:div.discussion-view-rounded.shadow-custom
      content]]])
 
