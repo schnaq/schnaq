5bcae6c0656681c6209bc03d54f6cfb60869ec1c
Alexander Schneider <alexander@schneider.gg>
2020-09-07 15:36:01 +0200

    Wrap activation and removal of js-events

--- src/main/schnaq/interface/utils/js_wrapper.cljs
+++ src/main/schnaq/interface/utils/js_wrapper.cljs
@@ -1,8 +1,29 @@
-(ns schnaq.interface.utils.js-wrapper)
+(ns schnaq.interface.utils.js-wrapper
+  (:require ["jquery" :as jquery]
+            [ghostwheel.core :refer [>defn]]))
 
 
-(defn prevent-default [event]
+(>defn prevent-default
+  "Wraps the js `<Event>.preventDefault` method to prevent cursive warnings."
+  [event]
+  [any? :ret nil?]
   (.preventDefault event))
 
-(defn stop-propagation [event]
+(>defn stop-propagation
+  "Wraps the js `<Event>.stopPrapagation` method to prevent cursive warnings."
+  [event]
+  [any? :ret nil?]
   (.stopPropagation event))
+
+(>defn add-listener
+  "Adds an event-listener to one or more elements matching the `selector`. When the
+  event fires, the `listener-fn` ist executed."
+  [selector event-name listener-fn]
+  [string? string? fn? :ret nil?]
+  (.on (jquery selector) event-name listener-fn))
+
+(>defn remove-listener
+  "Remove an event-listener from one or more elements matching `selector`."
+  [selector event-name]
+  [string? string? :ret nil?]
+  (.off (jquery selector) event-name))
