33353714af15aec0acf3980a855c00370dacf26b
Alexander Schneider <alexander@schneider.gg>
2020-09-10 11:02:43 +0200

    Remove test for non-existing function

--- src/test/schnaq/api_test.clj
+++ src/test/schnaq/api_test.clj
@@ -22,27 +22,6 @@
       (is (= 201 (:status response)))
       (is (s/valid? ::specs/meeting (-> response :body :new-meeting))))))
 
-(deftest add-agendas-test
-  (testing "Test whether the agenda is correctly validated before passed on."
-    (let [add-agendas @#'api/add-agendas
-          add-meeting (db/add-meeting {:meeting/title "foo"
-                                       :meeting/share-hash "abbada"
-                                       :meeting/start-date (db/now)
-                                       :meeting/end-date (db/now)
-                                       :meeting/author (db/add-user-if-not-exists "Wegi")})
-          valid-req {:body-params {:meeting-id add-meeting
-                                   :meeting-hash "abbada"
-                                   :agendas []}}
-          valid-response (add-agendas valid-req)
-          invalid-req {:body-params {:meeting-id nil
-                                     :meeting-hash :not-valid
-                                     :agendas []}}
-          invalid-response (add-agendas invalid-req)]
-      (is (= 200 (:status valid-response)))
-      (is (= "Agendas sent over successfully" (-> valid-response :body :text)))
-      (is (= 400 (:status invalid-response)))
-      (is (= "Agenda could not be added" (-> invalid-response :body :error))))))
-
 (deftest update-meeting-test
   (testing "Test whether a meeting updates correctly"
     (let [new-title "foo Neu"
