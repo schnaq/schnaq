ba788ad2b8a8afb661d9edf374b7dec0c306ae4a
Christian Meter <cmeter@googlemail.com>
2020-09-11 16:00:51 +0200

    Make modal optionally larger
    Co-authored-by: Alexander Schneider <alexander@schneider.gg>

--- src/main/schnaq/interface/views/modals/modal.cljs
+++ src/main/schnaq/interface/views/modals/modal.cljs
@@ -8,7 +8,7 @@
             [schnaq.interface.utils.js-wrapper :as js-wrap]))
 
 (defn modal-panel
-  [{:keys [child show?]}]
+  [{:keys [child show? large?]}]
   [:div {:class "modal-wrapper"}
    [:div {:class "modal-backdrop"
           :on-click (fn [event]
@@ -16,8 +16,9 @@
                                             :child nil}])
                       (js-wrap/prevent-default event)
                       (js-wrap/stop-propagation event))}]
-   [:div {:class "modal-child modal-dialog modal-dialog-scrollable"}
-    child]])
+   (let [classes "modal-child modal-dialog modal-dialog-scrollable"]
+     [:div {:class (if large? (str classes " modal-lg") classes)}
+      child])])
 
 (defn modal []
   (let [modal (rf/subscribe [:modal])]
