195ce102dbf3dfd7ba4e43fb0e06ac04d3bd05f3
Alexander Schneider <alexander@schneider.gg>
2020-09-12 14:18:58 +0200

    If the original was nil and the new description value is blank, do not generate a diff.

--- src/main/schnaq/suggestions.clj
+++ src/main/schnaq/suggestions.clj
@@ -1,7 +1,8 @@
 (ns schnaq.suggestions
   (:require [clojure.spec.alpha :as s]
             [ghostwheel.core :refer [>defn >defn- ?]]
-            [schnaq.meeting.database :as db]))
+            [schnaq.meeting.database :as db]
+            [clojure.string :as string]))
 
 (>defn new-meeting-suggestion
   "Adds a new meeting suggestions, if it has any changes."
@@ -9,8 +10,12 @@
   [map? :db/id :ret (? :db/id)]
   (let [original-meeting (db/meeting-by-hash (:meeting/share-hash suggestion))
         updated-meeting (select-keys suggestion [:db/id :meeting/title :meeting/description])]
-    (when (or (not= (:meeting/title original-meeting) (:meeting/title suggestion))
-              (not= (:meeting/description original-meeting) (:meeting/description suggestion)))
+    (when
+      (or (not= (:meeting/title original-meeting) (:meeting/title suggestion))
+          ;; If they are truly different the original can not be nil and the new one blank at the same time
+          (and (not= (:meeting/description original-meeting) (:meeting/description suggestion))
+               (not (and (nil? (:meeting/description original-meeting))
+                         (not (string/blank? (:meeting/description updated-meeting)))))))
       (db/suggest-meeting-updates! updated-meeting user-id))))
 
 (>defn- does-agenda-suggestion-change-anything?
@@ -20,7 +25,9 @@
   [map? :ret boolean?]
   (let [original-agenda (db/agenda (:db/id suggestion))]
     (or (not= (:agenda/title original-agenda) (:agenda/title suggestion))
-        (not= (:agenda/description original-agenda) (:agenda/description suggestion)))))
+        (and (not= (:agenda/description original-agenda) (:agenda/description suggestion))
+             (not (and (nil? (:agenda/description original-agenda))
+                       (not (string/blank? (:agenda/description suggestion)))))))))
 
 (>defn new-agenda-updates-suggestion
   "Adds agenda-suggestions, if they represent changes."
