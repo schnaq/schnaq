7d812a02cf4e05399f696f9ab594f2f7e453467e
Mike Birkhoff <michael.birkhoff@hhu.de>
2020-09-22 11:17:31 +0200

    Fix that old meeting description was displayed when editing meeting
    Co-authored-by: Christian Meter <cmeter@googlemail.com>

--- src/main/schnaq/interface/views/text_editor/view.cljs
+++ src/main/schnaq/interface/views/text_editor/view.cljs
@@ -10,19 +10,26 @@
   ([text on-change-function]
    (view text on-change-function "300px"))
   ([text on-change-function min-height]
-   (reagent/create-class
-     {:display-name "mde-component"
-      :reagent-render
-      (fn [] [:textarea])
-      :component-did-mount
-      (fn [comp]
-        (let [newMDE (mde.
-                       (clj->js {:element (rdom/dom-node comp)
-                                 :minHeight min-height
-                                 :initialValue (data/labels :meeting-form-desc-placeholder)}))]
-          (when text (.value newMDE text))
-          (.on (.-codemirror newMDE) "change"
-               #(on-change-function (.value newMDE)))))})))
+   (let [mde-ref (reagent/atom nil)]
+     (reagent/create-class
+       {:display-name "mde-component"
+        :reagent-render (fn [] [:textarea])
+        :component-did-update
+        (fn [comp _argv]
+          (let [[_ new-text _] (reagent/argv comp)]
+            (when new-text
+              (.value @mde-ref new-text))))
+        :component-did-mount
+        (fn [comp]
+          (let [newMDE (mde.
+                         (clj->js {:element (rdom/dom-node comp)
+                                   :minHeight min-height
+                                   :initialValue (data/labels :meeting-form-desc-placeholder)}))]
+            (reset! mde-ref newMDE)
+            (when text
+              (.value @mde-ref text))
+            (.on (.-codemirror @mde-ref) "change"
+                 #(on-change-function (.value @mde-ref)))))}))))
 
 (defn view-store-on-change
   "Mark Up Editor View which automatically stores its content in the local db.
