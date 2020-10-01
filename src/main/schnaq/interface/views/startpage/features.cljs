b26c19a9e1ca937b68f884b09263e927baa10d2e
Alexander Schneider <alexander@schneider.gg>
2020-10-01 11:08:15 +0200

    Move the request demo button higher up

--- src/main/schnaq/interface/views/startpage/features.cljs
+++ src/main/schnaq/interface/views/startpage/features.cljs
@@ -1,12 +1,6 @@
 (ns schnaq.interface.views.startpage.features
-  (:require [ajax.core :as ajax]
-            [oops.core :refer [oget]]
-            [re-frame.core :as rf]
-            [schnaq.interface.config :refer [config]]
-            [schnaq.interface.text.display-data :refer [labels img-path]]
-            [schnaq.interface.utils.js-wrapper :as js-wrap]
-            [schnaq.interface.views.common :as common]
-            [schnaq.interface.views.modals.modal :as modal]))
+  (:require [schnaq.interface.text.display-data :refer [labels img-path]]
+            [schnaq.interface.views.common :as common]))
 
 (defn- build-feature-text-box
   "Composing the text-part of a feature-row. Takes a `text-namespace` which
@@ -36,98 +30,6 @@
    [:div.col-12.col-lg-5.offset-lg-1
     [:img.img-fluid {:src (img-path image-key)}]]])
 
-(defn- request-demo-modal
-  "A modal which the user can use to request a demo"
-  []
-  [modal/modal-template
-   "Persönliche Demo anfordern"
-   [:form.form
-    {:on-submit
-     (fn [e]
-       (js-wrap/prevent-default e)
-       (rf/dispatch [:startpage.demo.request/send
-                     (oget e [:target :elements :requester-name :value])
-                     (oget e [:target :elements :requester-contact :value])
-                     (oget e [:target :elements :requester-company :value])
-                     (oget e [:target :elements :requester-phone :value])]))}
-    [:div.form-group
-     [:label {:for "demo-requester-name"}
-      (labels :startpage.demo.request.modal.name/label)]
-     [:input {:id "demo-requester-name"
-              :class-name "form-control"
-              :placeholder (labels :startpage.demo.request.modal.name/placeholder)
-              :required true
-              :autoFocus true :name "requester-name"}]]
-    [:div.form-group.pb-2
-     [:label {:for "demo-requester-contact"}
-      (labels :startpage.demo.request.modal.email/label)]
-     [:input {:id "demo-requester-contact" :name "requester-contact"
-              :class-name "form-control" :type "email"
-              :required true
-              :placeholder (labels :startpage.demo.request.modal.email/placeholder)}]]
-    [:div.form-group
-     [:label {:for "demo-requester-company"}
-      (labels :startpage.demo.request.modal.company/label)]
-     [:input {:id "demo-requester-company" :name "requester-company"
-              :class-name "form-control"
-              :placeholder (labels :startpage.demo.request.modal.company/placeholder)}]
-     [:small.form-text.text-muted
-      (labels :feedbacks.modal/optional)]]
-    [:div.form-group
-     [:label {:for "demo-requester-phone"}
-      (labels :startpage.demo.request.modal.phone/label)]
-     [:input {:id "demo-requester-phone" :name "requester-phone"
-              :class-name "form-control" :type "tel"
-              :placeholder (labels :startpage.demo.request.modal.phone/placeholder)}]
-     [:small.form-text.text-muted
-      (labels :feedbacks.modal/optional)]]
-    [:div.modal-footer
-     [:input.btn.btn-primary.mr-auto {:type "submit"}]
-     [:small.text-muted (labels :feedbacks.modal/disclaimer)]]]])
-
-(rf/reg-event-fx
-  :startpage.demo.request/send
-  (fn [_ [_ name email company phone]]
-    {:fx [[:http-xhrio {:method :post
-                        :uri (str (:rest-backend config) "/emails/request-demo")
-                        :params {:name name
-                                 :email email
-                                 :company company
-                                 :phone phone}
-                        :format (ajax/transit-request-format)
-                        :response-format (ajax/transit-response-format)
-                        :on-success [:startpage.demo.request/send-success]
-                        :on-failure [:startpage.demo.request/send-failure]}]]}))
-
-(rf/reg-event-fx
-  :startpage.demo.request/send-success
-  (fn [_ _]
-    {:fx [[:dispatch [:modal {:show? false :child nil}]]
-          [:dispatch [:notification/add
-                      #:notification{:title (labels :startpage.demo.request.send.notification/title)
-                                     :body (labels :startpage.demo.request.send.notification/body)
-                                     :context :success}]]]}))
-
-(rf/reg-event-fx
-  :startpage.demo.request/send-failure
-  (fn [_ _]
-    {:fx [[:dispatch [:notification/add
-                      #:notification{:title (labels :startpage.demo.request.send.notification/failed-title)
-                                     :body (labels :startpage.demo.request.send.notification/failed-body)
-                                     :context :alert}]]]}))
-
-(defn- request-demo-section
-  "A button and some text to request a personal demo"
-  []
-  [:div.row.align-items-center.feature-row
-   [:div.col-12.col-lg-5.text-center
-    [:button.btn.button-secondary.font-150.mb-5
-     {:on-click #(rf/dispatch [:modal {:show? true :large? false
-                                       :child [request-demo-modal]}])}
-     (labels :startpage.demo.request/button)]]
-   [:div.col-12.col-lg-6.offset-lg-1
-    [build-feature-text-box :startpage.demo.request]]])
-
 (defn- meeting-organisation
   "Featuring meeting-organisation with an image."
   []
@@ -156,7 +58,6 @@
   "Collection of feature rows."
   []
   [:section.pt-5
-   [request-demo-section]
    [meeting-organisation]
    [structured-discussions]
    [graph-visualization]])
