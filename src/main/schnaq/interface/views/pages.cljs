5d90cdaad667cf83b6c316547f8cab2b9f327b00
Christian Meter <cmeter@googlemail.com>
2020-10-12 16:35:05 +0200

    Use own namespace for specs

--- src/main/schnaq/interface/views/pages.cljs
+++ src/main/schnaq/interface/views/pages.cljs
@@ -1,19 +1,19 @@
 (ns schnaq.interface.views.pages
   "Defining page-layouts."
-  (:require [ghostwheel.core :refer [>defn]]
-            [schnaq.interface.views.base :as base]
-            [cljs.spec.alpha :as s]))
+  (:require [cljs.spec.alpha :as s]
+            [ghostwheel.core :refer [>defn]]
+            [schnaq.interface.views.base :as base]))
 
 (s/def :page/heading string?)
 (s/def :page/subheading (s/? string?))
-(s/def ::page
+(s/def ::page-headings
   (s/keys :req [:page/heading]
           :opt [:page/subheading]))
 
 (>defn with-nav-and-header
   "Default page with header and curly wave."
   [{:page/keys [heading subheading]} & body]
-  [::navigation (s/+ vector?) :ret vector?]
+  [::page-headings (s/+ vector?) :ret vector?]
   [:<>
    [base/nav-header]
    [base/header heading subheading]
