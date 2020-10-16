59f9ae8f4e302229854ebc677c3618627b17f174
Christian Meter <meter@hhu.de>
2020-10-16 13:31:28 +0200

    Apply 1 suggestion(s) to 1 file(s)

--- src/main/schnaq/interface/text/display_data.cljs
+++ src/main/schnaq/interface/text/display_data.cljs
@@ -32,7 +32,7 @@
      :startpage.value-cards.meetings/title "Meetings optimieren"
      :startpage.value-cards.meetings/description "Vor allem seitdem die Arbeitswelt verteilter geworden ist, hetzt man von einem (digitalen) Meeting zum anderen. Damit die Vor- und Nachbereitung nicht mehr zur Nebensache wird, gibt es schnaq. Sparen Sie Arbeitszeit und optimieren sie die Ergebnisse Ihrer Meetings."
      :startpage.value-cards.meetings/alt-text "Menschen in einem Meeting, eine Frau redet gerade"
-     :startpage.value-cards.knowledge/title "Brainstorming-Sessions und Wissensgenerierung"
+     :startpage.value-cards.knowledge/title "Brainstorming und Wissensgenerierung"
      :startpage.value-cards.knowledge/description "Nutzen Sie schnaq, um Ihr Team nachdenken und diskutieren zu lassen. Automatisch wird eine MindMap der Beiträge erstellt, die dann analysiert werden kann und das Ergebnis der Session darstellt. Perfekt für Brainstorming-Sitzungen!"
      :startpage.value-cards.knowledge/alt-text "Etliche Klebezettel auf einer Wand"
      :startpage.value-cards.button/text "Mehr erfahren"
