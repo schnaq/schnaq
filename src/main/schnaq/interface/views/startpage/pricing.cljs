5f38ea2a5cd9035a8f61eeb5315a7d183db9017e
Alexander Schneider <alexander@schneider.gg>
2020-10-28 15:30:50 +0100

    Add pricing cards

--- src/main/schnaq/interface/views/startpage/pricing.cljs
+++ src/main/schnaq/interface/views/startpage/pricing.cljs
@@ -1,32 +1,45 @@
 (ns schnaq.interface.views.startpage.pricing
   (:require [schnaq.interface.views.pages :as pages]))
 
+(defn- free-tier-card
+  []
+  [:div.card.shadow-sm
+   [:div.card-body.d-flex.flex-column
+    [:h3.card-title.text-center "Gratis"]
+    [:p.card-text "Für kleine Teams und private Zwecke. Der gratis Plan ist der
+     perfekte Einstieg in strukturierte Wissensgenerierung."]
+    [:p.card-text.text-center.display-2 "0 €"]
+    [:p.text-muted.text-center "Nach der Beta-Phase ist der Plan weiterhin verfügbar für bis zu 5 Nutzer:innen pro Team"]
+    [:div.text-center.mt-auto
+     [:button.btn.button-primary
+      [:p.card-text "Gratis loslegen"]]]]])
+
+(defn- business-tier-card
+  []
+  [:div.card.shadow-sm
+   [:div.card-body.d-flex.flex-column
+    [:h3.card-title.text-center "Business"]
+    [:p.card-text "Ob 10 oder 50 Nutzer:innen - der Preis ist der gleiche.
+      Eignet sich für Unternehmen, Vereine, Bildungsinstitutionen und alle
+      die strukturiert Wissen sammeln möchten."]
+    [:p.card-text.text-center [:span.display-2 "99 €"] [:span "/ Monat"]]
+    [:p.text-muted.text-center "zzgl. MwSt."]
+    [:p.text-muted.text-center "Bei jährlicher Zahlweise im Voraus 15% Rabatt"]
+    [:div.text-center.mt-auto
+     [:button.btn.button-primary {:disabled true}
+      [:p.card-text "Verfügbar ab 01.01.2021"]]]]])
+
+(defn- trial-box
+  []
+  [:div.trial-box
+   "Trial abschließen"])
+
 (defn- pricing-box
   "A box displaying the different subscription tiers we offer."
   []
   [:div.card-deck
-   [:div.card.shadow-sm
-    [:div.card-body.d-flex.flex-column
-     [:h3.card-title.text-center "Gratis"]
-     [:p.card-text "Für kleine Teams und private Zwecke. Der gratis Plan ist der
-     perfekte Einstieg in strukturierte Wissensgenerierung."]
-     [:p.card-text.text-center.display-2 "0 €"]
-     [:p.text-muted.text-center "Nach der Beta-Phase ist der Plan weiterhin verfügbar für bis zu 5 Nutzer:innen pro Team"]
-     [:div.text-center.mt-auto
-      [:button.btn.button-primary
-       [:p.card-text "Gratis loslegen"]]]]]
-   [:div.card.shadow-sm
-    [:div.card-body.d-flex.flex-column
-     [:h3.card-title.text-center "Business"]
-     [:p.card-text "Ob 10 oder 50 Nutzer:innen - der Preis ist der gleiche.
-      Eignet sich für Unternehmen, Vereine, Bildungsinstitutionen und alle
-      die strukturiert Wissen sammeln möchten."]
-     [:p.card-text.text-center [:span.display-2 "99 €"] [:span "/ Monat"]]
-     [:p.text-muted.text-center "zzgl. MwSt."]
-     [:p.text-muted.text-center "Bei jährlicher Zahlweise im Voraus 15% Rabatt"]
-     [:div.text-center.mt-auto
-      [:button.btn.button-primary {:disabled true}
-       [:p.card-text "Verfügbar ab 01.01.2021"]]]]]])
+   [free-tier-card]
+   [business-tier-card]])
 
 (defn- pricing-page
   "A full page depicting our pricing and related items."
@@ -35,7 +48,8 @@
    {:page/heading "Einmal schnaqqen, alles wissen!"
     :page/subheading "Schnaq Abonnement"}
    [:section.container
-    [pricing-box]]])
+    [pricing-box]
+    [trial-box]]])
 
 (defn pricing-view
   "The pricing view."
