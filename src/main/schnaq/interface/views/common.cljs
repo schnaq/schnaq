0132c07761edb4f3b5820725712e714d07db2b7b
Christian Meter <cmeter@googlemail.com>
2020-10-28 14:03:39 +0100

    Refactor feature rows into a separate namespace

--- src/main/schnaq/interface/views/common.cljs
+++ src/main/schnaq/interface/views/common.cljs
@@ -4,7 +4,8 @@
             [ghostwheel.core :refer [>defn]]
             [goog.string :as gstring]
             [oops.core :refer [oget]]
-            [reitit.frontend.easy :as reitfe]))
+            [reitit.frontend.easy :as reitfe]
+            [schnaq.interface.text.display-data :refer [labels fa img-path video]]))
 
 (>defn avatar
   "Create an image based on the nickname."
@@ -83,3 +84,46 @@
                                                         :edit-hash edit-hash})
         location (oget js/window :location)]
     (gstring/format "%s//%s%s" (oget location :protocol) (oget location :host) path)))
+
+
+;; -----------------------------------------------------------------------------
+;; Feature rows
+
+(defn- build-feature-text-box
+  "Composing the text-part of a feature-row. Takes a `text-namespace` which
+  looks up the corresponding text entries, which are then rendered."
+  [text-namespace]
+  (let [prepend-namespace (partial add-namespace-to-keyword text-namespace)]
+    [:article.feature-text-box.pb-5
+     [:p.lead.mb-1 (labels (prepend-namespace :lead))]
+     [:h5 (labels (prepend-namespace :title))]
+     [:p (labels (prepend-namespace :body))]]))
+
+(defn feature-row-image-left
+  "Build a feature row, where the image is located on the left side."
+  [image-key text-namespace]
+  [:div.row.align-items-center.feature-row
+   [:div.col-12.col-lg-5
+    [:img.img-fluid {:src (img-path image-key)}]]
+   [:div.col-12.col-lg-6.offset-lg-1
+    [build-feature-text-box text-namespace]]])
+
+(defn feature-row-image-right
+  "Build a feature row, where the image is located on the right side."
+  [image-key text-namespace]
+  [:div.row.align-items-center.feature-row
+   [:div.col-12.col-lg-6
+    [build-feature-text-box text-namespace]]
+   [:div.col-12.col-lg-5.offset-lg-1
+    [:img.img-fluid {:src (img-path image-key)}]]])
+
+(defn feature-row-video-right
+  "Feature row where the video is located on the right side."
+  [video-key-webm vide-key-webm text-namespace]
+  [:div.row.align-items-center.feature-row
+   [:div.col-12.col-lg-6
+    [build-feature-text-box text-namespace]]
+   [:div.col-12.col-lg-6
+    [:video.w-100.feature-animations {:auto-play true :loop true :muted true :plays-inline true}
+     [:source {:src (video video-key-webm) :type "video/webm"}]
+     [:source {:src (video vide-key-webm) :type "video/mp4"}]]]])
--- src/main/schnaq/interface/views/startpage/features.cljs
+++ src/main/schnaq/interface/views/startpage/features.cljs
@@ -3,49 +3,10 @@
             [schnaq.interface.views.common :as common]
             [schnaq.interface.views.pages :as pages]))
 
-(defn- build-feature-text-box
-  "Composing the text-part of a feature-row. Takes a `text-namespace` which
-  looks up the corresponding text entries, which are then rendered."
-  [text-namespace]
-  (let [prepend-namespace (partial common/add-namespace-to-keyword text-namespace)]
-    [:article.feature-text-box.pb-5
-     [:p.lead.mb-1 (labels (prepend-namespace :lead))]
-     [:h5 (labels (prepend-namespace :title))]
-     [:p (labels (prepend-namespace :body))]]))
-
-(defn- feature-row-image-left
-  "Build a feature row, where the image is located on the left side."
-  [image-key text-namespace]
-  [:div.row.align-items-center.feature-row
-   [:div.col-12.col-lg-5
-    [:img.img-fluid {:src (img-path image-key)}]]
-   [:div.col-12.col-lg-6.offset-lg-1
-    [build-feature-text-box text-namespace]]])
-
-(defn- feature-row-image-right
-  "Build a feature row, where the image is located on the right side."
-  [image-key text-namespace]
-  [:div.row.align-items-center.feature-row
-   [:div.col-12.col-lg-6
-    [build-feature-text-box text-namespace]]
-   [:div.col-12.col-lg-5.offset-lg-1
-    [:img.img-fluid {:src (img-path image-key)}]]])
-
-(defn- feature-row-video-right
-  "Feature row where the video is located on the right side."
-  [video-key-webm vide-key-webm text-namespace]
-  [:div.row.align-items-center.feature-row
-   [:div.col-12.col-lg-6
-    [build-feature-text-box text-namespace]]
-   [:div.col-12.col-lg-6
-    [:video.w-100.feature-animations {:auto-play true :loop true :muted true :plays-inline true}
-     [:source {:src (video video-key-webm) :type "video/webm"}]
-     [:source {:src (video vide-key-webm) :type "video/mp4"}]]]])
-
 (defn- meeting-organisation
   "Featuring meeting-organisation with an image."
   []
-  [feature-row-video-right
+  [common/feature-row-video-right
    :start-page.work-together/webm
    :start-page.work-together/mp4
    :startpage.features.meeting-organisation])
@@ -53,14 +14,14 @@
 (defn- structured-discussions
   "Overview of structured discussions."
   []
-  [feature-row-image-left
+  [common/feature-row-image-left
    :startpage.features/sample-discussion
    :startpage.features.discussion])
 
 (defn- graph-visualization
   "Feature box showcasing the graph."
   []
-  [feature-row-image-right
+  [common/feature-row-image-right
    :startpage.features/discussion-graph
    :startpage.features.graph])
 
@@ -70,17 +31,17 @@
   (pages/with-nav-and-header
     {:page/heading (labels :feature.meetings/lead)}
     [:div.container.py-4
-     [feature-row-image-right
+     [common/feature-row-image-right
       :feature.meetings/hero-image
       :feature.meetings]
      [:h3.text-center.pb-4 (labels :feature.meetings/features-subheading)]
-     [feature-row-image-left
+     [common/feature-row-image-left
       :feature.meetings/schedule-meetings
       :feature.meetings.schedule]
-     [feature-row-image-right
+     [common/feature-row-image-right
       :startpage.features/sample-discussion
       :feature.meetings.discuss]
-     [feature-row-image-left
+     [common/feature-row-image-left
       :startpage.features/admin-center
       :feature.meetings.admin-center]
      [:section.feature-text-box
@@ -102,17 +63,17 @@
   (pages/with-nav-and-header
     {:page/heading (labels :feature.discussions/lead)}
     [:div.container.py-4
-     [feature-row-image-right
+     [common/feature-row-image-right
       :feature.discussions/hero-image
       :feature.discussions]
      [:h3.text-center.pb-4 (labels :feature.discussions/features-subheading)]
-     [feature-row-image-left
+     [common/feature-row-image-left
       :feature.discussions/create-discussion-spaces
       :feature.discussions.spaces]
-     [feature-row-image-right
+     [common/feature-row-image-right
       :startpage.features/sample-discussion
       :feature.discussions.discuss]
-     [feature-row-image-left
+     [common/feature-row-image-left
       :startpage.features/discussion-graph
       :feature.discussions.graph]
      [:section.feature-text-box
@@ -134,17 +95,17 @@
     {:page/heading (labels :feature.knowledge/lead)
      :page/subheading (labels :feature.knowledge/subheading)}
     [:div.container.py-4
-     [feature-row-image-right
+     [common/feature-row-image-right
       :feature.knowledge/hero-image
       :feature.knowledge.general]
      [:h3.text-center.pb-4 (labels :feature.knowledge/features-subheading)]
-     [feature-row-image-left
+     [common/feature-row-image-left
       :startpage.features/sample-discussion
       :feature.knowledge.discussions]
-     [feature-row-image-right
+     [common/feature-row-image-right
       :feature.knowledge/overview
       :feature.knowledge.database]
-     [feature-row-image-left
+     [common/feature-row-image-left
       :startpage.features/discussion-graph
       :feature.knowledge.change-of-facts]
      [:section.feature-text-box
