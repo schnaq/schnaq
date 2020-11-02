bc22c1c1dcc66ee48aaa27b33abb5c101d1de938
Christian Meter <cmeter@googlemail.com>
2020-11-02 18:24:32 +0100

    Set website title also in discussions

--- src/main/schnaq/interface/views/pages.cljs
+++ src/main/schnaq/interface/views/pages.cljs
@@ -1,10 +1,9 @@
 (ns schnaq.interface.views.pages
   "Defining page-layouts."
   (:require [cljs.spec.alpha :as s]
-            [ghostwheel.core :refer [>defn >defn-]]
-            [goog.string :as gstring]
-            [oops.core :refer [oset!]]
+            [ghostwheel.core :refer [>defn]]
             [schnaq.interface.views.base :as base]
+            [schnaq.interface.views.common :as common]
             [schnaq.interface.views.navbar :as navbar]))
 
 (s/def :page/heading string?)
@@ -14,18 +13,11 @@
   (s/keys :req [:page/heading]
           :opt [:page/subheading :page/more-for-heading]))
 
-(>defn- set-website-title!
-  "Set a document's website title."
-  [title]
-  [string? :ret nil?]
-  (let [new-title (gstring/format "schnaq - %s" title)]
-    (oset! js/document [:title] new-title)))
-
 (>defn with-nav-and-header
   "Default page with header and curly wave."
   [{:page/keys [title heading subheading more-for-heading]} body]
   [::page-headings (s/+ vector?) :ret vector?]
-  (set-website-title! (or title heading))
+  (common/set-website-title! (or title heading))
   [:<>
    [navbar/navbar]
    [base/header heading subheading more-for-heading]
