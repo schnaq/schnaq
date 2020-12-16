9fc8cb1418376fba3b94dd8b25f8d2f9668f739d
Alexander Schneider <alexander@schneider.gg>
2020-12-16 16:40:42 +0100

    Fix test

--- src/test/schnaq/meeting/database_test.clj
+++ src/test/schnaq/meeting/database_test.clj
@@ -51,7 +51,7 @@
                                    :meeting/share-hash "Wegi-ist-der-schönste"
                                    :meeting/author (db/add-user-if-not-exists "Wegi")})
           agenda (db/add-agenda-point "Hi" "Beschreibung" meeting)
-          discussion (:agenda/discussion (db/fast-pull agenda))
+          discussion (:db/id (:agenda/discussion (db/fast-pull agenda)))
           christian-id (db/author-id-by-nickname "Christian")
           first-id (db/add-starting-statement! discussion christian-id "this is sparta")
           second-id (db/add-starting-statement! discussion christian-id "this is kreta")]
