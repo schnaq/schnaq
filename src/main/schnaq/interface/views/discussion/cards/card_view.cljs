ddcbb7c7c3919c18e23b9e626b633af5fcfa239b
Alexander Schneider <alexander@schneider.gg>
2020-12-22 11:01:13 +0100

    Use same header for all discussion views

--- src/main/schnaq/interface/views/discussion/cards/card_view.cljs
+++ src/main/schnaq/interface/views/discussion/cards/card_view.cljs
@@ -3,21 +3,42 @@
             [reitit.frontend.easy :as reitfe]
             [schnaq.interface.text.display-data :refer [img-path]]
             [schnaq.interface.utils.toolbelt :as toolbelt]
-            [schnaq.interface.views.base :as base]
+            [schnaq.interface.views.brainstorm.tools :as btools]
             [schnaq.interface.views.discussion.cards.card-elements :as elements]
+            [schnaq.interface.views.meeting.admin-buttons :as admin-buttons]
             [schnaq.interface.views.navbar :as navbar]))
 
 (defn- card-meeting-header
   "Overview header for a meeting with a name input"
-  []
-  [:nav.navbar.navbar-expand-lg.py-3.navbar-dark.context-header.shadow-straight-light
-   ;; schnaq logo
-   [:a.navbar-brand.mr-auto {:href (reitfe/href :routes/startpage)}
-    [:img.d-inline-block.align-middle.mr-2
-     {:src (img-path :logo-white) :width "150" :alt "schnaq logo"}]]
-   ;; name input
-   [:div.float-right
-    [navbar/username-bar-view-light]]])
+  [{:meeting/keys [title share-hash] :as meeting}]
+  (let [admin-access-map @(rf/subscribe [:meetings/load-admin-access])
+        edit-hash (get admin-access-map share-hash)]
+    [:nav.navbar.navbar-expand-lg.py-3.navbar-dark.context-header.shadow-straight-light
+     ;; schnaq logo
+     [:a.navbar-brand.mr-auto {:href (reitfe/href :routes/startpage)}
+      [:img.d-inline-block.align-middle.mr-2
+       {:src (img-path :logo-white) :width "150" :alt "schnaq logo"}]]
+     ;; hamburger
+     [:button.navbar-toggler
+      {:type "button" :data-toggle "collapse" :data-target "#schnaq-navbar"
+       :aria-controls "schnaq-navbar" :aria-expanded "false" :aria-label "Toggle navigation"
+       :data-html2canvas-ignore true}
+      [:span.navbar-toggler-icon]]
+     ;; menu items
+     [:div {:id "schnaq-navbar" :class "collapse navbar-collapse"}
+      ;; clickable title
+      [:div.mr-auto.clickable-no-hover
+       {:on-click
+        (fn []
+          (rf/dispatch [:navigation/navigate :routes.meeting/show
+                        {:share-hash share-hash}])
+          (rf/dispatch [:meeting/select-current meeting]))}
+       [:h3.mx-5 title]]
+      [admin-buttons/txt-export share-hash title]
+      (when (and edit-hash (btools/is-brainstorm? meeting))
+        [admin-buttons/admin-center share-hash edit-hash])
+      ;; name input
+      [navbar/username-bar-view-light]]]))
 
 (defn- discussion-start-view
   "The first step after starting a discussion."
@@ -51,7 +72,7 @@
         input-desktop [elements/input-conclusion-form "input-statement-id-desktop"]
         input-mobile [elements/input-conclusion-form "input-statement-id-mobile"]]
     [:<>
-     [base/meeting-header current-meeting]
+     [card-meeting-header current-meeting]
      [:div.container-fluid.px-0
       [toolbelt/desktop-mobile-switch
        [elements/discussion-view-desktop
