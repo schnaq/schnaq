4a0456a9a58b7304700a90a9707f7e736c1b716e
Christian Meter <cmeter@googlemail.com>
2021-01-25 10:37:54 +0100

    Add dev config for keycloak

--- src/main/schnaq/interface/config.cljs
+++ src/main/schnaq/interface/config.cljs
@@ -7,6 +7,16 @@
 (goog-define spotlight-1 "https://schnaq.com/schnaq/7d6f68cb-ac6c-4407-ba86-ada29b2abb3b/")
 (goog-define spotlight-2 "https://schnaq.com/schnaq/0950ab05-0edb-441e-b8ca-f8907e85c5c6/")
 (goog-define spotlight-3 "https://schnaq.com/schnaq/ed5788b6-11da-4016-bb24-a93646705739/")
+(goog-define keycloak-realm "schnaq")
+(goog-define keycloak-auth-server-url "https://keycloak.disqtec.com/auth/")
+(goog-define keycloak-ssl-required "external")
+(goog-define keycloak-resource "development")
+(goog-define keycloak-public-client true)
+
+(def keycloak
+  {:url keycloak-auth-server-url
+   :realm keycloak-realm
+   :clientId keycloak-resource})
 
 (def deleted-statement-text "[deleted]")
 
