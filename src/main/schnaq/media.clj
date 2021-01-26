642fdc035987059001b772ec9310877ae0de67eb
Mike Birkhoff <michael.birkhoff@hhu.de>
2021-01-26 18:50:34 +0100

    Add s3 upload

--- src/main/schnaq/media.clj
+++ src/main/schnaq/media.clj
@@ -1,18 +1,37 @@
 (ns schnaq.media
   (:require [ghostwheel.core :refer [>defn]]
+            [clj-http.client :as client]
             [ring.util.http-response :refer [ok]]
             [schnaq.validator :as validator]
+            [schnaq.s3 :as s3]
             [taoensso.timbre :as log]))
 
+(defn upload-header-from-url [url key]
+  (try
+    (let [img (client/get url {:as :stream})]
+      (s3/upload-data-to-s3 img key))
+    (catch Exception e
+      (log/debug (.getMessage e))
+      {:error "Setting image failed"})))
 
 (>defn set-preview-image
   "Set preview image"
   [{:keys [body-params]}]
   [:ring/request :ret :ring/response]
-  (let [{:keys [share-hash edit-hash image-url]} body-params]
+  (let [{:keys [share-hash edit-hash image-url]} body-params
+        key (str "header-" share-hash)]
     (if (validator/valid-credentials? share-hash edit-hash)
       (do (log/debug (str "Set preview image" image-url))
-          (ok (merge
-                {:message "Image set"}
-                {:result "ToDo"})))
+          (upload-header-from-url image-url key)
+          (ok (upload-header-from-url image-url key)))
       (validator/deny-access))))
+
+;; experimental stuff
+
+(comment
+  (try
+    (upload-header-from-url
+      "https://cdn.pixabay.com/photo/2020/10/23/17/47/girl-5679419_960_720.jpg"
+      "fooo2")
+    (catch Exception e
+      {:error (.getMessage e)})))
---
+++ src/main/schnaq/s3.clj
@@ -0,0 +1,18 @@
+(ns schnaq.s3
+  (:require [amazonica.aws.s3 :as s3]))
+
+(def ^:private bucket-headers "schnaq-header-images")
+
+(def ^:private credentials {:access-key "minio"
+                            :secret-key "***REMOVED***"
+                            :endpoint "https://s3.disqtec.com"
+                            :client-config
+                            {:path-style-access-enabled true}})
+
+(defn upload-data-to-s3 [file key]
+  (-> credentials
+      (s3/put-object :bucket-name bucket-headers
+                     :key key
+                     :input-stream (:body file)
+                     :metadata {:content-length (:length file)}))
+  {:message "Image upload sucessfull"})
