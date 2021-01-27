9e9d1283ff4d84ed1fe66271d034a6a8612a71e5
Mike Birkhoff <michael.birkhoff@hhu.de>
2021-01-27 14:20:52 +0100

    Check for image source

--- src/main/schnaq/media.clj
+++ src/main/schnaq/media.clj
@@ -6,6 +6,8 @@
             [schnaq.s3 :as s3]
             [taoensso.timbre :as log]))
 
+(def ^:private trusted-cdn-url-regex (re-pattern "https://cdn\\.pixabay\\.com/photo(.+)"))
+
 (defn- upload-header-from-url [url key]
   (try
     (let [img (client/get url {:as :stream})]
@@ -14,21 +16,31 @@
       (log/debug (.getMessage e))
       {:error "Setting image failed"})))
 
+(defn- check-url [url]
+  (re-matches trusted-cdn-url-regex url))
+
 (>defn set-preview-image
   "Set preview image"
   [{:keys [body-params]}]
   [:ring/request :ret :ring/response]
   (let [{:keys [share-hash edit-hash image-url]} body-params
-        key (str "header-" share-hash)]
+        key (str "header-" share-hash)
+        checked-url (check-url image-url)]
     (if (validator/valid-credentials? share-hash edit-hash)
-      (do (log/debug (str "Set preview image" image-url))
-          (upload-header-from-url image-url key)
-          (ok (upload-header-from-url image-url key)))
+      (if checked-url
+        (do (log/debug (str "Set preview image" image-url))
+            (upload-header-from-url image-url key)
+            (ok (upload-header-from-url image-url key)))
+        (ok {:error "prohibited cdn"}))
       (validator/deny-access))))
 
 ;; experimental stuff
 
 (comment
+
+  (check-url "https://cdn.pixabay.com/photo/2020/10/23/17/47/girl-5679419_960_720.jpg")
+  (check-url "aaaaa")
+
   (try
     (upload-header-from-url
       "https://cdn.pixabay.com/photo/2020/10/23/17/47/girl-5679419_960_720.jpg"
