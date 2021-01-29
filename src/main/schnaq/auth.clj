16d0f35a21474f92878bf5ca82f37c4fd7489489
Christian Meter <cmeter@googlemail.com>
2021-01-29 16:32:50 +0100

    Define second backend for testing, in dev mode

--- src/main/schnaq/auth.clj
+++ src/main/schnaq/auth.clj
@@ -2,12 +2,36 @@
   (:require [buddy.auth :refer [authenticated?]]
             [buddy.auth.backends :as backends]
             [buddy.auth.middleware :refer [wrap-authentication]]
+            [buddy.core.keys :as keys]
             [ghostwheel.core :refer [>defn-]]
             [ring.util.http-response :refer [unauthorized]]
-            [schnaq.config :as config]))
+            [schnaq.config :as config]
+            [schnaq.core :as schnaq-core]))
 
-(def signed-jwt-backend (backends/jws {:secret config/keycloak-public-key
-                                       :options {:alg :rs256}}))
+(def ^:private public-key-for-test-backend
+  "Public key just for testing purposes."
+  (keys/str->public-key
+    "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4icNzKKV/FhHFmYTRZGp28Gc7Vk4TAsCRsdb3w2Mv7EUzAOgdB0/I+VluvIN7u9wwohdg+UThjtQYY4tW036AefBgG6qdjpnGkxovmx1tlPEloR0fcnFUyB19XCQVhWOktnvQ45MB/O7VcxK14+A0t16iBcULT91mnqeKm08WiFgdU7s2qONPfDsMGOH7XBX4RNiBshxEqlpmAMvyxDHitqzt2bLJ9E08OCNbFKjXCMc763E/t90/RqEVGQsMGAu49Pi12r2ZszJYwZRMNYl0EJIfPeX4g9q0BdcuqKGvnjwyllFOswD/NE5htnLhJUP8aNS+45iHysMwyy7AuhvEwIDAQAB\n-----END PUBLIC KEY-----"))
+
+(def ^:private signed-jwt-backend-for-testing
+  "Second backend to validate the test-tokens."
+  (backends/jws {:secret public-key-for-test-backend
+                 :options {:alg :rs256}}))
+
+(def ^:private signed-jwt-backend
+  "Primary backend for JWT validation."
+  (backends/jws {:secret config/keycloak-public-key
+                 :options {:alg :rs256}}))
+
+(defn wrap-jwt-authentication
+  "Use buddys jwt backend with our public key for authentication."
+  [handler]
+  (if schnaq-core/production-mode?
+    (wrap-authentication handler signed-jwt-backend)
+    (wrap-authentication handler signed-jwt-backend signed-jwt-backend-for-testing)))
+
+
+;; -----------------------------------------------------------------------------
 
 (>defn- has-admin-role?
   "Check if user has realm-wide admin access."
@@ -15,11 +39,6 @@
   [map? :ret boolean?]
   (= "admin" (some #{"admin"}
                    (get-in request [:identity :roles]))))
-
-(defn wrap-jwt-authentication
-  "Use buddys jwt backend with our public key for authentication."
-  [handler]
-  (wrap-authentication handler signed-jwt-backend))
 
 (defn auth-middleware
   "Validate, that user is logged-in."
