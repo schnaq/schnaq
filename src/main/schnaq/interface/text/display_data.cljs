159312079c867482d4dc7372278a89058b6644cd
Mike Birkhoff <michael.birkhoff@hhu.de>
2021-02-03 18:20:08 +0100

    Refactor history view desktop

--- src/main/schnaq/interface/text/display_data.cljs
+++ src/main/schnaq/interface/text/display_data.cljs
@@ -365,6 +365,9 @@
         ;; History
         :history.home/text "Start"
         :history.home/tooltip "Back to the discussion's beginning"
+        :history.statement/user "Post from "
+        :history.all-schnaqs/text "My schnaqs"
+        :history.all-schnaqs/tooltip "Back to all your schnaqs"
 
         ;; Route Link Texts
         :router.features/discussion "Discussion features"
@@ -759,6 +762,8 @@
         :history.home/text "Diskussionsstart"
         :history.home/tooltip "Zurück zum Diskussionsanfang"
         :history.statement/user "Beitrag von "
+        :history.all-schnaqs/text "Meine schnaqs"
+        :history.all-schnaqs/tooltip "Zurück zu deinen schnaqs"
 
         ;; Route Link Texts
         :router.features/discussion "Diskussionsfeatures"
