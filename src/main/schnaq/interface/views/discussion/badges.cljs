187632b44b6b33d5005053b401413dc74b994990
Mike Birkhoff <michael.birkhoff@hhu.de>
2021-02-04 14:13:28 +0100

    Refactor badges

--- src/main/schnaq/interface/views/discussion/badges.cljs
+++ src/main/schnaq/interface/views/discussion/badges.cljs
@@ -56,10 +56,10 @@
      (when edit-hash
        [delete-clicker statement edit-hash])]))
 
-(defn current-schnaq-info-badges
-  "Badges that display current schnaq info."
-  []
-  (let [meta-info @(rf/subscribe [:current-schnaq/meta-info])
+(defn satic-info-badges
+  "Badges that display schnaq info."
+  [subscribe]
+  (let [meta-info @(rf/subscribe [subscribe])
         statement-count (:all-statements meta-info)
         user-count (count (:authors meta-info))]
     [:p.mb-0
@@ -70,6 +70,11 @@
       {:tabIndex 20
        :title (labels :discussion.badges/user-overview)}
       [:i {:class (str "m-auto fas " (fa :user/group))}] " " user-count]]))
+
+(defn current-schnaq-info-badges
+  "Badges that display info about the current schnaq."
+  []
+  [satic-info-badges :current-schnaq/meta-info])
 
 ;; #### Subs ####
 
