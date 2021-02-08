5d8f92e962710bba10692eb2b15da8de8b308631
Christian Meter <cmeter@googlemail.com>
2021-02-08 17:09:33 +0100

    Add tests to analytics routes

---
+++ src/test/schnaq/api/analytics_test.clj
@@ -0,0 +1,45 @@
+(ns schnaq.api.analytics-test
+  (:require [clojure.test :refer [deftest is are testing]]
+            [ring.mock.request :as mock]
+            [schnaq.api.analytics :as sut]
+            [schnaq.test.toolbelt :refer [token-schnaqqifant-user token-n2o-admin token-wrong-signature token-timed-out mock-authorization-header]]))
+
+(defn- response-status [path token]
+  (:status (sut/analytics-routes
+             (-> (mock/request :get (format "/analytics%s" path))
+                 (mock-authorization-header token)))))
+
+(deftest permission-test
+  (testing "Analytics should only be accessible from logged in super users."
+    (are [path]
+      (= 200 (response-status path token-n2o-admin))
+      "/active-users"
+      "/statements-per-discussion"
+      "/active-users"
+      "/discussions"
+      "/statement-lengths"
+      "/statements"
+      "/usernames"))
+  (testing "Wrongly logged in users have no permission."
+    (are [path]
+      (= 403 (response-status path token-schnaqqifant-user))
+      "/active-users"
+      "/statements-per-discussion"
+      "/active-users"
+      "/discussions"
+      "/statement-lengths"
+      "/statements"
+      "/usernames"))
+  (testing "Unauthenticated users also have no access."
+    (are [path]
+      (= 401 (response-status path nil))
+      "/active-users"
+      "/statements-per-discussion"
+      "/active-users"
+      "/discussions"
+      "/statement-lengths"
+      "/statements"
+      "/usernames")))
+
+
+
