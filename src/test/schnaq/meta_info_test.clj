050f80bdb65e8dc5b1341ebf99905fbea3c30b05
Mike Birkhoff <michael.birkhoff@hhu.de>
2021-02-09 11:09:53 +0100

    Fix test

--- src/test/schnaq/meta_info_test.clj
+++ src/test/schnaq/meta_info_test.clj
@@ -9,8 +9,8 @@
 (use-fixtures :once schnaq-toolbelt/clean-database-fixture)
 
 (deftest meta-infos-test
-  (testing "Tests if number of posts are correct and authors increase after adding a new one to the discussion"
-    (let [share-hash "cat-dog-hash"
+  (testing "Tests if number of posts are correct and authors increase after adding a new one to the discussion\n"
+    (let [share-hash "simple-hash"
           discussion (discussion-db/discussion-by-share-hash share-hash)
           starting (:discussion/starting-statements discussion)
           arguments (discussion-db/all-arguments-for-discussion share-hash)
@@ -22,7 +22,10 @@
           _ (discussion-db/add-starting-statement! share-hash user-id statement)
           ;; new meta infos
           new-meta-infos (#'meta-info/discussion-meta-info share-hash)]
-      (is (= total-count (:statements-num meta-infos)))
-      (is (= (inc total-count) (:statements-num new-meta-infos)))
-      (is (= (inc (count (:authors meta-infos)))
-             (count (:authors new-meta-infos)))))))
+      (testing "Test if total count is correct"
+        (is (= total-count (:all-statements meta-infos))))
+      (testing "Test if total count is increased after adding a new statement"
+        (is (= (inc total-count) (:all-statements new-meta-infos))))
+      (testing "Test if author count is increased after adding a new statement by a new user"
+        (is (= (inc (count (:authors meta-infos)))
+               (count (:authors new-meta-infos))))))))
