2e13f9d0a120e28e7700f42e8ecfd4daa21da047
Christian Meter <cmeter@googlemail.com>
2021-02-10 23:22:36 +0100

    Move tooltip button to tooltip namespace

--- src/main/schnaq/interface/utils/tooltip.cljs
+++ src/main/schnaq/interface/utils/tooltip.cljs
@@ -18,3 +18,19 @@
              {:data-toggle "tooltip"
               :data-placement tooltip-location
               :title tooltip} content])}))
+
+(defn tooltip-button
+  [tooltip-location tooltip content on-click-fn]
+  (reagent/create-class
+    {:component-did-mount
+     (fn [comp] (js-wrap/tooltip (rdom/dom-node comp)))
+     :component-will-unmount
+     (fn [comp]
+       (js-wrap/tooltip (rdom/dom-node comp) "disable")
+       (js-wrap/tooltip (rdom/dom-node comp) "dispose"))
+     :reagent-render
+     (fn [] [:button.button-secondary-b-1.button-md.my-2.mx-3
+             {:on-click on-click-fn
+              :data-toggle "tooltip"
+              :data-placement tooltip-location
+              :title tooltip} content])}))
