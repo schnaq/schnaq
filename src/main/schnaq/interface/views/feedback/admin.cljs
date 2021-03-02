8eed30bc9e1544c26f5372c7e97e7c1fa02da9ed
Christian Meter <cmeter@googlemail.com>
2021-03-02 14:42:00 +0100

    Visualize txinstant in feedbacks

--- src/main/schnaq/interface/views/feedback/admin.cljs
+++ src/main/schnaq/interface/views/feedback/admin.cljs
@@ -6,39 +6,44 @@
             [schnaq.interface.auth :as auth]
             [schnaq.interface.config :refer [config]]
             [schnaq.interface.text.display-data :refer [labels]]
+            [schnaq.interface.utils.time :as time]
             [schnaq.interface.views.pages :as pages]))
 
 (defn- list-feedbacks
   "Shows a list of all feedback."
   []
   [:div.container.py-4
-   (if-let [feedbacks @(rf/subscribe [:feedbacks])]
-     [:<>
-      [:h4 (gstring/format "Es gibt %s Rückmeldungen 🥳!" (count feedbacks))]
-      [:table.table.table-striped
-       [:thead
-        [:tr
-         [:th {:width "20%"} (labels :feedbacks.overview/contact-name)]
-         [:th {:width "60%"} (labels :feedbacks.overview/description)]
-         [:th {:width "20%"} (labels :feedbacks/screenshot)]]]
-       [:tbody
-        (for [feedback feedbacks]
-          [:tr {:key (:db/id feedback)}
-           [:td (:feedback/contact-name feedback)
-            (when-not (string/blank? (:feedback/contact-mail feedback))
-              [:a {:href (gstring/format "mailto:%s" (:feedback/contact-mail feedback))}
-               [:i.far.fa-envelope.pl-1]])]
-           [:td (:feedback/description feedback)]
-           [:td.image
-            (when (:feedback/has-image? feedback)
-              (let [img-src (gstring/format "/media/feedbacks/screenshots/%s.png" (:db/id feedback))]
-                [:a {:href img-src}
-                 [:img.img-fluid.img-thumbnail {:src img-src}]]))]])]]]
-     [:div.text-center
-      [:h4.pb-3 (labels :feedbacks.missing/heading)]
-      [:button.btn.btn-outline-primary
-       {:on-click #(rf/dispatch [:feedbacks/fetch])}
-       (labels :feedbacks.missing/button-text)]])])
+   (let [feedbacks @(rf/subscribe [:feedbacks])
+         locale @(rf/subscribe [:current-locale])]
+     (if feedbacks
+       [:<>
+        [:h4 (gstring/format (labels :feedbacks.overview/table-header) (count feedbacks))]
+        [:table.table.table-striped
+         [:thead
+          [:tr
+           [:th {:width "10%"} (labels :feedbacks.overview/contact-name)]
+           [:th {:width "10%"} (labels :feedbacks.overview/when?)]
+           [:th {:width "60%"} (labels :feedbacks.overview/description)]
+           [:th {:width "20%"} (labels :feedbacks/screenshot)]]]
+         [:tbody
+          (for [feedback feedbacks]
+            [:tr {:key (:db/id feedback)}
+             [:td (:feedback/contact-name feedback)
+              (when-not (string/blank? (:feedback/contact-mail feedback))
+                [:a {:href (gstring/format "mailto:%s" (:feedback/contact-mail feedback))}
+                 [:i.far.fa-envelope.pl-1]])]
+             [:td (time/format-distance (:db/txInstant feedback) locale)]
+             [:td (:feedback/description feedback)]
+             [:td.image
+              (when (:feedback/has-image? feedback)
+                (let [img-src (gstring/format "/media/feedbacks/screenshots/%s.png" (:db/id feedback))]
+                  [:a {:href img-src}
+                   [:img.img-fluid.img-thumbnail {:src img-src}]]))]])]]]
+       [:div.text-center
+        [:h4.pb-3 (labels :feedbacks.missing/heading)]
+        [:button.btn.btn-outline-primary
+         {:on-click #(rf/dispatch [:feedbacks/fetch])}
+         (labels :feedbacks.missing/button-text)]]))])
 
 (defn- overview
   "Shows the page for an overview of all feedbacks."
