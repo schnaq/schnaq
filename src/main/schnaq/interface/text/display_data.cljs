69ee23b06b90b2c0dd6f8b7cc56d707328555647
Alexander Schneider <alexander.schneider@uni-duesseldorf.de>
2021-03-02 08:31:08 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- src/main/schnaq/interface/text/display_data.cljs
+++ src/main/schnaq/interface/text/display_data.cljs
@@ -901,7 +901,7 @@
      :startpage.features/discussion-graph "/imgs/startpage/sample_graph.png"
      :startpage.features/meeting-organisation "/imgs/startpage/meeting_organisation_500px.png"
      :startpage.features/sample-discussion "/imgs/startpage/discussion_vegan.jpg"
-     :startpage/newsletter "imgs/startpage/newsletter.jpeg"}))
+     :startpage/newsletter "/imgs/startpage/newsletter.jpeg"}))
 
 (defn video
   "Returns an video path"
