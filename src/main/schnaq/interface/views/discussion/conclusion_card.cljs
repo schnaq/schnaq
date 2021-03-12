1643a2c38e1aa9aa491e3b8c8c0c93312d965ad7
Alexander Schneider <alexander@schneider.gg>
2021-03-12 19:26:10 +0100

    Add sorting by newest

--- src/main/schnaq/interface/views/discussion/conclusion_card.cljs
+++ src/main/schnaq/interface/views/discussion/conclusion_card.cljs
@@ -86,17 +86,18 @@
         admin-access-map @(rf/subscribe [:schnaqs/load-admin-access])
         edit-hash (get admin-access-map share-hash)]
     (if (seq conclusions)
-      [:div.card-columns.card-columns-discussion.py-3
-       (for [conclusion conclusions]
-         [:div {:key (:db/id conclusion)
-                :on-click (fn [_e]
-                            (let [selection (.toString (.getSelection js/window))]
-                              (when (zero? (count selection))
-                                (rf/dispatch [:discussion.select/conclusion conclusion])
-                                (rf/dispatch [:discussion.history/push conclusion])
-                                (rf/dispatch [:navigation/navigate :routes.schnaq.select/statement
-                                              (assoc path-params :statement-id (:db/id conclusion))]))))}
-          [statement-card edit-hash conclusion (logic/arg-type->attitude (:meta/argument-type conclusion))]])]
+      (let [sorted-conclusions (sort-by :db/txInstant > conclusions)]
+        [:div.card-columns.card-columns-discussion.py-3
+         (for [conclusion sorted-conclusions]
+           [:div {:key (:db/id conclusion)
+                  :on-click (fn [_e]
+                              (let [selection (.toString (.getSelection js/window))]
+                                (when (zero? (count selection))
+                                  (rf/dispatch [:discussion.select/conclusion conclusion])
+                                  (rf/dispatch [:discussion.history/push conclusion])
+                                  (rf/dispatch [:navigation/navigate :routes.schnaq.select/statement
+                                                (assoc path-params :statement-id (:db/id conclusion))]))))}
+            [statement-card edit-hash conclusion (logic/arg-type->attitude (:meta/argument-type conclusion))]])])
       [call-to-contribute])))
 
 (rf/reg-event-fx
