94acfe5ab7218f80845609ae45870f109760fa48
Christian Meter <cmeter@googlemail.com>
2021-03-21 00:34:05 +0100

    Add authentication and group membership verification to hubs

--- src/main/schnaq/api/hubs.clj
+++ src/main/schnaq/api/hubs.clj
@@ -1,16 +1,21 @@
 (ns schnaq.api.hubs
-  (:require [compojure.core :refer [GET routes context]]
-            [ring.util.http-response :refer [ok]]
+  (:require [compojure.core :refer [GET routes wrap-routes context]]
+            [ring.util.http-response :refer [ok forbidden]]
+            [schnaq.auth :as auth]
             [schnaq.database.hub :as hub-db]))
 
 (defn- hub-by-keycloak-name
   "Query hub by its referenced name in keycloak."
   [request]
   (let [keycloak-name (get-in request [:route-params :keycloak-name])]
-    (ok {:hubs (hub-db/hub-by-keycloak-name keycloak-name)})))
+    (if (auth/group-membership? request keycloak-name)
+      (ok {:hub (hub-db/hub-by-keycloak-name keycloak-name)})
+      (forbidden "You are not allowed to access this ressource."))))
 
 (def hub-routes
   (->
     (routes
-      (context "/hubs" []
-        (GET "/:keycloak-name" [] hub-by-keycloak-name)))))
+      (context "/hub" []
+        (GET "/:keycloak-name" [] hub-by-keycloak-name)))
+    (wrap-routes auth/auth-middleware)
+    (wrap-routes auth/wrap-jwt-authentication)))
