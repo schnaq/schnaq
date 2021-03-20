05d66af6a85c39016c82bb3e1bdf4de6e1567491
Alexander Schneider <alexander@schneider.gg>
2021-03-20 22:20:56 +0100

    Add functions for adding hubs and schnaqs to hubs
    Co-Authored-By: Philip Bernardy <philip.bernardy@gmail.com>

--- src/main/schnaq/database/hub.clj
+++ src/main/schnaq/database/hub.clj
@@ -1,5 +1,5 @@
 (ns schnaq.database.hub
-  (:require [datomic.client.api :as d]
+  (:require [clojure.spec.alpha :as s]
             [ghostwheel.core :refer [>defn ? >defn-]]
             [schnaq.meeting.database :refer [transact new-connection query] :as main-db]
             [schnaq.meeting.specs :as specs]
@@ -13,10 +13,25 @@
 (>defn create-hub
   [hub-name]
   [:hub/name :ret ::specs/hub]
-  (transact [{:hub/name hub-name}]))
+  (let [new-hub (get-in
+                  (transact [{:db/id "temp"
+                              :hub/name hub-name}])
+                  [:tempids "temp"])]
+    (log/info "Created hub " new-hub)
+    (main-db/fast-pull new-hub hub-pattern)))
+
+(>defn add-discussions-to-hub
+  [hub-id discussion-ids]
+  [:db/id (s/coll-of :db/id) :ret ::specs/hub]
+  (transact (mapv #(vector :db/add hub-id :hub/schnaqs %) discussion-ids))
+  (log/info "Added schnaqs with ids " discussion-ids " to hub " hub-id)
+  (main-db/fast-pull hub-id hub-pattern))
 
 (comment
-  (create-hub "pornhub")
+  (create-hub "pinhub")
+  (add-discussions-to-hub 83562883715369 [101155069759783
+                                          101155069759742
+                                          101155069759677])
   (query
     '[:find (pull ?hubs hub-pattern)
       :in $ hub-pattern
---
+++ src/test/schnaq/database/hub_test.clj
@@ -0,0 +1,30 @@
+(ns schnaq.database.hub-test
+  (:require [clojure.test :refer [is use-fixtures deftest]]
+            [schnaq.database.discussion-test-data :as test-data]
+            [schnaq.database.discussion :as discussion-db]
+            [schnaq.database.hub :refer [add-discussions-to-hub] :as hub]
+            [schnaq.test.toolbelt :as schnaq-toolbelt]))
+
+(use-fixtures :each
+              schnaq-toolbelt/init-test-delete-db-fixture
+              #(schnaq-toolbelt/init-test-delete-db-fixture % test-data/public-discussions))
+(use-fixtures :once schnaq-toolbelt/clean-database-fixture)
+
+(deftest add-discussions-to-hub-test
+  (let [hub (hub/create-hub "test-hub")
+        discussion (first (discussion-db/all-discussions-by-title "Public Test"))
+        cat-dog-discussion (first (discussion-db/all-discussions-by-title "Cat or Dog?"))]
+    (is (empty? (:hub/schnaqs hub)))
+    (let [modified-hub (add-discussions-to-hub (:db/id hub) [(:db/id discussion)
+                                                             (:db/id cat-dog-discussion)])]
+      (is (= 2 (count (:hub/schnaqs modified-hub))))
+      (is (= #{"Public Test" "Cat or Dog?"} (->> modified-hub
+                                                 :hub/schnaqs
+                                                 (map :discussion/title)
+                                                 set))))))
+
+(deftest create-hub-test
+  (let [name "porni"
+        new-hub (hub/create-hub name)]
+    (is (= name (:hub/name new-hub)))
+    (is (empty? (:hub/schnaqs new-hub)))))
