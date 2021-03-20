722d76f7b88f5783239d5a06dfb9d075ab9a9b9d
Alexander Schneider <alexander@schneider.gg>
2021-03-20 22:25:30 +0100

    Remove whitespaces from logs
    Co-Authored-By: Philip Bernardy <philip.bernardy@gmail.com>

--- src/main/schnaq/database/hub.clj
+++ src/main/schnaq/database/hub.clj
@@ -17,12 +17,12 @@
                   (transact [{:db/id "temp"
                               :hub/name hub-name}])
                   [:tempids "temp"])]
-    (log/info "Created hub " new-hub)
+    (log/info "Created hub" new-hub)
     (main-db/fast-pull new-hub hub-pattern)))
 
 (>defn add-discussions-to-hub
   [hub-id discussion-ids]
   [:db/id (s/coll-of :db/id) :ret ::specs/hub]
   (transact (mapv #(vector :db/add hub-id :hub/schnaqs %) discussion-ids))
-  (log/info "Added schnaqs with ids " discussion-ids " to hub " hub-id)
+  (log/info "Added schnaqs with ids" discussion-ids "to hub" hub-id)
   (main-db/fast-pull hub-id hub-pattern))
