7e664836e6a957ac3062c22cde2fd82ac88695b5
Christian Meter <cmeter@googlemail.com>
2021-03-21 01:13:54 +0100

    Add fixtures
    Co-Authored-By: Alexander Schneider <alexander@schneider.gg>

--- src/test/schnaq/api/hub_test.clj
+++ src/test/schnaq/api/hub_test.clj
@@ -1,7 +1,11 @@
 (ns schnaq.api.hub-test
-  (:require [clojure.test :refer [deftest is testing]]
+  (:require [clojure.test :refer [deftest is testing use-fixtures]]
             [ring.mock.request :as mock]
-            [schnaq.api.hub :as hub-api]))
+            [schnaq.api.hub :as hub-api]
+            [schnaq.test.toolbelt :as toolbelt]))
+
+(use-fixtures :each toolbelt/init-test-delete-db-fixture)
+(use-fixtures :once toolbelt/clean-database-fixture)
 
 (deftest hub-by-keycloak-name-test
   (let [sut #'hub-api/hub-by-keycloak-name
