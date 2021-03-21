6ace62f03a26f46a369799b29971111d763319a6
Christian Meter <cmeter@googlemail.com>
2021-03-21 01:11:20 +0100

    Wrap with testing fn
    Co-Authored-By: Alexander Schneider <alexander@schneider.gg>

--- src/test/schnaq/api/hub_test.clj
+++ src/test/schnaq/api/hub_test.clj
@@ -1,5 +1,5 @@
 (ns schnaq.api.hub-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [deftest is testing]]
             [ring.mock.request :as mock]
             [schnaq.api.hub :as hub-api]))
 
@@ -9,10 +9,6 @@
         request #(-> (mock/request :get (format "/hub/%s" keycloak-name))
                      (assoc-in [:identity :groups] [%])
                      (assoc-in [:params :keycloak-name] keycloak-name))]
-    (is (= 200 (:status (sut (request "schnaqqifantenparty")))))
-    (is (= 403 (:status (sut (request "wrong-group")))))))
-
-
-
-
-
+    (testing "Only request with valid group memberships should be allowed."
+      (is (= 200 (:status (sut (request "schnaqqifantenparty")))))
+      (is (= 403 (:status (sut (request "wrong-group"))))))))
