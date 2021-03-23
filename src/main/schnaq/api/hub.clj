014fac4ef8328e8d0fd16e5c00a260bfb1b6f5fb
Alexander Schneider <alexander@schneider.gg>
2021-03-23 11:38:15 +0100

    Process meta-information for discussions when querying hub

--- src/main/schnaq/api/hub.clj
+++ src/main/schnaq/api/hub.clj
@@ -2,14 +2,17 @@
   (:require [compojure.core :refer [GET routes wrap-routes context]]
             [ring.util.http-response :refer [ok forbidden]]
             [schnaq.auth :as auth]
-            [schnaq.database.hub :as hub-db]))
+            [schnaq.database.hub :as hub-db]
+            [schnaq.meeting.processors :as processors]))
 
 (defn- hub-by-keycloak-name
   "Query hub by its referenced name in keycloak."
   [request]
   (let [keycloak-name (get-in request [:params :keycloak-name])]
     (if (auth/group-membership? request keycloak-name)
-      (ok {:hub (hub-db/hub-by-keycloak-name keycloak-name)})
+      (let [hub (hub-db/hub-by-keycloak-name keycloak-name)
+            processed-hub (update hub :hub/schnaqs #(map processors/add-meta-info-to-schnaq %))]
+        (ok {:hub processed-hub}))
       (forbidden "You are not allowed to access this ressource."))))
 
 (def hub-routes
