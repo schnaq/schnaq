84dbb4480b7e58d35fe81b1d3ce0d91df5ee8a84
Alexander Schneider <alexander@schneider.gg>
2021-03-26 10:51:45 +0100

    Add endpoint for adding a schnaq to a hub

--- src/main/schnaq/api/hub.clj
+++ src/main/schnaq/api/hub.clj
@@ -1,9 +1,11 @@
 (ns schnaq.api.hub
-  (:require [compojure.core :refer [GET routes wrap-routes context]]
-            [ring.util.http-response :refer [ok forbidden]]
+  (:require [compojure.core :refer [GET POST routes wrap-routes context]]
+            [ring.util.http-response :refer [ok forbidden bad-request]]
             [schnaq.auth :as auth]
+            [schnaq.meeting.database :refer [fast-pull]]
             [schnaq.database.hub :as hub-db]
-            [schnaq.processors :as processors]))
+            [schnaq.processors :as processors]
+            [schnaq.validator :as validators]))
 
 (defn- hub-by-keycloak-name
   "Query hub by its referenced name in keycloak."
@@ -21,12 +23,26 @@
   (let [keycloak-names (get-in request [:identity :groups])]
     (ok {:hubs (hub-db/hubs-by-keycloak-names keycloak-names)})))
 
+(defn- add-schnaq-to-hub
+  "Adds a schnaq to a hub identified by the group-name. Only allow the adding when
+  the schnaq is not exclusively tied to another hub. Also check for appropriate group membership."
+  [{:keys [params identity]}]
+  (let [{:keys [keycloak-name share-hash]} params]
+    (if (some #{keycloak-name} (:groups identity))
+      (if (validators/valid-discussion? share-hash)
+        ;; NOTE: When hub-exclusive schnaqs are in, check it in the if above.
+        (let [discussion-id (:db/id (fast-pull [:discussion/share-hash share-hash] [:db/id]))]
+          (hub-db/add-discussions-to-hub keycloak-name [discussion-id]))
+        (bad-request "The discussion could not be found."))
+      (forbidden "You are not a member of the group."))))
+
 (def hub-routes
   (->
     (routes
       (context "/hubs" []
         (GET "/personal" [] all-hubs-for-user))
       (context "/hub" []
-        (GET "/:keycloak-name" [] hub-by-keycloak-name)))
+        (GET "/:keycloak-name" [] hub-by-keycloak-name)
+        (POST "/:keycloak-name/add" [] add-schnaq-to-hub)))
     (wrap-routes auth/auth-middleware)
     (wrap-routes auth/wrap-jwt-authentication)))
