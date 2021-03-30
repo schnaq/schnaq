2ccb50373ab26f3d1da931a47f86d96c53979d71
Alexander Schneider <alexander@schneider.gg>
2021-03-30 15:54:42 +0200

    Wire in automatic update, after updating in the backend

--- src/main/schnaq/interface/views/discussion/edit.cljs
+++ src/main/schnaq/interface/views/discussion/edit.cljs
@@ -1,5 +1,7 @@
 (ns schnaq.interface.views.discussion.edit
-  (:require [re-frame.core :as rf]
+  (:require [oops.core :refer [oget oget+]]
+            [re-frame.core :as rf]
+            [schnaq.interface.utils.http :as http]
             [schnaq.interface.utils.js-wrapper :as js-wrap]))
 
 ;; TODO replace strings with labels
@@ -9,6 +11,10 @@
   [statement]
   (let [statement-html-id (str "statement-edit-" (:db/id statement))]
     [:form.card.statement-card.py-2.px-3
+     {:on-submit (fn [e]
+                   (js-wrap/prevent-default e)
+                   (rf/dispatch [:statement.edit/send (:db/id statement) statement-html-id
+                                 (oget e [:target :elements])]))}
      [:div.form-group
       [:label {:for statement-html-id} "New statement text:"]
       [:textarea.form-control {:id statement-html-id
@@ -16,13 +22,40 @@
                                :placeholder (:statement/content statement)
                                :defaultValue (:statement/content statement)}]]
      [:div.text-right
-      [:button.btn.btn-outline-primary.mr-1 "Submit"]
-      ;; TODO wire in functionality to submit.
+      [:button.btn.btn-outline-primary.mr-1 {:type "submit"} "Submit"]
       [:button.btn.btn-outline-secondary
        {:on-click (fn [e]
                     (js-wrap/prevent-default e)
                     (rf/dispatch [:statement.edit/deactivate-edit (:db/id statement)]))}
        "Cancel"]]]))
+
+(rf/reg-event-fx
+  :statement.edit/send
+  (fn [{:keys [db]} [_ statement-id html-selector form]]
+    (let [share-hash (get-in db [:current-route :path-params :share-hash])]
+      {:fx [(http/xhrio-request db :put "/discussion/statement/edit"
+                                [:statement.edit.send/success form]
+                                {:statement-id statement-id
+                                 :share-hash share-hash
+                                 :new-content (oget+ form [html-selector :value])}
+                                ;; TODO failure case
+                                [:statement.edit.send/failure])]})))
+
+(defn- update-statement-in-list
+  "Updates the content of a statement in a collection."
+  [coll new-statement]
+  (map #(if (= (:db/id new-statement) (:db/id %)) new-statement %) coll))
+
+;; TODO jede menge warnings auf der Konsole
+(rf/reg-event-fx
+  :statement.edit.send/success
+  (fn [{:keys [db]} [_ form response]]
+    (let [updated-statement (:updated-statement response)]
+      {:db (-> db
+               (update-in [:discussion :conclusions :starting] #(update-statement-in-list % updated-statement))
+               (update-in [:discussion :premises :current] #(update-statement-in-list % updated-statement)))
+       :fx [[:form/clear form]
+            [:dispatch [:statement.edit/deactivate-edit (:db/id updated-statement)]]]})))
 
 (rf/reg-event-db
   :statement.edit/activate-edit
