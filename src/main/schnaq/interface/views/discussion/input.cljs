2aad103f30b27d3519ac547c774ac99b18ede913
Mike Birkhoff <michael.birkhoff@hhu.de>
2021-04-15 20:51:37 +0200

    Refactor argument-type buttons

--- src/main/schnaq/interface/views/discussion/input.cljs
+++ src/main/schnaq/interface/views/discussion/input.cljs
@@ -8,34 +8,27 @@
 
 (defn- argument-type-button
   "Button to select current attitude."
-  [id button-type tooltip]
+  [id button-type label tooltip]
   (let [argument-type @(rf/subscribe [:form/argument-type])
-        checked? (= button-type argument-type)]
-    [:input {:id (str "argument-type-button-" id) :type "radio" :name "options" :autoComplete "off"
-             :defaultChecked checked?
-             :title (labels tooltip)
-             :on-click (fn [e] (jq/prevent-default e)
-                         (rf/dispatch [:form/argument-type! button-type]))}]))
+        checked? (= button-type argument-type)
+        active (= argument-type button-type)]
+    [:label.btn.btn-outline-primary.rounded-4
+     (when active
+       {:class "active"})
+     [:input {:id (str "argument-type-button-" id) :type "radio" :name "options" :autoComplete "off"
+              :defaultChecked checked?
+              :title (labels tooltip)
+              :on-click (fn [e] (jq/prevent-default e)
+                          (rf/dispatch [:form/argument-type! button-type]))}]
+     (labels label)]))
 
 (defn- argument-type-choose-button
   "Switch to differentiate between the argument types."
   []
-  (let [argument-type @(rf/subscribe [:form/argument-type])
-        active-class (fn [argument-type current-button] {:class (when (= argument-type current-button) "active")})
-        set-active (partial active-class argument-type)]
-    [:div.btn-group.btn-group-toggle {:data-toggle "buttons"}
-     [:label.btn.btn-outline-primary.rounded-4
-      (set-active :argument.type/support)
-      [argument-type-button "support" :argument.type/support :discussion/add-premise-against]
-      (labels :discussion.add.button/support)]
-     [:label.btn.btn-outline-primary
-      (set-active :argument.type/neutral)
-      [argument-type-button "neutral" :argument.type/neutral :discussion/add-premise-neutral]
-      (labels :discussion.add.button/neutral)]
-     [:label.btn.btn-outline-primary.rounded-4
-      (set-active :argument.type/attack)
-      [argument-type-button "attack" :argument.type/attack :discussion/add-premise-supporting]
-      (labels :discussion.add.button/attack)]]))
+  [:div.btn-group.btn-group-toggle {:data-toggle "buttons"}
+   [argument-type-button "support" :argument.type/support :discussion.add.button/support :discussion/add-premise-against]
+   [argument-type-button "neutral" :argument.type/neutral :discussion.add.button/neutral :discussion/add-premise-neutral]
+   [argument-type-button "attack" :argument.type/attack :discussion.add.button/attack :discussion/add-premise-supporting]])
 
 (defn- textarea-for-statements
   "Input, where users provide (starting) conclusions."
