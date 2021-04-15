d86812fb86b5854c05240178850fafd021285ccb
Alexander Schneider <alexander@schneider.gg>
2021-04-15 12:44:25 +0200

    Add social proof to startpage

--- src/main/schnaq/interface/views/startpage/call_to_actions.cljs
+++ src/main/schnaq/interface/views/startpage/call_to_actions.cljs
@@ -1,5 +1,5 @@
 (ns schnaq.interface.views.startpage.call-to-actions
-  (:require [schnaq.interface.text.display-data :refer [fa labels video]]
+  (:require [schnaq.interface.text.display-data :refer [fa labels video img-path]]
             [re-frame.core :as rf]))
 
 (defn- header-animation
@@ -29,14 +29,23 @@
    [:div.col-10.col-lg-11
     [:span.icon-points-text (labels desc-label)]]])
 
+(defn- social-proof
+  "A small section showing the user, that the risk was already taken by others."
+  []
+  [:p.text-social-proof.text-center.pt-2
+   [:img.social-proof-icon
+    {:src (img-path :schnaqqifant/white)}]
+   "schnaq hat schon in " [:b 350] " Diskussionen geholfen " [:b 2200] " mal Wissen auszutauschen."])
+
 (defn features-call-to-action
   "Displays a list of features with a call-to-action button to start a schnaq"
   []
   [:section.row.mt-3 {:key "HeaderExtras-Bullet-Points-and-Animation"}
    [:div.col-lg-6.py-lg-4.pr-lg-5
     [header-animation]
-    [start-schnaq-button]]
-   [:div.col-lg-6.py-lg-5
+    [start-schnaq-button]
+    [social-proof]]
+   [:div.col-lg-6.py-lg-5.pt-5
     [bullet-point :clipboard :feature/what]
     [bullet-point :user/group :feature/share]
     [bullet-point :site-map :feature/graph]
