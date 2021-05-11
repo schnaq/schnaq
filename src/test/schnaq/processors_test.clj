d9c596a1c8309a365f56d4518f816ad3533bf5c6
Alexander Schneider <alexander@schneider.gg>
2021-05-11 20:05:53 +0200

    Fix votes-processor test

--- src/test/schnaq/processors_test.clj
+++ src/test/schnaq/processors_test.clj
@@ -12,10 +12,9 @@
   (testing "Result should have all statements enriched with votes-metadata"
     (let [share-hash "cat-dog-hash"
           enriched-data (processors/with-votes (discussion-db/all-statements share-hash))
-          statements-only (flatten (map :argument/premises enriched-data))
-          upvotes-only (filter number? (map :meta/upvotes statements-only))
-          downvotes-only (filter number? (map :meta/downvotes statements-only))]
-      (is (= (count statements-only) (count upvotes-only) (count downvotes-only))))))
+          upvotes-only (filter number? (map :meta/upvotes enriched-data))
+          downvotes-only (filter number? (map :meta/downvotes enriched-data))]
+      (is (= (count enriched-data) (count upvotes-only) (count downvotes-only))))))
 
 (deftest add-meta-info-test
   (testing "Test if meta info was correctly added to schnaq"
