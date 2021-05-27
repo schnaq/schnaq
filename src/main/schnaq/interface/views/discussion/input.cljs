093a6d3256de3ba69218de799916f87e5a4816e5
Mike Birkhoff <michael.birkhoff@hhu.de>
2021-05-27 18:23:06 +0200

    Add user avatar in input area

--- src/main/schnaq/interface/views/discussion/input.cljs
+++ src/main/schnaq/interface/views/discussion/input.cljs
@@ -4,7 +4,8 @@
             [schnaq.interface.text.display-data :refer [fa labels]]
             [schnaq.interface.utils.js-wrapper :as jq]
             [schnaq.interface.utils.toolbelt :as toolbelt]
-            [schnaq.interface.views.discussion.logic :as logic]))
+            [schnaq.interface.views.discussion.logic :as logic]
+            [schnaq.interface.views.common :as common]))
 
 (defn- statement-type-button
   "Button to select the attitude of a statement. Current attitude is subscribed via get-subscription.
@@ -42,6 +43,7 @@
   (let [pro-con-disabled? @(rf/subscribe [:schnaq.selected/pro-con?])
         current-route-name @(rf/subscribe [:navigation/current-route-name])
         starting-route? (= :routes.schnaq/start current-route-name)
+        user @(rf/subscribe [:user/current])
         statement-type @(rf/subscribe [:form/statement-type])
         attitude (if starting-route? "neutral"
                                      (case statement-type
@@ -50,25 +52,31 @@
                                        :statement.type/neutral "neutral"))]
     [:div
      {:class (str "statement-card-" attitude " mobile-attitude-" attitude)}
-     [:div.discussion-input-content.input-group
-      [:textarea.form-control.discussion-text-input-area
-       {:name textarea-name :wrap "soft" :rows 1
-        :auto-complete "off"
-        :onInput #(toolbelt/height-to-scrollheight! (oget % :target))
-        ;; first reset input then set height +1px in order to prevent scrolling
-        :required true
-        :data-dynamic-height true
-        :placeholder (labels :discussion/add-argument-conclusion-placeholder)}]
-      [:div.d-flex.justify-content-between.mt-1.justify-content-md-end.mt-md-0.w-100
-       (when-not (or starting-route? pro-con-disabled?)
-         [:div.input-group-prepend
-          [statement-type-choose-button [:form/statement-type] [:form/statement-type!]]])
-       [:div.input-group-append
-        [:button.btn.btn-outline-primary.rounded-4.shadow-sm.ml-2
-         {:type "submit" :title (labels :discussion/create-argument-action)}
-         [:div.d-flex.flex-row
-          [:div.d-none.d-md-block.mr-1 (labels :statement.edit.button/submit)]
-          [:i {:class (str "m-auto fas " (fa :plane))}]]]]]]]))
+     [:div.discussion-input-content
+      [:div.d-flex.flex-row.justify-content-end.pr-lg-2
+       ;; hide 'new post from you' text on mobile
+       [:small.d-none.d-md-block.text-muted.mr-2 (labels :discussion.add.statement/new)]
+       [common/avatar #:user.registered{:profile-picture (get-in user [:profile-picture :display])
+                                        :display-name (get-in user [:names :display])} 32]]
+      [:div.input-group
+       [:textarea.form-control.discussion-text-input-area
+        {:name textarea-name :wrap "soft" :rows 1
+         :auto-complete "off"
+         :onInput #(toolbelt/height-to-scrollheight! (oget % :target))
+         ;; first reset input then set height +1px in order to prevent scrolling
+         :required true
+         :data-dynamic-height true
+         :placeholder (labels :discussion/add-argument-conclusion-placeholder)}]
+       [:div.d-flex.justify-content-between.mt-1.justify-content-md-end.mt-md-0.w-100
+        (when-not (or starting-route? pro-con-disabled?)
+          [:div.input-group-prepend
+           [statement-type-choose-button [:form/statement-type] [:form/statement-type!]]])
+        [:div.input-group-append
+         [:button.btn.btn-outline-primary.rounded-4.shadow-sm.ml-2
+          {:type "submit" :title (labels :discussion/create-argument-action)}
+          [:div.d-flex.flex-row
+           [:div.d-none.d-md-block.mr-1 (labels :statement.edit.button/submit)]
+           [:i {:class (str "m-auto fas " (fa :plane))}]]]]]]]]))
 
 (defn input-form
   "Form to collect the user's statements."
