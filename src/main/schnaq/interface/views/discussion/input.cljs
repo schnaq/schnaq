5b9066d697b3b422f576d5b32f9bd2097bab5428
Mike Birkhoff <michael.birkhoff@hhu.de>
2021-05-27 17:44:07 +0200

    Restyle input area to resemble a statement-card.

--- src/main/schnaq/interface/views/discussion/input.cljs
+++ src/main/schnaq/interface/views/discussion/input.cljs
@@ -12,7 +12,7 @@
   [statement-type label tooltip get-subscription set-event]
   (let [current-attitude @(rf/subscribe get-subscription)
         checked? (= statement-type current-attitude)]
-    [:label.btn.btn-outline-primary.rounded-4
+    [:label.btn.btn-outline-primary.rounded-4.shadow-sm
      (when checked? {:class "active"})
      [:input {:type "radio" :name "options" :autoComplete "off"
               :defaultChecked checked?
@@ -40,30 +40,35 @@
   "Input, where users provide (starting) conclusions."
   [textarea-name]
   (let [pro-con-disabled? @(rf/subscribe [:schnaq.selected/pro-con?])
-        statement-type @(rf/subscribe [:form/statement-type])
         current-route-name @(rf/subscribe [:navigation/current-route-name])
-        current-color (case statement-type
-                        :statement.type/support "text-primary"
-                        :statement.type/attack "text-secondary"
-                        :statement.type/neutral "text-dark")]
-    [:div.input-group
-     [:textarea.form-control.discussion-text-input-area
-      {:name textarea-name :wrap "soft" :rows 1
-       :auto-complete "off"
-       :onInput #(toolbelt/height-to-scrollheight! (oget % :target))
-       ;; first reset input then set height +1px in order to prevent scrolling
-       :required true
-       :data-dynamic-height true
-       :placeholder (labels :discussion/add-argument-conclusion-placeholder)}]
-     [:div.d-flex.justify-content-between.mt-1.justify-content-md-end.mt-md-0.w-100
-      (when-not (or (= :routes.schnaq/start current-route-name) pro-con-disabled?)
-        [:div.input-group-prepend
-         [statement-type-choose-button [:form/statement-type] [:form/statement-type!]]])
-      [:div.input-group-append
-       [:button.btn
-        {:type "submit" :class current-color
-         :title (labels :discussion/create-argument-action)}
-        [:i {:class (str "m-auto fas " (fa :plane))}]]]]]))
+        starting-route? (= :routes.schnaq/start current-route-name)
+        statement-type @(rf/subscribe [:form/statement-type])
+        attitude (if starting-route? "neutral"
+                                     (case statement-type
+                                       :statement.type/support "support"
+                                       :statement.type/attack "attack"
+                                       :statement.type/neutral "neutral"))]
+    [:div
+     {:class (str "statement-card-" attitude " mobile-attitude-" attitude)}
+     [:div.discussion-input-content.input-group
+      [:textarea.form-control.discussion-text-input-area
+       {:name textarea-name :wrap "soft" :rows 1
+        :auto-complete "off"
+        :onInput #(toolbelt/height-to-scrollheight! (oget % :target))
+        ;; first reset input then set height +1px in order to prevent scrolling
+        :required true
+        :data-dynamic-height true
+        :placeholder (labels :discussion/add-argument-conclusion-placeholder)}]
+      [:div.d-flex.justify-content-between.mt-1.justify-content-md-end.mt-md-0.w-100
+       (when-not (or starting-route? pro-con-disabled?)
+         [:div.input-group-prepend
+          [statement-type-choose-button [:form/statement-type] [:form/statement-type!]]])
+       [:div.input-group-append
+        [:button.btn.btn-outline-primary.rounded-4.shadow-sm.ml-2
+         {:type "submit" :title (labels :discussion/create-argument-action)}
+         [:div.d-flex.flex-row
+          [:div.d-none.d-md-block.mr-1 (labels :statement.edit.button/submit)]
+          [:i {:class (str "m-auto fas " (fa :plane))}]]]]]]]))
 
 (defn input-form
   "Form to collect the user's statements."
