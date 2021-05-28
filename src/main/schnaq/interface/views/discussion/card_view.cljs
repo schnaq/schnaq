f54c6b94460870ed57c32d2d355c89eacd306758
Alexander Schneider <alexander@schneider.gg>
2021-05-28 16:00:36 +0200

    Add a minimum width to the search-bar

--- src/main/schnaq/interface/views/discussion/card_view.cljs
+++ src/main/schnaq/interface/views/discussion/card_view.cljs
@@ -19,9 +19,10 @@
    {:on-submit (fn [e]
                  (jq/prevent-default e)
                  (rf/dispatch [:schnaq/search (oget e [:target :elements "search-input" :value])]))}
-   [:div.input-group
-    [:input.form-control.my-auto {:type "text" :aria-label "Search" :placeholder
-                                  (labels :schnaq.search/input) :name "search-input"}]
+   [:div.input-group.search-bar
+    [:input.form-control.my-auto
+     {:type "text" :aria-label "Search" :placeholder
+      (labels :schnaq.search/input) :name "search-input"}]
     [:div.input-group-append
      [:button.btn.btn-secondary
       {:type "submit"}
