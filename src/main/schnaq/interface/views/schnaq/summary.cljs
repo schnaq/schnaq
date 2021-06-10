48b749310962a2f5cef930a364d1de80bba13420
Alexander Schneider <alexander@schneider.gg>
2021-06-10 14:23:28 +0200

    Add request function and its handling

--- src/main/schnaq/interface/views/schnaq/summary.cljs
+++ src/main/schnaq/interface/views/schnaq/summary.cljs
@@ -1,17 +1,18 @@
 (ns schnaq.interface.views.schnaq.summary
   "All views and events important to extractive summaries can be found here."
   (:require [re-frame.core :as rf]
+            [schnaq.interface.utils.http :as http]
             [schnaq.interface.views.pages :as pages]))
 
 ;; todo Show modal if person is not registered / not in beta
 (defn- summary-request-button
   "Requests a summary or a refresh."
-  [schnaq]
+  [share-hash]
   [:section.d-block.text-center
    [:button.btn.btn-secondary
-    ;; todo dispatch method
-    {:on-click #(rf/dispatch [:todo schnaq])}
+    {:on-click #(rf/dispatch [:schnaq.summary/request share-hash])}
     ;; todo labelize
+    ;; todo button that changes text
     "Request summary"]
    ;; todo status text that changes
    [:p.small.text-muted.mt-2
@@ -26,7 +27,21 @@
       :page/subheading "See the discussion in a few sentences"}
      current-schnaq
      [:div.container.panel-white.mt-3
-      [summary-request-button current-schnaq]]]))
+      [summary-request-button (:discussion/share-hash current-schnaq)]]]))
 
 (defn public-user-view []
   [user-summary-view])
+
+(rf/reg-event-fx
+  :schnaq.summary/request
+  (fn [{:keys [db]} [_ share-hash]]
+    {:db (assoc-in db [:schnaq :summary :status share-hash] :requested)
+     :fx [(http/xhrio-request db :post "/schnaq/summary/request" [:schnaq.summary.request/success share-hash]
+                              {:share-hash share-hash})]}))
+
+(rf/reg-event-db
+  :schnaq.summary.request/success
+  (fn [db [_ share-hash result]]
+    (-> db
+        (assoc-in [:schnaq :summary :status share-hash] :request-succeeded)
+        (assoc-in [:schnaq :summary :result share-hash] result))))
