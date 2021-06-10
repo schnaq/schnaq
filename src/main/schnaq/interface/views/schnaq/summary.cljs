3eb51cb9518dbd6d5d990fe65d3c4e05f5344e36
Alexander Schneider <alexander@schneider.gg>
2021-06-10 13:03:40 +0200

    Add area for request button and summary status

--- src/main/schnaq/interface/views/schnaq/summary.cljs
+++ src/main/schnaq/interface/views/schnaq/summary.cljs
@@ -3,15 +3,29 @@
   (:require [schnaq.interface.views.pages :as pages]
             [re-frame.core :as rf]))
 
+(defn- summary-request-button
+  "Requests a summary or a refresh."
+  [schnaq]
+  [:section.d-block.text-center
+   [:button.btn.btn-secondary
+    ;; todo dispatch method
+    {:on-click #(rf/dispatch [:todo schnaq])}
+    ;; todo labelize
+    "Request summary"]
+   ;; todo status text that changes
+   [:p.small.text-muted.mt-2
+    "Press the button to request a summary. It will take a few hours. The summary will appear here as soon as its done."]])
+
 (defn- user-summary-view
   []
-  (let [current-discussion @(rf/subscribe [:schnaq/selected])]
+  (let [current-schnaq @(rf/subscribe [:schnaq/selected])]
     [pages/with-discussion-nav
      ;; todo labelize
      {:page/heading "Summaries"
       :page/subheading "See the discussion in a few sentences"}
-     current-discussion
-     [:div "Hallo"]]))
+     current-schnaq
+     [:div.container.panel-white.mt-3
+      [summary-request-button current-schnaq]]]))
 
 (defn public-user-view []
   [user-summary-view])
