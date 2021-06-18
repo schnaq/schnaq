18f9d61b7a924de081b0e2d7970f93cd9b115374
Alexander Schneider <alexander@schneider.gg>
2021-06-18 16:27:10 +0200

    Remove unused code

--- src/main/schnaq/interface/views/startpage/testimonials.cljs
+++ src/main/schnaq/interface/views/startpage/testimonials.cljs
@@ -2,24 +2,6 @@
   (:require [goog.string :as gstring]
             [schnaq.interface.text.display-data :refer [labels img-path]]))
 
-(defn- doctronic
-  "Add quote from doctronic."
-  []
-  [:div.row.testimonial
-   [:div.col-md-8.col-12
-    [:div.shadow.p-4.bg-light.rounded
-     [:blockquote.blockquote.text-center
-      [:p.mb-0
-       (gstring/format "\"%s\"" (labels :testimonials.doctronic/quote))]
-      [:div.blockquote-footer
-       (labels :testimonials.doctronic/author)]]]]
-
-   [:div.col-md-4.col-12.d-flex.align-items-center
-    [:a {:href "https://doctronic.de" :target :_blank}
-     [:img
-      {:src (img-path :logos/doctronic)
-       :alt "doctronic logo"}]]]])
-
 (defn- testimonial-card
   "A single testimonial of a user."
   [img company-name company-body reference-name]
