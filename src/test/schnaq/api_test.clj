0ba95ae979d0db9423b759ec50e5ea960f98780f
Christian Meter <cmeter@googlemail.com>
2021-07-06 13:50:51 +0200

    Fix schnaq by hashes test

--- src/test/schnaq/api_test.clj
+++ src/test/schnaq/api_test.clj
@@ -88,35 +88,35 @@
       (is (= 403 (:status (schnaq-by-hash-as-admin req-wrong-edit-hash))))
       (is (= 403 (:status (schnaq-by-hash-as-admin req-wrong-share-hash)))))))
 
-;(deftest schnaqs-by-hashes-test
-;  (let [schnaqs-by-hashes #'api/schnaqs-by-hashes
-;        share-hash1 "cat-dog-hash"
-;        share-hash2 "graph-hash"]
-;    (testing "No hash provided, no discussion returned."
-;      (is (= 400 (:status (schnaqs-by-hashes {})))))
-;    (testing "Invalid hash returns no discussion."
-;      (is (= 200 (:status (schnaqs-by-hashes
-;                            {:params {:share-hashes "something-non-existent"}}))))
-;      (is (empty? (get-in (schnaqs-by-hashes {:params {:share-hashes "something-non-existent"}})
-;                          [:body :discussions]))))
-;    (testing "Querying by a single valid hash returns a discussion."
-;      (let [api-call (schnaqs-by-hashes {:params {:share-hashes share-hash1}})]
-;        (is (= 200 (:status api-call)))
-;        (is (= 1 (count (get-in api-call [:body :discussions]))))
-;        (is (s/valid? ::specs/discussion (first (get-in api-call [:body :discussions]))))))
-;    (testing "A valid hash packed into a collection should also work."
-;      (let [api-call (schnaqs-by-hashes {:params {:share-hashes [share-hash1]}})]
-;        (is (= 200 (:status api-call)))
-;        (is (= 1 (count (get-in api-call [:body :discussions]))))
-;        (is (s/valid? ::specs/discussion (first (get-in api-call [:body :discussions]))))))
-;    (testing "Asking for multiple valid hashes, returns a list of valid discussions."
-;      (let [api-call (schnaqs-by-hashes {:params {:share-hashes [share-hash1 share-hash2]}})]
-;        (is (= 200 (:status api-call)))
-;        (is (= 2 (count (get-in api-call [:body :discussions]))))
-;        (is (every? true?
-;                    (map (partial s/valid? ::specs/discussion)
-;                         (get-in api-call [:body :discussions]))))))))
-;
+(deftest schnaqs-by-hashes-test
+  (let [schnaqs-by-hashes #'api/schnaqs-by-hashes
+        share-hash1 "cat-dog-hash"
+        share-hash2 "graph-hash"]
+    (testing "No hash provided, no discussion returned."
+      (is (= 400 (:status (schnaqs-by-hashes {})))))
+    (testing "Invalid hash returns no discussion."
+      (is (= 200 (:status (schnaqs-by-hashes
+                            {:parameters {:query {:share-hashes "something-non-existent"}}}))))
+      (is (empty? (get-in (schnaqs-by-hashes {:parameters {:query {:share-hashes "something-non-existent"}}})
+                          [:body :discussions]))))
+    (testing "Querying by a single valid hash returns a discussion."
+      (let [api-call (schnaqs-by-hashes {:parameters {:query {:share-hashes share-hash1}}})]
+        (is (= 200 (:status api-call)))
+        (is (= 1 (count (get-in api-call [:body :discussions]))))
+        (is (s/valid? ::specs/discussion (first (get-in api-call [:body :discussions]))))))
+    (testing "A valid hash packed into a collection should also work."
+      (let [api-call (schnaqs-by-hashes {:parameters {:query {:share-hashes [share-hash1]}}})]
+        (is (= 200 (:status api-call)))
+        (is (= 1 (count (get-in api-call [:body :discussions]))))
+        (is (s/valid? ::specs/discussion (first (get-in api-call [:body :discussions]))))))
+    (testing "Asking for multiple valid hashes, returns a list of valid discussions."
+      (let [api-call (schnaqs-by-hashes {:parameters {:query {:share-hashes [share-hash1 share-hash2]}}})]
+        (is (= 200 (:status api-call)))
+        (is (= 2 (count (get-in api-call [:body :discussions]))))
+        (is (every? true?
+                    (map (partial s/valid? ::specs/discussion)
+                         (get-in api-call [:body :discussions]))))))))
+
 ;(deftest edit-statement!-test
 ;  (let [edit-statement! #'api/edit-statement!
 ;        share-hash "simple-hash"
