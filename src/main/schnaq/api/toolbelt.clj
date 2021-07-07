6dff9fa7f9bd4995aaa0945f409163f8e64a5434
Christian Meter <cmeter@googlemail.com>
2021-07-07 14:05:25 +0200

    Add namespace for helpful api functions

---
+++ src/main/schnaq/api/toolbelt.clj
@@ -0,0 +1,16 @@
+(ns schnaq.api.toolbelt
+  (:require [clojure.spec.alpha :as s]
+            [ghostwheel.core :refer [>defn]]))
+
+(s/def ::error keyword?)
+(s/def ::message string?)
+(s/def ::error-body
+  (s/keys :req-un [::error ::message]))
+
+(>defn build-error-body
+  "Builds common error responses. Provide an `error-message` and it builds a
+  body for an api response."
+  [error-message error-type]
+  [::error ::message :ret ::error-body]
+  {:error error-type
+   :message error-message})
