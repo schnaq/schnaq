7767f5ec364569edfd08c0c77cc9b05477abcb42
Christian Meter <cmeter@googlemail.com>
2021-07-16 14:00:36 +0200

    Migrate mail routes

--- src/main/schnaq/api.clj
+++ src/main/schnaq/api.clj
@@ -23,6 +23,7 @@
             [ring.util.http-response :refer [ok created bad-request forbidden not-found]]
             [schnaq.api.analytics :refer [analytics-routes]]
             [schnaq.api.dto-specs :as dto]
+            [schnaq.api.emails :refer [email-routes]]
             [schnaq.api.feedback :refer [feedback-routes]]
             [schnaq.api.hub :refer [hub-routes]]
             [schnaq.api.summaries :refer [summary-routes]]
@@ -47,7 +48,6 @@
             [schnaq.media :as media]
             [schnaq.processors :as processors]
             [schnaq.toolbelt :as toolbelt]
-            [schnaq.translations :refer [email-templates]]
             [schnaq.validator :as validator]
             [spec-tools.core :as st]
             [taoensso.timbre :as log])
@@ -253,37 +253,6 @@
     (:body parameters) identity reaction-db/downvote-statement! reaction-db/remove-downvote!
     reaction-db/did-user-downvote-statement reaction-db/did-user-upvote-statement))
 
-(>defn- send-invite-emails
-  "Expects a list of recipients and the meeting which shall be send."
-  [{:keys [parameters]}]
-  [:ring/request :ret :ring/response]
-  (let [{:keys [share-hash edit-hash recipients share-link]} (:body parameters)
-        discussion-title (:discussion/title (discussion-db/discussion-by-share-hash share-hash))]
-    (if (validator/valid-credentials? share-hash edit-hash)
-      (do (log/debug "Invite Emails for some meeting sent")
-          (ok (merge
-                {:message "Emails sent successfully"}
-                (emails/send-mails
-                  (format (email-templates :invitation/title) discussion-title)
-                  (format (email-templates :invitation/body) discussion-title share-link)
-                  recipients))))
-      (validator/deny-access))))
-
-(>defn- send-admin-center-link
-  "Send URL to admin-center via mail to recipient."
-  [{:keys [parameters]}]
-  [:ring/request :ret :ring/response]
-  (let [{:keys [share-hash edit-hash recipient admin-center]} (:body parameters)
-        meeting-title (:discussion/title (discussion-db/discussion-by-share-hash share-hash))]
-    (if (validator/valid-credentials? share-hash edit-hash)
-      (do (log/debug "Send admin link for meeting " meeting-title " via E-Mail")
-          (ok (merge
-                {:message "Emails sent successfully"}
-                (emails/send-mails
-                  (format (email-templates :admin-center/title) meeting-title)
-                  (format (email-templates :admin-center/body) meeting-title admin-center)
-                  [recipient]))))
-      (validator/deny-access))))
 
 ;; -----------------------------------------------------------------------------
 ;; Discussion
@@ -644,25 +613,6 @@
                    :description (at/get-doc #'toggle-upvote-statement)
                    :responses {200 {:body (s/keys :req-un [:statement.vote/operation])}
                                400 at/response-error-body}}]]]]]
-
-       ["/emails" {:swagger {:tags ["emails"]}
-                   :parameters {:body {:share-hash :discussion/share-hash
-                                       :edit-hash :discussion/edit-hash}}}
-        ["/send-admin-center-link" {:post send-admin-center-link
-                                    :description (at/get-doc #'send-admin-center-link)
-                                    :parameters {:body {:recipient string?
-                                                        :admin-center string?}}
-                                    :responses {200 {:body {:message string?
-                                                            :failed-sendings (s/coll-of string?)}}
-                                                403 at/response-error-body}}]
-        ["/send-invites" {:post send-invite-emails
-                          :description (at/get-doc #'send-invite-emails)
-                          :parameters {:body {:recipients (s/coll-of string?)
-                                              :share-link :discussion/share-link}}
-                          :responses {200 {:body {:message string?
-                                                  :failed-sendings (s/coll-of string?)}}
-                                      403 at/response-error-body}}]]
-
        ["/schnaq" {:swagger {:tags ["schnaqs"]}}
         ["/by-hash" {:get schnaq-by-hash
                      :description (at/get-doc #'schnaq-by-hash)
@@ -722,11 +672,12 @@
                                :parameters {:body {:statement-ids (s/coll-of :db/id)}}
                                :responses {200 {:body {:deleted-statements (s/coll-of :db/id)}}}}]]
 
-       user-routes
-       hub-routes
        analytics-routes
-       summary-routes
+       email-routes
        feedback-routes
+       hub-routes
+       summary-routes
+       user-routes
 
        ["/swagger.json"
         {:get {:no-doc true
