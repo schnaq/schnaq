5d54014a326cc5fa6cc9796a8a5009149b8774fd
Mike Birkhoff <michael.birkhoff@hhu.de>
2021-08-11 15:44:23 +0200

    Add sort options for search

--- src/main/schnaq/interface/views/discussion/search.cljs
+++ src/main/schnaq/interface/views/discussion/search.cljs
@@ -5,6 +5,7 @@
             [schnaq.interface.views.common :as common]
             [schnaq.interface.views.discussion.card-elements :as elements]
             [schnaq.interface.views.discussion.conclusion-card :as card]
+            [schnaq.interface.views.discussion.logic :as logic]
             [schnaq.interface.views.pages :as pages]))
 
 (rf/reg-sub
@@ -17,8 +18,36 @@
   (fn [db _]
     (get-in db [:search :schnaq :current :result] [])))
 
-(defn- search-view
-  []
+(defn- search-info [results search-string empty-search?]
+  [common/move-in :left
+   [:div.panel-white.mb-4
+    [:div.d-inline-block
+     [:h2 (labels :schnaq.search/heading)]
+     [:div.row.mx-0.mt-4.mb-3
+      [:img.dashboard-info-icon-sm {:src (img-path :icon-search)}]
+      [:div.text.display-6.my-auto.mx-3
+       (if empty-search?
+         (labels :schnaq.search/no-input)
+         search-string)]]]
+    [:div.row.m-0
+     [:img.dashboard-info-icon-sm {:src (img-path :icon-posts)}]
+     (if (or empty-search? (empty? results))
+       [:p.mx-3 (labels :schnaq.search/new-search-title)]
+       [:p.mx-3 (str (count results) " " (labels :schnaq.search/results))])]]])
+
+(defn search-results [results]
+  (let [sort-method @(rf/subscribe [:discussion.statements/sort-method])
+        key-fn (case sort-method
+                :newest :statement/created-at
+                :popular #(logic/calculate-votes % @(rf/subscribe [:local-votes])))
+        sorted-results (sort-by key-fn > results)]
+    [common/move-in :right
+     (for [statement sorted-results]
+       [:div.p-2.w-lg-50.d-inline-block
+        {:key (:db/id statement)}
+        [card/statement-card nil statement]])]))
+
+(defn- search-view []
   (let [search-string @(rf/subscribe [:schnaq.search.current/search-string])
         results @(rf/subscribe [:schnaq.search.current/result])
         page-wrapper (if shared-config/embedded? pages/embeddable-view pages/with-discussion-header)
@@ -28,29 +57,13 @@
      [:div.container-fluid
       [:div.row
        [:div.col-md-6.col-lg-4.py-4.px-0.px-md-3
-        [common/move-in :left
-         [:div.panel-white.mb-4
-          [:div.d-inline-block
-           [:h2 (labels :schnaq.search/heading)]
-           [:div.row.mx-0.mt-4.mb-3
-            [:img.dashboard-info-icon-sm {:src (img-path :icon-search)}]
-            [:div.text.display-6.my-auto.mx-3
-             (if empty-search?
-               (labels :schnaq.search/no-input)
-               search-string)]]]
-          [:div.row.m-0
-           [:img.dashboard-info-icon-sm {:src (img-path :icon-posts)}]
-           (if (or empty-search? (empty? results))
-             [:p.mx-3 (labels :schnaq.search/new-search-title)]
-             [:p.mx-3 (str (count results) " " (labels :schnaq.search/results))])]]]]
+        [search-info results search-string empty-search?]]
        [:div.col-md-6.col-lg-8.py-4.px-0.px-md-3
         [elements/action-view true]
         (when-not empty-search?
-          [common/move-in :right
-           (for [statement results]
-             [:div.p-2.w-lg-50.d-inline-block
-              {:key (:db/id statement)}
-              [card/statement-card nil statement]])])]]]]))
+          [search-results results])]]]]))
 
-(defn view []
+(defn view
+  "Search view"
+  []
   [search-view])
