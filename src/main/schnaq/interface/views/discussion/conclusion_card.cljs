ecbff594232802bd35da89338a0c3b6dddd6966b
Alexander Schneider <alexander@schneider.gg>
2021-08-26 14:19:21 +0200

    Remove unused component

--- src/main/schnaq/interface/views/discussion/conclusion_card.cljs
+++ src/main/schnaq/interface/views/discussion/conclusion_card.cljs
@@ -31,25 +31,6 @@
 
 ;; -----------------------------------------------------------------------------
 
-(defn up-down-vote-breaking
-  "Add panel for up and down votes."
-  [statement]
-  (let [votes @(rf/subscribe [:local-votes])]
-    [:div.float-right
-     [:div.d-flex
-      [:div.px-2
-       {:on-click (fn [e]
-                    (js-wrap/stop-propagation e)
-                    (rf/dispatch [:discussion/toggle-upvote statement]))}
-       [:i.vote-arrow.up-vote {:class (str "m-auto fas " (fa :arrow-up))}]]]
-     [:h6.d-flex.p-2.m-0 (logic/calculate-votes statement votes)]
-     [:div.d-flex
-      [:div.px-2
-       {:on-click (fn [e]
-                    (js-wrap/stop-propagation e)
-                    (rf/dispatch [:discussion/toggle-downvote statement]))}
-       [:i.vote-arrow.down-vote.align-bottom {:class (str "m-auto fas " (fa :arrow-down))}]]]]))
-
 (defn up-down-vote
   "Add inline panel for up and down votes."
   [statement]
