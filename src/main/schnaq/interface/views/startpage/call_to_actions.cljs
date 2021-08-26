7a3d12e4144bf3a0cb5185860f79a28b063ab922
Mike Birkhoff <michael.birkhoff@hhu.de>
2021-08-26 16:53:06 +0200

    Update deps and delete unused functions

--- src/main/schnaq/interface/views/startpage/call_to_actions.cljs
+++ src/main/schnaq/interface/views/startpage/call_to_actions.cljs
@@ -1,15 +1,6 @@
 (ns schnaq.interface.views.startpage.call-to-actions
   (:require [re-frame.core :as rf]
-            [schnaq.interface.text.display-data :refer [fa labels img-path video]]))
-
-(defn- _header-animation
-  "Display header animation video."
-  []
-  [:section.animation-background.mb-3
-   [:img.taskbar-background {:src (img-path :how-to/taskbar)}]
-   [:video.w-100.video-scalable-with-shadow-and-border {:auto-play true :loop true :muted true :plays-inline true}
-    [:source {:src (video :how-to.discussion/webm) :type "video/webm"}]
-    [:source {:src (video :how-to.discussion/mp4) :type "video/mp4"}]]])
+            [schnaq.interface.text.display-data :refer [fa labels img-path]]))
 
 (defn- header-screenshot
   "Display header screenshot of an example discussion."
@@ -36,26 +27,6 @@
    [:img.social-proof-icon
     {:src (img-path :schnaqqifant/white)}]
    (labels :startpage.social-proof/numbers)])
-
-(defn- bullet-point
-  "Display a bullet-point with a leading icon.
-  _call-to-action.scss contains the related styling."
-  [icon-label desc-label]
-  [:div.row.py-2
-   [:div.col-2.col-lg-1.my-auto
-    [:i.icon-points-icon {:class (str "fas fa-2x " (fa icon-label))}]]
-   [:div.col-10.col-lg-11
-    [:span.icon-points-text (labels desc-label)]]])
-
-(defn- _intro-bullets
-  "The teaser bullets for above the fold."
-  []
-  [:<>
-   [bullet-point :clipboard :feature/organization]
-   [bullet-point :comment :feature/inputs]
-   [bullet-point :puzzle :feature/integration]
-   [bullet-point :user/group :feature/equality]
-   [bullet-point :shield :feature/datenschutz]])
 
 (defn features-call-to-action
   "Displays a list of features with a call-to-action button to start a schnaq"
