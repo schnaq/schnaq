438fafdf0e17acb79c113902ff8f40ab3a83365e
Alexander Schneider <alexander@schneider.gg>
2021-08-26 13:45:24 +0200

    Mark labels that are already set

--- src/main/schnaq/interface/views/discussion/labels.cljs
+++ src/main/schnaq/interface/views/discussion/labels.cljs
@@ -5,28 +5,25 @@
             [schnaq.config.shared :as shared-config]
             [schnaq.interface.text.display-data :refer [fa]]
             [schnaq.interface.utils.js-wrapper :as js-wrap]
-            [schnaq.interface.utils.toolbelt :as tools]
             [schnaq.interface.views.modal :as modal]))
 
 (defn build-label
   "Takes a label and builds the neccesary html."
-  [label]
+  [label set?]
   ;; TODO add hover animation and pointer
-  (case label
-    ":comment"
-    [:span.badge.badge-pill.badge-primary.px-4 [:i {:class "m-auto fas fa-comment"}]]
-    ":arrow-right"
-    [:span.badge.badge-pill.badge-purple.px-4 [:i {:class "m-auto fas fa-arrow-right"}]]
-    ":calendar-alt"
-    [:span.badge.badge-pill.badge-info.px-4 [:i {:class "m-auto fas fa-calendar-alt"}]]
-    ":check"
-    [:span.badge.badge-pill.badge-success.px-4 [:i {:class "m-auto fas fa-check"}]]
-    ":ghost"
-    [:span.badge.badge-pill.badge-dark.px-4 [:i {:class "m-auto fas fa-ghost"}]]
-    ":question"
-    [:span.badge.badge-pill.badge-warning.px-4 [:i {:class "m-auto fas fa-question"}]]
-    ":times"
-    [:span.badge.badge-pill.badge-danger.px-4 [:i {:class "m-auto fas fa-times"}]]))
+  (let [[badge-color icon-class]
+        (case label
+          ":comment" ["badge-primary" "fa-comment"]
+          ":arrow-right" ["badge-purple" "fa-arrow-right"]
+          ":calendar-alt" ["badge-info" "fa-calendar-alt"]
+          ":check" ["badge-success" "fa-check"]
+          ":ghost" ["badge-dark" "fa-ghost"]
+          ":question" ["badge-warning" "fa-question"]
+          ":times" ["badge-danger" "fa-times"])
+        extra-class (if set? (str badge-color " label-set") badge-color)]
+    [:span.badge.badge-pill.px-4
+     {:class extra-class}
+     [:i {:class (str "m-auto fas " icon-class)}]]))
 
 (defn- anonymous-labels-modal
   "Explain to anonymous users that they need to log in to set and remove labels."
@@ -35,14 +32,15 @@
                          :discussion.anonymous-labels.modal/explain
                          :discussion.anonymous-labels.modal/cta))
 
-(defn test-component
+(defn build-labels
   [statement]
-  [:div
+  ;; TODO add on-click method
+  [:<>
    (for [label shared-config/allowed-labels]
      [:span.mr-3
       {:key (str "label-" (:db/id statement) "-" label)
        :on-click #(js/alert (str "Clicked on" label))}
-      [build-label label]])])
+      [build-label label ((set (:statement/labels statement)) label)]])])
 
 (defn edit-labels-button
   "Give the registered user the ability to add or remove labels to a statement."
@@ -53,7 +51,7 @@
       [:> Tooltip
        {:animation "scale"
         :arrow true
-        :html (r/as-element [test-component statement])
+        :html (r/as-element [build-labels statement])
         :interactive true
         :offset 5
         :position "bottom"
