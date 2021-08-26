27072c1c5426262ade78122da86dbe2e9196286f
Alexander Schneider <alexander@schneider.gg>
2021-08-26 16:49:26 +0200

    Remove tooltip from admin-menu

--- src/main/schnaq/interface/utils/js_wrapper.cljs
+++ src/main/schnaq/interface/utils/js_wrapper.cljs
@@ -1,8 +1,10 @@
 (ns schnaq.interface.utils.js-wrapper
   (:require ["jquery" :as jquery]
+            ["react-tippy" :refer [Tooltip]]
             [ghostwheel.core :refer [>defn]]
             [goog.dom :as gdom]
-            [goog.dom.dataset :as dataset]))
+            [goog.dom.dataset :as dataset]
+            [reagent.core :as r]))
 
 
 (>defn $
@@ -55,11 +57,33 @@
   ([selector option]
    (.tooltip (jquery selector) option)))
 
-(defn popover
-  ([selector]
-   (popover selector "enable"))
-  ([selector option]
-   (.popover (jquery selector) option)))
+(defn TippyHtml
+  [component content options]
+  [:> Tooltip
+   (merge
+     {:animation "scale"
+      :arrow true
+      :html (r/as-element component)
+      :interactive true
+      :offset 5
+      :position "bottom"
+      :theme "light"
+      :trigger "click"}
+     options)
+   content])
+
+(defn TippyText
+  [title content options]
+  [:> Tooltip
+   (merge
+     {:animation "scale"
+      :arrow true
+      :offset 5
+      :position "bottom"
+      :theme "light"
+      :title title}
+     options)
+   content])
 
 (>defn replace-url
   "Replaces the current URL in the users window and acts as a redirect."
