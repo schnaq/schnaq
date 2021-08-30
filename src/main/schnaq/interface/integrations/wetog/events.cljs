87dee56ad0f63a68ac0e8e96929468c293686425
Christian Meter <cmeter@googlemail.com>
2021-08-30 15:48:37 +0200

    Load JWT from frontend

--- src/main/schnaq/interface/integrations/wetog/events.cljs
+++ src/main/schnaq/interface/integrations/wetog/events.cljs
@@ -5,8 +5,10 @@
 (rf/reg-event-fx
   :wetog/initialize-from-data
   (fn [{:keys [db]} _]
-    (let [share-hash (jsw/data-attribute "schnaq-integration" "shareHash")
-          display-name (jsw/data-attribute "schnaq-integration" "displayName")]
+    (let [integration-div "schnaq-integration"
+          share-hash (jsw/data-attribute integration-div "shareHash")
+          display-name (jsw/data-attribute integration-div "displayName")
+          jwt (jsw/data-attribute integration-div "jwt")]
       {:db (-> db
                (assoc-in [:user :names :display] display-name)
                (assoc-in [:schnaq :selected :discussion/share-hash] share-hash))
@@ -17,7 +19,6 @@
   (fn [_ _]
     {:fx [[:dispatch [:how-to-visibility/from-localstorage-to-app-db]]
           ;; TODO Use the JWT from wetog here instead of keycoak
-          [:dispatch [:schnaq.discussion-secrets/load-from-localstorage]]
           [:dispatch [:visited.save-statement-nums/store-hashes-from-localstorage]]
           [:dispatch [:visited.save-statement-ids/store-hashes-from-localstorage]]
           [:dispatch [:schnaq.discussion-secrets/load-from-localstorage]]]}))
