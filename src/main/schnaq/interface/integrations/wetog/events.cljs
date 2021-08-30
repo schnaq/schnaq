9b515c650e78742f9a14c81c737346a9ef59cefe
Christian Meter <cmeter@googlemail.com>
2021-08-30 22:57:03 +0200

    Allow external jwt token for authentication

--- src/main/schnaq/interface/integrations/wetog/events.cljs
+++ src/main/schnaq/interface/integrations/wetog/events.cljs
@@ -8,17 +8,20 @@
     (let [integration-div "schnaq-integration"
           share-hash (jsw/data-attribute integration-div "shareHash")
           display-name (jsw/data-attribute integration-div "displayName")
-          jwt (jsw/data-attribute integration-div "jwt")]
+          jwt (jsw/data-attribute integration-div "jwt")
+          authenticated? (string? (and jwt (not-empty jwt)))]
       {:db (-> db
                (assoc-in [:user :names :display] display-name)
+               (assoc-in [:user :jwt] jwt)
                (assoc-in [:schnaq :selected :discussion/share-hash] share-hash))
-       :fx [[:dispatch [:schnaq/load-by-share-hash share-hash]]]})))
+       :fx [[:dispatch [:user/authenticated! authenticated?]]
+            [:dispatch [:user/register authenticated?]]
+            [:dispatch [:schnaq/load-by-share-hash share-hash]]]})))
 
 (rf/reg-event-fx
   :initialize/wetog-integration
   (fn [_ _]
     {:fx [[:dispatch [:how-to-visibility/from-localstorage-to-app-db]]
-          ;; TODO Use the JWT from wetog here instead of keycoak
           [:dispatch [:visited.save-statement-nums/store-hashes-from-localstorage]]
           [:dispatch [:visited.save-statement-ids/store-hashes-from-localstorage]]
           [:dispatch [:schnaq.discussion-secrets/load-from-localstorage]]]}))
