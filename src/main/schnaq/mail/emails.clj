18284252c9581a8fc6c4ea98bc1e10932f8334e3
Mike Birkhoff <michael.birkhoff@hhu.de>
2021-08-31 16:50:50 +0200

    Remove html-mail method and use cutom-body instead for all mails
    Refactor welcome and lead-magnet mail to be in the template namespace

--- src/main/schnaq/mail/emails.clj
+++ src/main/schnaq/mail/emails.clj
@@ -1,13 +1,11 @@
 (ns schnaq.mail.emails
   (:require [clojure.spec.alpha :as s]
-            [clojure.string :as cstring]
             [ghostwheel.core :refer [>defn >defn- ?]]
             [postal.core :refer [send-message]]
             [schnaq.config :as config]
             [schnaq.mail.template :as template]
             [schnaq.translations :refer [email-templates]]
-            [taoensso.timbre :as log])
-  (:import (java.util UUID)))
+            [taoensso.timbre :as log]))
 
 (def ^:private conn {:host (:sender-host config/email)
                      :ssl true
@@ -74,45 +72,20 @@
   (run! (partial send-mail title content) recipients)
   {:failed-sendings @failed-sendings})
 
-(>defn send-html-mail
-  "Sends a html mail and an alternative text version to any contact. The html-template should be a url or file-path
-  usable by slurp. Title and body are keys for the email-templates map. Format-map is used to substitute things
-  in the text-body and html template. The keys are substituted by their values."
-  [recipient title text-body html-template-path email-type format-map]
-  [string? keyword? keyword? string? any? map? :ret any?]
-  (let [replace-fn #(cstring/replace %1 (first %2) (second %2))]
-    (if (valid-mail recipient)
-      (try
-        (send-message conn {:from (:sender-address config/email)
-                            :to recipient
-                            :subject (email-templates title)
-                            :body [:alternative
-                                   {:type "text/plain; charset=utf-8" :content
-                                    (reduce replace-fn (email-templates text-body) format-map)}
-                                   {:type "text/html; charset=utf-8" :content
-                                    (reduce replace-fn (slurp html-template-path) format-map)}]})
-        (log/info "Sent" email-type "mail to" recipient)
-        :ok
-        (catch Exception exception
-          (log/error "Failed to send" email-type "mail to" recipient)
-          (log/error exception)))
-      (log/error "Recipient's mail address is invalid: " recipient))))
-
 (>defn send-welcome-mail
   "Sends a welcome e-mail to a recipient. The mail template is stored in s3."
   [recipient]
   [string? :ret any?]
-  (send-html-mail recipient :welcome/title :welcome/body
-                  "https://s3.disqtec.com/welcome-mail/welcome_template.html"
-                  "welcome" {}))
+  (send-mail-with-custom-body
+    (email-templates :welcome/title)
+    recipient
+    (template/welcome)))
 
 (>defn send-remote-work-lead-magnet
   "Sends the lead magnet pdf to a recipient. The mail template is stored in s3."
   [recipient]
   [string? :ret any?]
-  (send-html-mail recipient :lead-magnet/title :lead-magnet/body
-                  "https://s3.disqtec.com/email/lead-magnet/dsgvo-check-mail.html"
-                  "lead-magnet remote work"
-                  {"$DOWNLOAD_LINK"
-                   (str "https://s3.disqtec.com/downloads/Datenschutzkonform_arbeiten_schnaq.com.pdf?key="
-                        (.toString (UUID/randomUUID)))}))
+  (send-mail-with-custom-body
+    (email-templates :lead-magnet/title)
+    recipient
+    (template/remote-work-lead-magnet)))
--- src/main/schnaq/mail/template.clj
+++ src/main/schnaq/mail/template.clj
@@ -1,7 +1,8 @@
 (ns schnaq.mail.template
   (:require [clojure.string :as cstring]
-            [schnaq.config :as config]))
-
+            [schnaq.config :as config]
+            [schnaq.translations :refer [email-templates]])
+  (:import (java.util UUID)))
 
 (defn mail
   "Basic html mail template with a schnaq logo and passed heading.
@@ -28,8 +29,6 @@
      {:type "text/html; charset=utf-8" :content
       (reduce replace-fn (slurp config/mail-template) format-map)}]))
 
-
-
 (defn mail-content-left-button-right
   "Additional html content to display content on the left side and a button on the right side"
   [content-title content-subtitle button-text button-link]
@@ -39,4 +38,26 @@
                     "$$$BUTTON-TEXT$$$" button-text
                     "$$$BUTTON-LINK$$$" button-link}]
     (reduce replace-fn (slurp config/mail-content-button-right-template) format-map)))
+
+(defn welcome
+  "Welcome Mail Template"
+  []
+  [:alternative
+   {:type "text/plain; charset=utf-8" :content
+    (email-templates :welcome/body)}
+   {:type "text/html; charset=utf-8" :content
+    (slurp "https://s3.disqtec.com/welcome-mail/welcome_template.html")}])
+
+(defn remote-work-lead-magnet
+  "Template for remote work lead magnet"
+  []
+  (let [replace-fn #(cstring/replace %1 (first %2) (second %2))
+        format-map {"$DOWNLOAD_LINK"
+                    (str "https://s3.disqtec.com/downloads/Datenschutzkonform_arbeiten_schnaq.com.pdf?key="
+                         (.toString (UUID/randomUUID)))}]
+    [:alternative
+     {:type "text/plain; charset=utf-8" :content
+      (reduce replace-fn (email-templates :lead-magnet/body) format-map)}
+     {:type "text/html; charset=utf-8" :content
+      (reduce replace-fn (slurp "https://s3.disqtec.com/email/lead-magnet/dsgvo-check-mail.html") format-map)}]))
 
