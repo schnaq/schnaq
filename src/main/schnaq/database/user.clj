e0f03c46d530445098e3c859426e7b355d538f70
Christian Meter <cmeter@googlemail.com>
2021-09-01 14:58:59 +0200

    Remove string cast

--- src/main/schnaq/database/user.clj
+++ src/main/schnaq/database/user.clj
@@ -160,7 +160,7 @@
   whether the user is newly created and the user entity itself."
   [{:keys [sub email preferred_username given_name family_name groups avatar] :as identity} visited-schnaqs visited-statements]
   [associative? (s/coll-of :db/id) (s/coll-of :db/id) :ret (s/tuple boolean? ::specs/registered-user)]
-  (let [id (str sub)
+  (let [id sub
         existing-user (fast-pull [:user.registered/keycloak-id id] private-user-pattern)
         temp-id (str "new-registered-user-" id)
         new-user {:db/id temp-id
