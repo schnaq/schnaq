9268fcc7163d0a68f60e68eab55e35072fc4e198
Alexander Schneider <alexander@schneider.gg>
2021-09-06 12:39:01 +0200

    Start adding discussion filters

--- src/main/schnaq/interface/views/feed/filters.cljs
+++ src/main/schnaq/interface/views/feed/filters.cljs
@@ -1,13 +1,53 @@
 (ns schnaq.interface.views.feed.filters
-  (:require [re-frame.core :as rf]
-            [schnaq.interface.text.display-data :refer [labels]]
+  (:require [goog.dom :as gdom]
+            [re-frame.core :as rf]
+            [reagent.core :as r]
+            [schnaq.interface.text.display-data :refer [fa labels]]
+            [schnaq.interface.utils.toolbelt :as tools]
             [schnaq.interface.utils.tooltip :as tooltip]))
+
+(defn- add-filters
+  "A small component for adding new filters."
+  []
+  (let [current-selection (r/atom "labels")
+        selected-label (r/atom nil)]
+    (fn []
+      [:section.border-bottom.pb-2.text-left
+       [:div.form-group
+        [:label {:for :add-filter-menu}
+         (labels :filters.label/filter-for)]
+        [:select#add-filter-menu.mr-1.form-control
+         {:on-change #(set-selected-option % current-selection)}
+         [:option {:value :labels} (labels :filters.option.labels/text)]
+         [:option {:value :type} (labels :filters.option.type/text)]
+         [:option {:value :votes} (labels :filters.option.votes/text)]]]
+       (case @current-selection
+         "labels" [label-selections selected-label]
+         "type" [type-selections]
+         "votes" [vote-selections]
+         "")
+       [:button.btn.btn-outline-dark.mr-2
+        {:on-click #(case @current-selection
+                      "labels"
+                      (when @selected-label
+                        (rf/dispatch [:filters.activate/labels
+                                      (tools/get-current-selection (gdom/getElement "filter-labels-selection"))
+                                      @selected-label]))
+                      "type"
+                      (rf/dispatch [:filters.activate/type
+                                    (tools/get-current-selection (gdom/getElement "filter-type-selection"))
+                                    (tools/get-current-selection (gdom/getElement "filter-type-type"))])
+                      "votes"
+                      (rf/dispatch [:filters.activate/votes
+                                    (tools/get-current-selection (gdom/getElement "filter-votes-selection"))
+                                    (.-value (gdom/getElement "filter-votes-number"))]))}
+        [:i {:class (fa :plus)}] " " (labels :filters.add/button)]])))
 
 (defn- default-menu
   "The default filter menu that is shown to the user."
   []
   [:<>
-   #_[add-filter-selection]
+   [add-filters]
    #_[active-filters]
    (when (< 1 (count @(rf/subscribe [:filters/active])))
      [:button.btn.btn-outline-secondary.text-center
