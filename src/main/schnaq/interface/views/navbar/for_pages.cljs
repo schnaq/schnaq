60ba733d3dec455607f9560c16d0115950a1b316
Christian Meter <cmeter@googlemail.com>
2021-09-16 12:29:34 +0200

    Extract language toggle dropdown

--- src/main/schnaq/interface/views/navbar/for_pages.cljs
+++ src/main/schnaq/interface/views/navbar/for_pages.cljs
@@ -30,30 +30,36 @@
      [um/user-handling-menu "btn-link"]]
     [:h1.h3.font-weight-bold.my-auto.d-none.d-md-block.text-dark-highlight title]]])
 
-(defn navbar-user []
+(defn language-dropdown []
   (let [current-language @(rf/subscribe [:current-language])]
-    [:div.d-flex.flex-row.schnaq-navbar-space.mb-0.mb-md-4.ml-auto.schnaq-navbar.align-items-center.flex-wrap.px-md-3
-     [:div.mx-1
-      [:a.nav-link {:role "button" :href (reitfe/href :routes/privacy)}
-       (labels :router/privacy)]]
-     [:div.mx-1
-      [blog-link]]
-     [:div.mx-1
-      [:div.dropdown.ml-auto
-       [:a#schnaq-dropdown.nav-link.dropdown-toggle
-        {:href "#" :role "button" :data-toggle "dropdown"
-         :aria-haspopup "true" :aria-expanded "false"}
-        [:i {:class (str "fas " (fa :language))}] " " current-language]
-       [:div.dropdown-menu {:aria-labelledby "schnaq-dropdown"}
-        [:button.dropdown-item
-         {:on-click #(language/set-language :en)} "English"]
-        [:button.dropdown-item
-         {:on-click #(language/set-language :de)} "Deutsch"]]]]
-     [:div.mx-1
-      [um/admin-dropdown "btn-outline-secondary"]]
-     [:div.mx-1.d-none.d-md-block
-      [:div.d-flex.flex-row.align-items-center
-       [um/user-handling-menu "btn-link"]]]]))
+    [:<>
+     [:a#schnaq-language-dropdown.nav-link.dropdown-toggle
+      {:href "#" :role "button" :data-toggle "dropdown"
+       :aria-haspopup "true" :aria-expanded "false"}
+      [:i {:class (str "fas " (fa :language))}] " " current-language]
+     [:div.dropdown-menu {:aria-labelledby "schnaq-language-dropdown"}
+      [:button.dropdown-item
+       {:on-click #(language/set-language :de)} "Deutsch"]
+      [:button.dropdown-item
+       {:on-click #(language/set-language :en)} "English"]
+      [:button.dropdown-item
+       {:on-click #(language/set-language :pl)} "Polski"]]]))
+
+(defn navbar-user []
+  [:div.d-flex.flex-row.schnaq-navbar-space.mb-0.mb-md-4.ml-auto.schnaq-navbar.align-items-center.flex-wrap.px-md-3
+   [:div.mx-1
+    [:a.nav-link {:role "button" :href (reitfe/href :routes/privacy)}
+     (labels :router/privacy)]]
+   [:div.mx-1
+    [blog-link]]
+   [:div.mx-1
+    [:div.dropdown.ml-auto
+     [language-dropdown]]]
+   [:div.mx-1
+    [um/admin-dropdown "btn-outline-secondary"]]
+   [:div.mx-1.d-none.d-md-block
+    [:div.d-flex.flex-row.align-items-center
+     [um/user-handling-menu "btn-link"]]]])
 
 (defn navbar
   "Overview header for a discussion."
@@ -65,41 +71,32 @@
 (defn navbar-transparent
   "Navbar definition for the default pages."
   [wrapper-classes]
-  (let [current-language @(rf/subscribe [:current-language])]
-    ;; collapsable navbar
-    [:nav.navbar.navbar-expand-lg.py-3.navbar-transparent.bg-transparent.mb-4
-     ;; logo
-     [:div
-      {:class (if (str/blank? wrapper-classes) "container" wrapper-classes)}
-      [:a.navbar-brand {:href (reitfe/href :routes/startpage)}
-       [:img.d-inline-block.align-middle.mr-2
-        {:src (img-path :logo-white) :width "150" :alt "schnaq logo"}]]
-      ;; hamburger
-      [:button.navbar-toggler
-       {:type "button" :data-toggle "collapse" :data-target "#schnaq-navbar"
-        :aria-controls "schnaq-navbar" :aria-expanded "false" :aria-label "Toggle navigation"
-        :data-html2canvas-ignore true}
-       [:span.navbar-toggler-icon]]
-      ;; menu items
-      [:div#schnaq-navbar.collapse.navbar-collapse
-       [:ul.navbar-nav.ml-auto
-        [:li.nav-item
-         [:a.nav-link
-          {:role "button"
-           :href (reitfe/href :routes.schnaqs/personal)}
-          (labels :nav/schnaqs)]]
-        [blog-link]
-        [:li.nav-item
-         [:a.nav-link {:role "button" :href (reitfe/href :routes/privacy)}
-          (labels :router/privacy)]]
-        [:li.nav-item.dropdown
-         [:a#schnaq-dropdown.nav-link.dropdown-toggle
-          {:href "#" :role "button" :data-toggle "dropdown"
-           :aria-haspopup "true" :aria-expanded "false"} current-language]
-         [:div.dropdown-menu {:aria-labelledby "schnaq-dropdown"}
-          [:button.dropdown-item
-           {:on-click #(language/set-language :en)} "English"]
-          [:button.dropdown-item
-           {:on-click #(language/set-language :de)} "Deutsch"]]]]
-       [um/admin-dropdown "btn-outline-secondary"]
-       [um/user-handling-menu "btn-outline-light btn-transparent"]]]]))
+  [:nav.navbar.navbar-expand-lg.py-3.navbar-transparent.bg-transparent.mb-4
+   ;; logo
+   [:section
+    {:class (if (str/blank? wrapper-classes) "container" wrapper-classes)}
+    [:a.navbar-brand {:href (reitfe/href :routes/startpage)}
+     [:img.d-inline-block.align-middle.mr-2
+      {:src (img-path :logo-white) :width "150" :alt "schnaq logo"}]]
+    ;; hamburger
+    [:button.navbar-toggler
+     {:type "button" :data-toggle "collapse" :data-target "#schnaq-navbar"
+      :aria-controls "schnaq-navbar" :aria-expanded "false" :aria-label "Toggle navigation"
+      :data-html2canvas-ignore true}
+     [:span.navbar-toggler-icon]]
+    ;; menu items
+    [:div#schnaq-navbar.collapse.navbar-collapse
+     [:ul.navbar-nav.ml-auto
+      [:li.nav-item
+       [:a.nav-link
+        {:role "button"
+         :href (reitfe/href :routes.schnaqs/personal)}
+        (labels :nav/schnaqs)]]
+      [blog-link]
+      [:li.nav-item
+       [:a.nav-link {:role "button" :href (reitfe/href :routes/privacy)}
+        (labels :router/privacy)]]
+      [:li.nav-item.dropdown
+       [language-dropdown]]]
+     [um/admin-dropdown "btn-outline-secondary"]
+     [um/user-handling-menu "btn-outline-light btn-transparent"]]]])
