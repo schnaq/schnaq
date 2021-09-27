d79bf4b0ea12854b801c63c4cc4d814b20821af3
Christian Meter <cmeter@googlemail.com>
2021-09-27 12:42:26 +0200

    Fix origin-builder

--- src/test/schnaq/toolbelt_test.clj
+++ src/test/schnaq/toolbelt_test.clj
@@ -1,7 +1,7 @@
 (ns schnaq.toolbelt-test
   (:require [clojure.test :refer [deftest testing are is]]
             [schnaq.test.toolbelt :as test-toolbelt]
-            [schnaq.toolbelt :refer [pull-key-up]]))
+            [schnaq.toolbelt :refer [pull-key-up build-allowed-origin]]))
 
 (deftest pull-key-up-test
   (testing "No specified keys should be present anymore if the keys are correctly chosen."
@@ -17,3 +17,15 @@
   (testing "Generative tests."
     (is (test-toolbelt/check? `pull-key-up))))
 
+(deftest build-allowed-origin-test
+  (testing "Building valid patterns"
+    (are [string url result]
+      (= result (not (nil? (re-find (build-allowed-origin string) url))))
+      "localhost:8700" "http://localhost:8700" true
+      "localhost:8700" "http://localhost:3000" false
+      "schnaq.com" "http://schnaq.com" true
+      "schnaq.com" "https://schnaq.com" true
+      "schnaq.com" "https://api.schnaq.com" true
+      "schnaq.com" "https://api.razupaltuff.schnaq.com" true
+      "schnaq.com" "https://schnaqsel.com" false
+      "penguin.books" "https://penguin.books" true)))
