712958654d82a541396bb923f7814c67f9f8fb31
Christian Meter <cmeter@googlemail.com>
2021-10-07 10:54:39 +0200

    Add invalid-access-code responses

--- src/main/schnaq/api/toolbelt.clj
+++ src/main/schnaq/api/toolbelt.clj
@@ -19,12 +19,21 @@
   {:error error-type
    :message error-message})
 
+(defn- build-not-found-response
+  "Helper function to build unified not-found responses."
+  [error-type error-message]
+  (not-found
+    (build-error-body error-type error-message)))
+
 (def invalid-rights-message "You to not have enough permissions to access this data.")
 (def invalid-share-hash-message "Invalid share-hash.")
+(def invalid-access-code-message "Invalid access code.")
 (def not-found-hash-invalid
   "Return 403 with invalid-share-hash message."
-  (not-found
-    (build-error-body :share-hash/invalid invalid-share-hash-message)))
+  (build-not-found-response :share-hash/invalid invalid-share-hash-message))
+(def access-code-invalid
+  "Return 403 with invalid-access-code message."
+  (build-not-found-response :access-code/invalid invalid-access-code-message))
 
 (defn get-doc
   "Look the docstring up in the meta-description of a function.
