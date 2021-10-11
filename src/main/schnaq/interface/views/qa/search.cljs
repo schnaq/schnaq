d3f3de802ff2ca70b1581bedb347f84f670f1cf6
Alexander Schneider <alexander@schneider.gg>
2021-10-11 15:30:37 +0200

    Add dynamic search to q&a frontend

--- src/main/schnaq/interface/views/qa/search.cljs
+++ src/main/schnaq/interface/views/qa/search.cljs
@@ -1,9 +1,27 @@
 (ns schnaq.interface.views.qa.search
-  (:require [goog.functions :as gfun]
+  (:require [clojure.string :as cstring]
+            [goog.functions :as gfun]
             [oops.core :refer [oget]]
-            [re-frame.core :as rf]))
+            [re-frame.core :as rf]
+            [schnaq.interface.utils.http :as http]
+            [schnaq.interface.utils.toolbelt :as tools]))
 
 (def throttled-search
   (gfun/throttle
     #(rf/dispatch [:schnaq.qa/search (oget % [:?target :value])])
     500))
+
+(rf/reg-event-fx
+  :schnaq.qa/search
+  (fn [{:keys [db]} [_ search-term]]
+    (when-not (cstring/blank? search-term)
+      (let [share-hash (get-in db [:schnaq :selected :discussion/share-hash])]
+        {:fx [(http/xhrio-request db :get "/discussion/statements/search"
+                                  [:schnaq.qa.search/success]
+                                  {:share-hash share-hash
+                                   :search-string search-term
+                                   :display-name (tools/current-display-name db)})]}))))
+(rf/reg-event-db
+  :schnaq.qa.search/success
+  (fn [db [_ {:keys [matching-statements]}]]
+    (assoc-in db [:schnaq :qa :search :results] matching-statements)))
