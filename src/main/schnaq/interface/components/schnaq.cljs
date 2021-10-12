f5ffff25cbfa53b352cce668e9561c7920ae3c4f
Christian Meter <cmeter@googlemail.com>
2021-10-12 10:57:53 +0200

    Add options to component

--- src/main/schnaq/interface/components/schnaq.cljs
+++ src/main/schnaq/interface/components/schnaq.cljs
@@ -4,10 +4,10 @@
 
 (defn access-code
   "Component to add leading zeros and a padding between access code blocks."
-  []
+  [options]
   (let [access-code @(rf/subscribe [:schnaq.selected/access-code])
         code-length shared-config/access-code-length
         padded-access-code (.padStart (str access-code) code-length "0")]
-    [:span
+    [:span options
      (subs padded-access-code 0 (/ code-length 2)) [:span.pl-3]
      (subs padded-access-code (/ code-length 2))]))
