21d8ffef6d1d330f10beefd942134df35b3e47e7
Christian Meter <cmeter@googlemail.com>
2021-10-12 16:59:14 +0200

    Hide labels if in q&a mode

--- src/main/schnaq/interface/components/buttons.cljs
+++ src/main/schnaq/interface/components/buttons.cljs
@@ -1,7 +1,7 @@
 (ns schnaq.interface.components.buttons)
 
 (defn a-big
-  "The default big button as an anchor. By default in primary colors. No target on default."
+  "The default big button as an anchor. By default, in primary colors. No target on default."
   ([content]
    [a-big content "#"])
   ([content target]
