117a4c6368780a2ad49c7b6d99689bc2ed71529a
Christian Meter <cmeter@googlemail.com>
2021-10-15 17:07:47 +0200

    Re-add cache function

--- src/main/schnaq/interface/core.cljs
+++ src/main/schnaq/interface/core.cljs
@@ -96,6 +96,16 @@
   (reagent.dom/render [views/root]
                       (gdom/getElement (if shared-config/embedded? "schnaq-integration" "app"))))
 
+
+(defn ^:dev/after-load clear-cache-and-render!
+  []
+  ;; The `:dev/after-load` metadata causes this function to be called
+  ;; after shadow-cljs hot-reloads code. We force a UI update by clearing
+  ;; the re-frame subscription cache.
+  ;; This function is called implicitly by its annotation.
+  (rf/clear-subscription-cache!)
+  (render))
+
 (defn- say-hello
   "Add some logging to validate and verify the correct environment."
   []
