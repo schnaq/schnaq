92e2d47af85e81265de3620d47fc2e19cbc16179
Christian Meter <cmeter@googlemail.com>
2021-10-18 13:22:49 +0200

    Add small buton

--- src/main/schnaq/interface/components/buttons.cljs
+++ src/main/schnaq/interface/components/buttons.cljs
@@ -1,5 +1,22 @@
 (ns schnaq.interface.components.buttons)
 
+(defn anchor
+  "Create a `a`-Tag styled as a button. By default, in primary colors."
+  ([content]
+   [anchor content "#"])
+  ([content target]
+   [anchor content target "btn-primary"])
+  ([content target classes]
+   [anchor content target classes nil])
+  ([content target classes attrs]
+   [:a.btn
+    (cond->
+      {:href target
+       :role "button"
+       :class classes}
+      attrs (merge attrs))
+    content]))
+
 (defn anchor-big
   "The default big button as an anchor. By default, in primary colors. No target on default."
   ([content]
@@ -9,10 +26,4 @@
   ([content target classes]
    [anchor-big content target classes nil])
   ([content target classes attrs]
-   [:a.btn.btn-lg
-    (cond->
-      {:href target
-       :role "button"
-       :class classes}
-      attrs (merge attrs))
-    content]))
+   [anchor content target (str "btn-lg " classes) attrs]))
