b555e910f946c3d18426a2b3672d73f5e959618c
Christian Meter <cmeter@googlemail.com>
2021-10-27 10:16:40 +0200

    Fix preview statement retrieval
    Co-authored-by: Alexander Schneider <alexander@schnaq.com>

--- src/main/schnaq/interface/views/startpage/preview_statements.cljs
+++ src/main/schnaq/interface/views/startpage/preview_statements.cljs
@@ -10,50 +10,38 @@
   "Displays interactive example statements from the discussion specified in the config.
   When no statements are found displays a static image instead"
   []
-  (if @(rf/subscribe [:example-statement/static-image-fallback?])
-    [:img.img-fluid {:src (img-path :startpage.example/statements)}]
-    (when-let [statement-1 @(rf/subscribe [:example-statement/by-id config/example-statement])]
+  (let [statement @(rf/subscribe [:preview-statement])]
+    (if statement
       [:div.rounded-1.shadow-lg
-       [conclusion-card/answer-card statement-1]])))
+       [conclusion-card/answer-card statement]]
+      [:img.img-fluid {:src (img-path :startpage.example/statements)}])))
 
 (rf/reg-sub
-  :example-statement/by-id
-  (fn [db [_ id]]
-    (when (get-in db [:current-route :path-params :share-hash])
-      (get-in db [:preview-statements id]))))
-
-(rf/reg-sub
-  :example-statement/static-image-fallback?
-  (fn [db _]
-    (get-in db [:preview-statements :image-fallback])))
+  :preview-statement
+  (fn [db]
+    (:preview-statements db)))
 
 (rf/reg-event-fx
-  :discussion.query.example-statement/by-id
+  :discussion.query/preview-statement
   (fn [{:keys [db]} [_ share-hash statement-id api-url]]
     {:fx [(http/xhrio-request
             db :get "/discussion/statement/info"
-            [:preview-statements/by-id-success]
+            [:preview-statements/success]
             {:statement-id statement-id
              :share-hash share-hash
              :display-name (tools/current-display-name db)}
-            [:preview-statements/default]
+            []
             api-url)]}))
 
 (rf/reg-event-fx
-  :preview-statements/by-id-success
+  :preview-statements/success
   (fn [{:keys [db]} [_ {:keys [conclusion]}]]
-    {:db (assoc-in db [:preview-statements (:db/id conclusion)] conclusion)}))
+    {:db (assoc db :preview-statements conclusion)}))
 
 (rf/reg-event-fx
-  :preview-statements/default
-  (fn [{:keys [db]} _]
-    {:db (assoc-in db [:preview-statements :image-fallback] true)}))
-
-(rf/reg-event-fx
-  :load-example-statements
-  (fn [{:keys [db]} _]
+  :load-preview-statements
+  (fn [_ _]
     (let [share-hash config/example-share-hash
           api-url config/example-api-url]
-      {:db (assoc-in db [:current-route :path-params :share-hash] share-hash)
-       :fx [[:dispatch [:schnaq/load-by-share-hash share-hash api-url]]
-            [:dispatch [:discussion.query.example-statement/by-id share-hash config/example-statement api-url]]]})))
+      {:fx [[:dispatch [:schnaq/load-by-share-hash share-hash api-url]]
+            [:dispatch [:discussion.query/preview-statement share-hash config/example-statement api-url]]]})))
