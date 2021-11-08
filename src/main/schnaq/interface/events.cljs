5aed4a53d3151a1c71c6d94d3b03bc04938c3479
Christian Meter <cmeter@googlemail.com>
2021-11-08 11:24:50 +0100

    Re-enable keycloak
    Co-authored-by: Michael Birkhoff <mike@schnaq.com>

--- src/main/schnaq/interface/events.cljs
+++ src/main/schnaq/interface/events.cljs
@@ -46,7 +46,7 @@
     {:fx [[:dispatch [:username/from-localstorage]]
           [:dispatch [:re-frame-10x/hide-on-mobile]]
           [:dispatch [:how-to-visibility/from-localstorage-to-app-db]]
-          #_[:dispatch [:keycloak/init]]
+          [:dispatch [:keycloak/init]]
           [:dispatch [:visited.save-statement-nums/store-hashes-from-localstorage]]
           [:dispatch [:visited.save-statement-ids/store-hashes-from-localstorage]]
           [:dispatch [:schnaqs.save-admin-access/store-hashes-from-localstorage]]
