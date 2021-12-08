d78026eaec7ecf5b4b3b511d6ba60849384a7e60
Alexander Schneider <alexander@schneider.gg>
2021-12-08 15:07:30 +0100

    Use card deck on startpage

--- src/main/schnaq/interface/views/startpage/features.cljs
+++ src/main/schnaq/interface/views/startpage/features.cljs
@@ -39,28 +39,33 @@
     {:src (img-path :startpage.information/meeting)}]])
 
 (defn- feature-box
+  ;; TODO früher auf einzelne Karten umbrechen
   "A Single feature box that can be put in a row. All inputs are keys."
   [title body icon-key]
-  [:div.col-12.col-md-4
-   [:div.panel-white.mx-1.shadow.py-4.text-center
-    [:div.display-6.text-typography.mb-3 (labels title)]
-    [icon icon-key "py-2" {:size "3x"}]
-    [:p.text-justify (labels body)]]])
+  [:div.card.panel-white.text-center
+   [:div.card-body
+    [:div.display-6.text-typography.mb-3.card-title (labels title)]
+    [icon icon-key "mb-3 mx-auto card-text" {:size "3x"}]
+    [:p.text-justify.card-text (labels body)]]])
 
 (defn- how-does-schnaq-work
   "Arguments for getting schnaq in three columns."
   []
   [:div.mt-lg-5
    [:h3.h2.text-center (labels :startpage.feature-box/heading)]
-   [:div.row.py-3
+   [:div.card-deck.py-3
     [feature-box
      :startpage.feature-box.know-how/title
      :startpage.feature-box.know-how/body
      :chalkboard-teacher]
+    ;; This block is used to break the card deck into one card per row for devices smaller than md
+    ;; Without this only sm devices break.
+    [:div.w-100.d-none.d-sm-block.d-lg-none.py-2]
     [feature-box
      :startpage.feature-box.discussion/title
      :startpage.feature-box.discussion/body
      :qrcode]
+    [:div.w-100.d-none.d-sm-block.d-lg-none.py-2]
     [feature-box
      :startpage.feature-box.learnings/title
      :startpage.feature-box.learnings/body
