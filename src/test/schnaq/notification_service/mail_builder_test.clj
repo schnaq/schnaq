a1bb9df554defb6f583ecac8fe9f8c76409eceb8
Christian Meter <cmeter@googlemail.com>
2021-12-14 12:00:31 +0100

    Load users from db via function

--- src/test/schnaq/notification_service/mail_builder_test.clj
+++ src/test/schnaq/notification_service/mail_builder_test.clj
@@ -9,18 +9,18 @@
 (use-fixtures :each schnaq-toolbelt/init-test-delete-db-fixture)
 (use-fixtures :once schnaq-toolbelt/clean-database-fixture)
 
-(def ^:private users
+(defn- load-users []
   (#'notification-service/users-with-changed-discussions
    (main-db/days-ago 1)
    :notification-mail-interval/daily))
 
 (deftest build-new-statements-html-test
   (testing "At least an html-encoded string must be existent."
-    (is (< 10 (count (sut/build-new-statements-html (first users)))))))
+    (is (< 10 (count (sut/build-new-statements-html (first (load-users))))))))
 
 (deftest build-new-statements-plain-test
   (testing "Plain text message is produced on new strings"
-    (is (< 10 (count (sut/build-new-statements-plain (first users)))))))
+    (is (< 10 (count (sut/build-new-statements-plain (first (load-users))))))))
 
 (deftest build-personal-greeting-test
   (testing "display-name should be present in a greeting."
