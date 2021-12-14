22bda9ce5d361ff6cc2d152d5720281b52e7ce6d
Alexander Schneider <alexander@schneider.gg>
2021-12-14 10:49:19 +0100

    Remove i.Gr.

--- src/main/schnaq/interface/translations/english.cljs
+++ src/main/schnaq/interface/translations/english.cljs
@@ -529,7 +529,7 @@
    :privacy.extended.responsible/body
    [:<>
     [:p
-     "schnaq GmbH i.Gr." [:br]
+     "schnaq GmbH" [:br]
      "Speditionsstraße 15A" [:br]
      "40221 Düsseldorf" [:br]
      "Germany" [:br]
--- src/main/schnaq/interface/translations/polish.cljs
+++ src/main/schnaq/interface/translations/polish.cljs
@@ -525,7 +525,7 @@
    :privacy.extended.responsible/body
    [:<>
     [:p
-     "schnaq GmbH i.Gr." [:br]
+     "schnaq GmbH" [:br]
      "Speditionsstraße 15A" [:br]
      "40221 Düsseldorf" [:br]
      "Niemcy" [:br]
