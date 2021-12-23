3009d2ffce9e755e85e5f5137c3412a10d8000df
Alexander Schneider <alexander@schneider.gg>
2021-12-23 11:18:13 +0100

    Add model for surveys

--- src/main/schnaq/database/models.clj
+++ src/main/schnaq/database/models.clj
@@ -289,5 +289,36 @@
    {:db/ident :summary/requester
     :db/valueType :db.type/ref
     :db/cardinality :db.cardinality/one
-    :db/doc "The last requester (registered user) of a summary."}])
+    :db/doc "The last requester (registered user) of a summary."}
+
+   ;; Surveys
+   {:db/ident :survey/title
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "The title of a survey. Usually a question that is posed."}
+   {:db/ident :survey/discussion
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db/doc "The discussion that the survey belongs to."}
+   {:db/ident :survey/type
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db/doc "The type of survey being conducted. i.e. multiple or single choice. Referenced by `:survey.type` entities"}
+   {:db/ident :survey.type/single-choice
+    :db/doc "A typical single-choice survey, where only one vote per person is allowed."}
+   {:db/ident :survey.type/multiple-choice
+    :db/doc "A multiple choice survey, where participants may choose multiple answers."}
+   {:db/ident :survey/options
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/many
+    :db/doc "The options that are possible in this survey. An option knows how many voted for it."}
+   ;; Survey Option
+   {:db/ident :option/value
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "The option description, that the user gets to see. E.g. \"Milk\""}
+   {:db/ident :option/votes
+    :db/valueType :db.type/long
+    :db/cardinality :db.cardinality/one
+    :db/doc "The cummulative number of votes for this option. Must be 0 or positive."}])
 
