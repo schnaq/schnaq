5bbd725cdd0f468204ffb993526ecfe31c787b60
Alexander Schneider <alexander@schneider.gg>
2021-12-23 15:44:30 +0100

    Make new-survey fn public

--- src/main/schnaq/database/survey.clj
+++ src/main/schnaq/database/survey.clj
@@ -1,13 +1,13 @@
 (ns schnaq.database.survey
   (:require [clojure.spec.alpha :as s]
-            [ghostwheel.core :refer [>defn- ?]]
+            [ghostwheel.core :refer [>defn ?]]
             [schnaq.database.main :as db]
             [schnaq.database.patterns :as patterns]
             [schnaq.database.specs :as specs]
             [schnaq.toolbelt :as tools])
   (:import (java.util UUID)))
 
-(>defn- new-survey
+(>defn new-survey
   "Create and return a survey entity. Options must be passed as a collection of strings."
   [title survey-type options discussion-id]
   [:survey/title :survey/type (s/coll-of ::specs/non-blank-string) :db/id :ret (? ::specs/survey)]
