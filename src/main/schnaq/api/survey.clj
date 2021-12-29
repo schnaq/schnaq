1ebfb08406ed7a276f8b40cfc1f132c1bbd9158e
Alexander Schneider <alexander@schneider.gg>
2021-12-29 11:45:55 +0100

    Add api Route to cast a vote

--- src/main/schnaq/api/survey.clj
+++ src/main/schnaq/api/survey.clj
@@ -31,21 +31,44 @@
     (log/info "Requested surveys for share-hash " share-hash)
     (ok {:surveys (survey-db/surveys share-hash)})))
 
+(defn- cast-vote
+  "Lets the user cast a vote"
+  [{:keys [parameters]}]
+  (let [{:keys [share-hash option-id]} (:body parameters)
+        survey-id (get-in parameters [:path :survey-id])]
+    (if (survey-db/vote! option-id survey-id share-hash)
+      (do
+        (log/info "Vote cast for option" option-id)
+        (ok {:voted? true}))
+      (do
+        (log/debug "Something went wrong with a vote with option" option-id
+                   "for survey" survey-id "and share hash" share-hash)
+        (bad-request (at/build-error-body :survey.vote/bad-parameters "This vote was not for a valid option or survey"))))))
+
 (def survey-routes
   [["" {:swagger {:tags ["survey"]}}
-    ["/survey" {:post new-survey
-                :description (at/get-doc #'new-survey)
-                :middleware [:user/authenticated?
-                             :user/beta-tester?
-                             :discussion/valid-credentials?]
-                :name :survey/create
-                :parameters {:body {:title :survey/title
-                                    :survey-type dto/survey-type
-                                    :options (s/coll-of ::specs/non-blank-string)
-                                    :share-hash :discussion/share-hash
-                                    :edit-hash :discussion/edit-hash}}
-                :responses {200 {:body {:new-survey ::dto/survey}}
-                            400 at/response-error-body}}]
+    ["/survey"
+     ["" {:post new-survey
+          :description (at/get-doc #'new-survey)
+          :middleware [:user/authenticated?
+                       :user/beta-tester?
+                       :discussion/valid-credentials?]
+          :name :survey/create
+          :parameters {:body {:title :survey/title
+                              :survey-type dto/survey-type
+                              :options (s/coll-of ::specs/non-blank-string)
+                              :share-hash :discussion/share-hash
+                              :edit-hash :discussion/edit-hash}}
+          :responses {200 {:body {:new-survey ::dto/survey}}
+                      400 at/response-error-body}}]
+     ["/:survey-id/vote" {:put cast-vote
+                          :description (at/get-doc #'cast-vote)
+                          :name :survey/vote!
+                          :parameters {:body {:share-hash :discussion/share-hash
+                                              :option-id :db/id}
+                                       :path {:survey-id :db/id}}
+                          :responses {200 {:body {:voted? boolean?}}
+                                      400 at/response-error-body}}]]
     ["/surveys" {:get surveys-for-discussion
                  :description (at/get-doc #'surveys-for-discussion)
                  :name :surveys/get
