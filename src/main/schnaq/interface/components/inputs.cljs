06d92c695895c074fe96aad4a9c12bcf3d503678
Alexander Schneider <alexander@schneider.gg>
2022-01-11 15:34:25 +0100

    Add interactive skeleton for creating a survey

---
+++ src/main/schnaq/interface/components/inputs.cljs
@@ -0,0 +1,13 @@
+(ns schnaq.interface.components.inputs
+  "A number of schnaq-typical inputs.")
+
+
+(defn text
+  ([placeholder]
+   [text placeholder nil])
+  ([placeholder id]
+   [:input.form-control.my-1
+    (cond->
+      {:type "text"
+       :placeholder placeholder}
+      id (assoc :id id))]))
--- src/main/schnaq/interface/views/discussion/conclusion_card.cljs
+++ src/main/schnaq/interface/views/discussion/conclusion_card.cljs
@@ -3,6 +3,7 @@
             [re-frame.core :as rf]
             [reagent.core :as reagent]
             [reitit.frontend.easy :as reitfe]
+            [schnaq.interface.components.inputs :as inputs]
             [schnaq.config.shared :as shared-config]
             [schnaq.database.specs :as specs]
             [schnaq.interface.components.icons :refer [icon]]
@@ -275,11 +276,62 @@
     [:div.alert.alert-warning (labels :discussion.state/read-only-warning)]
     [input/input-form]))
 
+(defn- survey-option
+  "Returns a single option component. Can contain a button for removal of said component."
+  ([placeholder rank]
+   [inputs/text placeholder (str "survey-option-" rank)]))
+
+(defn- survey-form
+  "Input form to create a survey with multiple options."
+  []
+  (let [option-count (reagent/atom 2)]
+    (fn []
+      [:form.pt-2
+       [:div.form-group
+        ;; TODO labelize
+        [:label {:for :survey-topic} "Survey Topic"]
+        [inputs/text "What is your favorite elephant?" :survey-topic]
+        [:small.form-text.text-muted "Ask a clear question for good results!"]]
+       [:div.form-group
+        [:p "Options"]
+        [survey-option "Pyrrhus" 1]
+        [survey-option "Surus" 2]
+        (for [rank (range 3 (inc @option-count))]
+          (with-meta
+            [survey-option (str "Elephant " rank) rank]
+            {:key (str "survey-option-key-" rank)}))]
+       [:div.text-center.mb-3
+        [:button.btn.btn-dark.mr-2
+         {:type :button
+          :on-click #(swap! option-count inc)}
+         "+ Add Option"]
+        [:button.btn.btn-dark
+         {:type :button
+          :on-click #(swap! option-count dec)}
+         "- Remove Option"]]
+       [:div.form-check.form-check-inline
+        [:input#radio-single-choice.form-check-input
+         {:type "radio"
+          :name :radio-type-choice
+          :value :single
+          :defaultChecked true}]
+        [:label.form-check-label
+         {:for :radio-single-choice} "Single Choice"]]
+       [:div.form-check.form-check-inline
+        [:input#radio-multiple-choice.form-check-input
+         {:type "radio"
+          :name :radio-type-choice
+          :value :multiple}]
+        [:label.form-check-label
+         {:for :radio-multiple-choice} "Multiple Choice"]]
+       [:div.text-center.pt-2
+        [:button.btn.btn-primary.w-75 {:type "submit"} "Create Survey"]]])))
+
 (defn selection-card
   "Dispatch the different input options, e.g. questions, survey or activation.
   The survey and activation feature are not available for free plan users."
   []
-  (let [selected-option (reagent/atom :question)
+  (let [selected-option (reagent/atom :survey)
         on-click #(reset! selected-option %)
         active-class #(when (= @selected-option %) "active")
         iconed-heading (fn [icon-key label]
@@ -317,7 +369,7 @@
               [:span [iconed-heading :magic :schnaq.input-type/activation]]]]]]
           (case @selected-option
             :question [input-form-or-disabled-alert]
-            :survey [:h3 "TODO here be Survey"])]
+            :survey [survey-form])]
          card-fade-in-time]))))
 
 (defn- prepare-statements []
