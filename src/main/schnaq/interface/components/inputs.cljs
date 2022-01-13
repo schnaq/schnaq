aeff8fe4543a1a264e4c2b916cb78fb91baf28f3
Christian Meter <cmeter@googlemail.com>
2022-01-13 14:27:04 +0100

    Change visibility and formatting

--- src/main/schnaq/interface/components/inputs.cljs
+++ src/main/schnaq/interface/components/inputs.cljs
@@ -1,13 +1,11 @@
 (ns schnaq.interface.components.inputs
   "A number of schnaq-typical inputs.")
 
-
 (defn text
   ([placeholder]
    [text placeholder nil])
   ([placeholder id]
    [:input.form-control.my-1
-    (cond->
-      {:type "text"
-       :placeholder placeholder}
+    (cond-> {:type "text"
+             :placeholder placeholder}
       id (assoc :id id))]))
