2def4cac82de41dae69224039a9cf86074132e8c
Christian Meter <cmeter@googlemail.com>
2022-01-17 13:24:40 +0100

    Add common components ns

---
+++ src/main/schnaq/interface/components/common.cljs
@@ -0,0 +1,12 @@
+(ns schnaq.interface.components.common
+  (:require [com.fulcrologic.guardrails.core :refer [>defn =>]]
+            [schnaq.interface.components.icons :refer [icon]]))
+
+(>defn hint-text
+  "Info box to explain functionality.."
+  [text]
+  [string? => :re-frame/component]
+  [:small.text-muted.text-left
+   [:div.d-flex.flex-row
+    [icon :info "my-auto mr-3"]
+    text]])
