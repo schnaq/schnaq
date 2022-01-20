a0527abca61ee7a6482b0afb27f5783ea0e9096f
Christian Meter <cmeter@googlemail.com>
2022-01-20 11:16:10 +0100

    Destructure request inline

--- src/main/schnaq/api/activation.clj
+++ src/main/schnaq/api/activation.clj
@@ -8,24 +8,21 @@
 (defn- start-activation
   "Start activation-feature for a discussion. Only creates a new one if none
   exists."
-  [{:keys [parameters]}]
-  (let [{:keys [share-hash]} (:body parameters)]
-    (log/info "Starting activation for" share-hash)
-    (ok {:activation (activation-db/start-activation! share-hash)})))
+  [{{{:keys [share-hash]} :body} :parameters}]
+  (log/info "Starting activation for" share-hash)
+  (ok {:activation (activation-db/start-activation! share-hash)}))
 
 (defn- increment-activation-counter
   "Increment activation counter."
-  [{:keys [parameters]}]
-  (let [{:keys [share-hash]} (:body parameters)]
-    (log/info "Increment activation counter for" share-hash)
-    (ok {:activation (activation-db/increment-activation! share-hash)})))
+  [{{{:keys [share-hash]} :body} :parameters}]
+  (log/info "Increment activation counter for" share-hash)
+  (ok {:activation (activation-db/increment-activation! share-hash)}))
 
 (defn- reset-activation
   "Reset activation counter."
-  [{:keys [parameters]}]
-  (let [{:keys [share-hash]} (:body parameters)]
-    (log/info "Reset activation counter for" share-hash)
-    (ok {:activation (activation-db/reset-activation! share-hash)})))
+  [{{{:keys [share-hash]} :body} :parameters}]
+  (log/info "Reset activation counter for" share-hash)
+  (ok {:activation (activation-db/reset-activation! share-hash)}))
 
 (def activation-routes
   [["" {:swagger {:tags ["activation"]}}
