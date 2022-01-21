9381c1a4aea9d518c814648c1b934c3a0162f602
Christian Meter <cmeter@googlemail.com>
2022-01-21 09:48:26 +0100

    Remove unused spinner fns

--- src/main/schnaq/interface/views/loading.cljs
+++ src/main/schnaq/interface/views/loading.cljs
@@ -1,7 +1,5 @@
 (ns schnaq.interface.views.loading
-  (:require [re-frame.core :as rf]
-            [reagent.core :as reagent]
-            [schnaq.interface.components.motion :as motion]
+  (:require [schnaq.interface.components.motion :as motion]
             [schnaq.interface.translations :refer [labels]]))
 
 (defn spinner-icon
@@ -9,23 +7,6 @@
   []
   [:div.spinner-border.text-primary {:role "loading-status"}
    [:span.sr-only "Loading..."]])
-
-(defn spinner
-  "Place a spinner on top of a view.
-   Activate spinner with `(rf/dispatch [:spinner/active! true])`
-   Deactivate spinner with `(rf/dispatch [:spinner/active! false])`"
-  []
-  (reagent/create-class
-   {:display-name "Spinner View"
-    :reagent-render
-    (fn [_this]
-      (let [spinner-is-loading? @(rf/subscribe [:spinner/active?])]
-        (when spinner-is-loading?
-          [:div.spinner-position
-           [spinner-icon]])))
-    :component-will-unmount
-    (fn [_this]
-      (rf/dispatch [:spinner/active! false]))}))
 
 (defn loading-placeholder
   "Placeholder to give feedback to user, that data is currently on its way."
@@ -39,15 +20,3 @@
       "🤔 "
       (labels :loading.placeholder/takes-too-long)]
      5]]])
-
-;; -----------------------------------------------------------------------------
-
-(rf/reg-sub
- :spinner/active?
- (fn [db _]
-   (get-in db [:spinner :active?] false)))
-
-(rf/reg-event-db
- :spinner/active!
- (fn [db [_ toggle]]
-   (assoc-in db [:spinner :active?] toggle)))
