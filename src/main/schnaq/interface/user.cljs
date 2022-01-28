9306a6a3f5b827e0a9cd474d76fa39d91a254032
Christian Meter <cmeter@googlemail.com>
2022-01-28 14:40:12 +0100

    Read notification-interval from api

--- src/main/schnaq/interface/user.cljs
+++ src/main/schnaq/interface/user.cljs
@@ -29,7 +29,7 @@
 (rf/reg-event-fx
  :user.register/success
  (fn [{:keys [db]} [_ {:keys [registered-user updated-statements? updated-schnaqs?]}]]
-   (let [{:user.registered/keys [display-name first-name last-name email profile-picture visited-schnaqs keycloak-id]} registered-user
+   (let [{:user.registered/keys [display-name first-name last-name email profile-picture visited-schnaqs keycloak-id notification-mail-interval]} registered-user
          subscription-type (:user.registered.subscription/type registered-user)
          current-route (get-in db [:current-route :data :name])
          share-hash (get-in db [:schnaq :selected :discussion/share-hash])
@@ -40,6 +40,7 @@
               (assoc-in [:user :id] (:db/id registered-user))
               (assoc-in [:user :keycloak-id] keycloak-id)
               (assoc-in [:user :profile-picture :display] profile-picture)
+              (cond-> notification-mail-interval (assoc-in [:user :notification-mail-interval] notification-mail-interval))
               (cond-> first-name (assoc-in [:user :names :first] first-name))
               (cond-> last-name (assoc-in [:user :names :last] last-name))
               (cond-> subscription-type (assoc-in [:user :subscription :type] subscription-type))
