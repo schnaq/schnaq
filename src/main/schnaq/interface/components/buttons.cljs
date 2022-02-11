01276bc57b0a7287329313b34d1809b6bb7e24c0
Christian Meter <cmeter@googlemail.com>
2022-02-11 17:03:24 +0100

    Fix button

--- src/main/schnaq/interface/components/buttons.cljs
+++ src/main/schnaq/interface/components/buttons.cljs
@@ -31,7 +31,7 @@
 (defn button
   "Create a `button`-Tag styled. By default, styled in primary colors."
   ([content]
-   [button content "#"])
+   [button content (constantly "#")])
   ([content on-click]
    [button content on-click "btn-primary"])
   ([content on-click classes]
