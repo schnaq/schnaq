9403a408401f85e65d42b166a4a3d2ef5200a148
Alexander Schneider <alexander@schneider.gg>
2022-02-15 11:41:35 +0100

    Fix test

--- src/test/schnaq/interface/navigation_test.cljs
+++ src/test/schnaq/interface/navigation_test.cljs
@@ -5,9 +5,9 @@
 (deftest replace-language-in-path-test
   (let [replace-language-in-path @#'navigation/replace-language-in-path]
     (testing "Passing no element should clean the path of a language prefix."
-      (is (= "/test/clean" (replace-language-in-path "/en/test/clean")))
-      (is (= "/ente/clean" (replace-language-in-path "/ente/clean"))))
+      (is (= "/test/clean" (replace-language-in-path "https://schnaq.com/en/test/clean")))
+      (is (= "/ente/clean" (replace-language-in-path "https://schnaq.com/ente/clean"))))
     (testing "Passing a locale should add it or replace the current one."
-      (is (= "/de/test/clean" (replace-language-in-path "/en/test/clean" :de)))
-      (is (= "/de/tu/test/clean" (replace-language-in-path "/tu/test/clean" :de)))
-      (is (= "/es/test/clean" (replace-language-in-path "/de/test/clean" :es))))))
+      (is (= "/de/test/clean" (replace-language-in-path "https://schnaq.com/en/test/clean" :de)))
+      (is (= "/de/tu/test/clean" (replace-language-in-path "https://schnaq.com/tu/test/clean" :de)))
+      (is (= "/es/test/clean" (replace-language-in-path "https://schnaq.com/de/test/clean" :es))))))
