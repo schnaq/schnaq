f4af3122a2d955944efef7f0e80a8a4ef2cb1c3f
Michael Birkhoff <michael.birkhoff@hhu.de>
2022-02-15 15:22:30 +0100

    Change toot to torooo

--- src/main/schnaq/interface/translations/english.cljs
+++ src/main/schnaq/interface/translations/english.cljs
@@ -345,8 +345,8 @@
    :schnaq.activation.create/label "Present your participants with an activation input!"
    :schnaq.activation.create/start-button "Start activation"
    :schnaq.activation.create/reset-button "Reset Activation"
-   :schnaq.activation/title "Toots total:"
-   :schnaq.activation/activation-button "Toot"
+   :schnaq.activation/title "Torooo total:"
+   :schnaq.activation/activation-button "Torooo"
 
    ;; schnaq creation
    :schnaq.create/title "Start schnaq"
