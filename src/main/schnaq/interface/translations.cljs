f57aff5d4e9402ea906080678846c9c8e6ab06ac
Alexander Schneider <alexander@schneider.gg>
2022-02-15 16:33:46 +0100

    Remove polish language

--- src/main/schnaq/interface/translations.cljs
+++ src/main/schnaq/interface/translations.cljs
@@ -2,13 +2,11 @@
   (:require [schnaq.interface.config :refer [user-language]]
             [schnaq.interface.translations.english :as english]
             [schnaq.interface.translations.german :as german]
-            [schnaq.interface.translations.polish :as polish]
             [taoensso.tempura :refer [tr]]))
 
 (def ^:private translations
   {:en english/labels
-   :de german/labels
-   :pl polish/labels})
+   :de german/labels})
 
 (defn labels
   "Get a localized resource for the requested key. Returns either a string or a hiccup
