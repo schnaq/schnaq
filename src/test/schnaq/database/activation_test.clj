7db7dcf1e2c508148d051c7d5117890cbf4c3071
Michael Birkhoff <michael.birkhoff@hhu.de>
2022-03-01 12:29:56 +0100

    Refactor activation tests

--- src/test/schnaq/database/activation_test.clj
+++ src/test/schnaq/database/activation_test.clj
@@ -6,33 +6,27 @@
 (use-fixtures :each toolbelt/init-test-delete-db-fixture)
 (use-fixtures :once toolbelt/clean-database-fixture)
 
-(deftest increase-activation-counter-test
-  (let [share-hash "cat-dog-hash"]
-    (testing "Test Activation creation and counter."
-      (let [activation-0 (activation-db/start-activation! share-hash)]
-        (is (not (nil? activation-0)))
-        (is (zero? (:activation/count activation-0)))
-        (testing "Increase Counter by 1."
-          (let [_inc-counter (activation-db/increment-activation! share-hash)
-                activation-1 (activation-db/activation-by-share-hash share-hash)]
-            (is (= (inc (:activation/count activation-0))
-                   (:activation/count activation-1)))))))))
+(deftest start-activation-test
+  (testing "Test Activation creation and counter."
+    (let [activation (activation-db/start-activation! "cat-dog-hash")]
+      (is (not (nil? activation)))
+      (is (zero? (:activation/count activation))))))
 
-(deftest increase-and-reset-activation-counter-test
-  (let [share-hash "cat-dog-hash"]
-    (testing "Test increase and reset."
-      (let [activation-0 (activation-db/start-activation! share-hash)
-            max-inc 30]
-        (doseq [x (range 1 (inc max-inc))
-                :let [_ (activation-db/increment-activation! share-hash)
-                      activation-inc (activation-db/activation-by-share-hash share-hash)]]
-          (testing (str "Increase counter by: " x)
-            (is (= (+ x (:activation/count activation-0))
-                   (:activation/count activation-inc)))))
-        (testing "Reset counter."
-          (let [_ (activation-db/reset-activation! share-hash)
-                activation-reset (activation-db/activation-by-share-hash share-hash)]
-            (is (zero? (:activation/count activation-reset)))))))))
+(deftest increase-activation-counter-test
+  (let [share-hash "simple-hash"]
+    (testing "Increase Counter."
+      (let [activation-0 (activation-db/activation-by-share-hash share-hash)
+            _inc-counter (activation-db/increment-activation! share-hash)
+            activation-1 (activation-db/activation-by-share-hash share-hash)]
+        (is (= (inc (:activation/count activation-0))
+               (:activation/count activation-1)))))))
+
+(deftest reset-activation-counter-test
+  (testing "Reset activation counter."
+    (let [share-hash "simple-hash"
+          _ (activation-db/reset-activation! share-hash)
+          activation (activation-db/activation-by-share-hash share-hash)]
+      (is (zero? (:activation/count activation))))))
 
 (deftest delete-activation!-test
   (testing "Delete activation by share-hash."
