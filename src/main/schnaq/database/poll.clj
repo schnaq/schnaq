11c7b6c54c49cd79d67e1d55cf7be0a63c8ce40d
Alexander Schneider <alexander@schneider.gg>
2022-03-15 15:54:03 +0100

    Fix the fix

--- src/main/schnaq/database/poll.clj
+++ src/main/schnaq/database/poll.clj
@@ -33,7 +33,7 @@
       :in $ ?poll-id ?share-hash
       :where [?poll-id :poll/title]
       [?poll-id :poll/discussion ?discussion]
-      [?discussion :discussion/share-hash ?poll-id]]
+      [?discussion :discussion/share-hash ?share-hash]]
     poll-id share-hash)))
 
 (>defn delete-poll!
