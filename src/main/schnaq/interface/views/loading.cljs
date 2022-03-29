1ec969932400a58e000234aaa5f3d91951dedb17
Christian Meter <cmeter@googlemail.com>
2022-03-29 16:31:24 +0200

    Add loading card

--- src/main/schnaq/interface/views/loading.cljs
+++ src/main/schnaq/interface/views/loading.cljs
@@ -1,5 +1,6 @@
 (ns schnaq.interface.views.loading
-  (:require [schnaq.interface.components.motion :as motion]
+  (:require [re-frame.core :as rf]
+            [schnaq.interface.components.motion :as motion]
             [schnaq.interface.translations :refer [labels]]))
 
 (defn spinner-icon
@@ -20,3 +21,26 @@
       "🤔 "
       (labels :loading.placeholder/takes-too-long)]
      5]]])
+
+(defn loading-card
+  "Show a card when loading takes longer time."
+  []
+  [:div.statement-column
+   [motion/fade-in-and-out
+    [:div.statement-card
+     [loading-placeholder]]
+    1]])
+
+;; -----------------------------------------------------------------------------
+
+(rf/reg-event-db
+ :loading/statements
+ (fn [db [_ toggle]]
+   (if toggle
+     (assoc-in db [:loading :statements?] true)
+     (update db :loading dissoc :statements?))))
+
+(rf/reg-sub
+ :loading/statements?
+ (fn [db]
+   (get-in db [:loading :statements?])))
