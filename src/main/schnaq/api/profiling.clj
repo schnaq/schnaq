037f5022c44d41b293987a2ff6c96d476c79ab63
Christian Meter <cmeter@googlemail.com>
2022-03-31 11:19:46 +0200

    Add printing functions

--- src/main/schnaq/api/profiling.clj
+++ src/main/schnaq/api/profiling.clj
@@ -1,15 +1,20 @@
 (ns schnaq.api.profiling
+  "Tools to profile function calls and API requests.
+  Important: When you read from `stats-accumulator`, it resets itself. So you
+  can read the information only once."
   (:require [mount.core :as mount :refer [defstate]]
             [schnaq.api.middlewares :refer [extract-parameter-from-request]]
             [schnaq.config.shared :as shared-config]
             [schnaq.shared-toolbelt :as shared-tools]
+            [schnaq.toolbelt :as toolbelt]
             [taoensso.tufte :as tufte]))
 
 (declare stats-accumulator)
 
 (defstate stats-accumulator
   :start (when-not shared-config/production?
-           (tufte/add-accumulating-handler! {:ns-pattern "*"})))
+           (tufte/add-accumulating-handler! {:ns-pattern "*"}))
+  :stop (constantly nil))
 
 (defn profiling-middleware
   "Add a profiling middleware. Omits all calls to profiler if profiling is not 
@@ -24,3 +29,29 @@
          {:id (extract-parameter-from-request request :share-hash)}
          (tufte/p api-name (handler request))))
       (handler request))))
+
+(defn print-profiling-results
+  "Print profiling results on the console."
+  []
+  (when stats-accumulator
+    (when-let [stats (not-empty @stats-accumulator)]
+      (println (tufte/format-grouped-pstats stats)))))
+
+(defn post-profiling-to-mattermost
+  "Post profiling information to mattermost. Channel must be a slug of a real
+  channel, e.g. `gitlabs-dirty-secrets`"
+  [channel]
+  (when stats-accumulator
+    (when-let [stats (not-empty @stats-accumulator)]
+      (toolbelt/post-in-mattermost!
+       (format "```%n%s%n```"
+               (tufte/format-grouped-pstats stats))
+       channel))))
+
+(comment
+
+  (print-profiling-results)
+
+  (post-profiling-to-mattermost "meters-einode")
+
+  nil)
