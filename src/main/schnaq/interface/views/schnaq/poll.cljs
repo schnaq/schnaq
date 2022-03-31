54634d53fb02a04e58e693e15c3f5fdf1ca6cd2c
Alexander Schneider <alexander@schneider.gg>
2022-03-31 10:31:31 +0200

    Revert ESC Ranking Points System

--- src/main/schnaq/interface/views/schnaq/poll.cljs
+++ src/main/schnaq/interface/views/schnaq/poll.cljs
@@ -149,9 +149,7 @@
                    (rf/dispatch [:schnaq.ranking/cast-vote poll (map second (sort-by first selected-options))]))}
      [ranking-select poll 1]
      (for [voted-rankings-index (keys selected-options)
-           :while (and
-                   (< voted-rankings-index 5)
-                   (< voted-rankings-index (count (:poll/options poll))))]
+           :while (< voted-rankings-index (count (:poll/options poll)))]
        (with-meta
          [ranking-select poll (inc voted-rankings-index)]
          {:key (str poll-id voted-rankings-index)}))
@@ -351,9 +349,7 @@
    (let [poll-id (:db/id poll)
          share-hash (get-in db [:schnaq :selected :discussion/share-hash])
          poll-update-fn #(let [weighted-options
-                               (->> [12 8 5 3 1]
-                                    #_(range (count (:poll/options poll)) 0 -1)
-                                    ;; Digihub extra, put back after event
+                               (->> (range (count (:poll/options poll)) 0 -1)
                                     (interleave rankings)
                                     (apply hash-map))]
                            (if (contains? (set rankings) (:db/id %))
