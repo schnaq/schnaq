e4a8bc4b201e8c319f2c871cbd823d0735fe20e9
Christian Meter <cmeter@googlemail.com>
2022-04-25 16:59:57 +0200

    First draft of lexical

--- src/main/schnaq/interface/components/editor.cljs
+++ src/main/schnaq/interface/components/editor.cljs
@@ -1,21 +1,22 @@
 (ns schnaq.interface.components.editor
-  (:require ["lexical" :refer [$getRoot $getSelection createEditor]]
-            ["@lexical/react/LexicalComposer" :as LexicalComposer]
+  (:require ["@lexical/react/LexicalComposer" :as LexicalComposer]
             ["@lexical/react/LexicalComposerContext" :refer [useLexicalComposerContext]]
             ["@lexical/react/LexicalContentEditable" :as LexicalContentEditable]
-            ["@lexical/react/LexicalHistoryPlugin" :refer [HistoryPlugin]]
             ["@lexical/react/LexicalPlainTextPlugin" :as LexicalPlainTextPlugin]
+            ["@lexical/react/LexicalHistoryPlugin" :refer [HistoryPlugin]]
             ["@lexical/react/LexicalOnChangePlugin" :as LexicalOnChangePlugin]
             ["@lexical/react/LexicalTreeView" :as LexicalTreeView]
+            ["lexical" :refer [$getRoot $getSelection createEditor]]
             ["react" :refer [useEffect]]
             [goog.dom :as gdom]
+            [reagent.core :as r]
             [taoensso.timbre :as log]))
 
 (def theme
-  {:ltr "ltr"
-   :rtl "rtl"
-   :placeholder "editor-placeholder",
-   :paragraph "editor-paragraph"})
+  #js {:ltr "ltr"
+       :rtl "rtl"
+       :placeholder "editor-placeholder",
+       :paragraph "editor-paragraph"})
 
 (defn- on-change [editor-state]
   (let [root ($getRoot)
@@ -50,9 +51,9 @@
      [:> LexicalComposer {:initialConfig initial-config}
       [:div.editor-container
        [:> LexicalPlainTextPlugin
-        #js {:contentEditable [:> LexicalContentEditable #js {}],
-             :placeholder [:div "Enter some text..."]}]
-       [:> LexicalOnChangePlugin {:onChange on-change}]
+        {:contentEditable (r/as-element [:> LexicalContentEditable]) ;;#js {:className "editor-input"}
+         :placeholder (r/as-element [:div "Enter some text..."])}]
+       #_[:> LexicalOnChangePlugin {:onChange on-change}]
        [:> HistoryPlugin {}]
        #_[:> auto-focus-plugin]]]]))
 
