0cb12e3cccf823d23588d83fcd7efcdd64be39a4
Christian Meter <cmeter@googlemail.com>
2022-05-05 14:43:12 +0200

    Remove unused styles, remove lexical remainings

--- src/main/schnaq/interface/components/tiptap.cljs
+++ src/main/schnaq/interface/components/tiptap.cljs
@@ -4,59 +4,36 @@
             ["@tiptap/extension-typography" :as Typography]
             ["@tiptap/react" :refer [EditorContent useEditor]]
             ["@tiptap/starter-kit" :as StarterKit]
-            [reagent.core :as r]
-            [schnaq.interface.components.icons :refer [icon]]
-            [schnaq.interface.utils.tooltip :as tooltip]))
-
-;; <select class= "form-select" aria-label= "Default select example" >
-  ;; <option selected>Open this select menu</option>
-  ;; <option value= "1" >One</option>
-  ;; <option value= "2" >Two</option>
-  ;; <option value= "3" >Three</option>
-;; </select>
-
-(defn- select-heading []
-  (let [selection (r/atom nil)]
-    (fn []
-      [tooltip/html
-       [:<> [:button.btn.btn-sm.btn-outline-dark
-             {:on-click #(reset! selection 1)}
-             "fuu"] "baa"]
-       [:button.toolbar-item
-        [icon :rocket]]
-       nil
-       #_[:select.form-select {:aria-label "Select a heading"}
-          [:option {:value 1} ""]]])))
+            [schnaq.interface.components.icons :refer [icon]]))
 
 (defn MenuBar [{:keys [editor]}]
   (when editor
-    [:section.toolbar
-     [select-heading]
-     [:button.toolbar-item
+    [:section.menubar
+     [:button.menubar-item
       {:on-click #(-> editor .chain .focus .toggleBold .run)}
       [icon :bold]]
-     [:button.toolbar-item
+     [:button.menubar-item
       {:on-click #(-> editor .chain .focus .toggleItalic .run)}
       [icon :italic]]
-     [:button.toolbar-item
+     [:button.menubar-item
       {:on-click #(-> editor .chain .focus .toggleStrike .run)}
       [icon :strikethrough]]
-     [:button.toolbar-item
+     [:button.menubar-item
       {:on-click #(-> editor .chain .focus .toggleBulletList .run)}
       [icon :list]]
-     [:button.toolbar-item
+     [:button.menubar-item
       {:on-click #(-> editor .chain .focus .toggleOrderedList .run)}
       [icon :list-ol]]
-     [:button.toolbar-item
+     [:button.menubar-item
       {:on-click #(-> editor .chain .focus .toggleCodeBlock .run)}
       [icon :code]]
-     [:button.toolbar-item
+     [:button.menubar-item
       {:on-click #(-> editor .chain .focus .toggleBlockquote .run)}
       [icon :quote-right]]
-     [:button.toolbar-item
+     [:button.menubar-item
       {:on-click #(-> editor .chain .focus .undo .run)}
       [icon :undo]]
-     [:button.toolbar-item
+     [:button.menubar-item
       {:on-click #(-> editor .chain .focus .redo .run)}
       [icon :redo]]]))
 
