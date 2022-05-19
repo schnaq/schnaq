9a7eea659a32c12be018398e50565c614bbeae1b
Christian Meter <cmeter@googlemail.com>
2022-05-19 16:38:49 +0200

    Rename functions

--- src/main/schnaq/interface/components/lexical/plugins/markdown.cljs
+++ src/main/schnaq/interface/components/lexical/plugins/markdown.cljs
@@ -2,8 +2,8 @@
   (:require ["@lexical/markdown" :refer [TRANSFORMERS]]
             ["@lexical/react/LexicalMarkdownShortcutPlugin" :as MarkdownShortcut]
             [goog.string :refer [format]]
-            [schnaq.interface.components.lexical.nodes.image :refer [create-image-node
-                                                                     image-node?]]
+            [schnaq.interface.components.lexical.nodes.image :refer [$create-image-node
+                                                                     $image-node?]]
             [schnaq.interface.components.lexical.nodes.video :refer [create-video-node
                                                                      video-node?]]))
 
@@ -13,13 +13,13 @@
 (def ^:private image-transformer
   "Export / import image nodes."
   #js {:export (fn [^ImageNode node, _export-children, _export-format]
-                 (when (image-node? node)
+                 (when ($image-node? node)
                    (format "![%s](%s)" (or (.getAltText node) "") (.getSrc node))))
        :importRegExp markdown-image-import-regex
        :regExp markdown-image-import-regex
        :replace (fn [text-node match]
                   (let [[_ altText src] match
-                        image-node (create-image-node src altText)]
+                        image-node ($create-image-node src altText)]
                     (.replace text-node image-node)))
        :trigger ")"
        :type "text-match"})
