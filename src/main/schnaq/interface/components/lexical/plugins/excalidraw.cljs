ea0974879abf1faec66f1e21002399fcabf45fa6
Christian Meter <cmeter@googlemail.com>
2022-08-05 11:38:24 +0200

    Upload excalidraw drawings to s3

--- src/main/schnaq/interface/components/lexical/plugins/excalidraw.cljs
+++ src/main/schnaq/interface/components/lexical/plugins/excalidraw.cljs
@@ -1,9 +1,12 @@
 (ns schnaq.interface.components.lexical.plugins.excalidraw
   (:require ["lexical" :refer [$getSelection $isRangeSelection
                                COMMAND_PRIORITY_EDITOR createCommand]]
-            [oops.core :refer [ocall]]
+            ["react" :refer [useEffect]]
+            [goog.string :refer [format]]
+            [oops.core :refer [ocall oget]]
             [re-frame.core :as rf]
             [schnaq.interface.components.lexical.nodes.excalidraw :refer [$create-excalidraw-node ExcalidrawNode]]
+            [schnaq.interface.components.lexical.nodes.excalidraw-utils :refer [convert-elements-to-svg]]
             [taoensso.timbre :as log]))
 
 (def INSERT_EXCALIDRAW_COMMAND (createCommand))
@@ -14,10 +17,42 @@
    (if-not (ocall editor "hasNodes" #js [ExcalidrawNode])
      (log/error "ExcalidrawPlugin: ExcalidrawNode not registered on editor")
      (ocall editor "registerCommand" INSERT_EXCALIDRAW_COMMAND
-            (fn []
-              (let [selection ($getSelection)]
-                (when ($isRangeSelection selection)
-                  (let [node ($create-excalidraw-node)]
-                    (.insertNodes selection #js [node])))
-                true))
+            #(let [selection ($getSelection)]
+               (when ($isRangeSelection selection)
+                 (let [node ($create-excalidraw-node)]
+                   (.insertNodes selection #js [node])))
+               true)
             COMMAND_PRIORITY_EDITOR))))
+
+;; -----------------------------------------------------------------------------
+
+(rf/reg-event-fx
+ :excalidraw.elements.store/success
+ (fn [_ [_ editor node {:keys [url]}]]
+   {:fx [[:editor/update! [editor #(.setUrl node url)]]]}))
+
+(rf/reg-event-fx
+ :excalidraw.elements/store
+ (fn [{:keys [db]} [_ editor node svg]]
+   (let [share-hash (get-in db [:schnaq :selected :discussion/share-hash])
+         svg-b64 (.btoa js/window (oget svg :outerHTML))
+         file {:name "drawing.svg"
+               :type "text/plain"
+               :content (format "data:application/octet-stream;base64,%s" svg-b64)}]
+     (rf/dispatch [:file/upload share-hash file :schnaq/media [:excalidraw.elements.store/success editor node] [:ajax.error/as-notification]]))))
+
+(defn excalidraw-changed
+  "Handle a changed excalidraw node."
+  [^LexicalEditor editor]
+  (useEffect
+   (fn []
+     (ocall editor "registerNodeTransform"
+            ExcalidrawNode
+            (fn [^ExcalidrawNode node]
+              (let [data (oget node :__data)]
+                (when-not (or (= data "[]") (.hasUrl node))
+                  (let [elements (.parse js/JSON data)]
+                    (convert-elements-to-svg
+                     elements
+                     #(rf/dispatch [:excalidraw.elements/store editor node %]))))))))
+   #js [editor]))
