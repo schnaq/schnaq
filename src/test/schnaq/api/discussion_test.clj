42a67084b708ba99f05643e8e5936436fd79bd03
Christian Meter <cmeter@googlemail.com>
2022-08-11 17:57:00 +0200

    Change tests to check different users

--- src/test/schnaq/api/discussion_test.clj
+++ src/test/schnaq/api/discussion_test.clj
@@ -12,20 +12,36 @@
 (use-fixtures :each toolbelt/init-test-delete-db-fixture)
 (use-fixtures :once toolbelt/clean-database-fixture)
 
-(deftest add-label-test
+;; -----------------------------------------------------------------------------
+
+(defn- add-label-request [share-hash statement-id token]
+  (-> {:request-method :put :uri (:path (api/route-by-name :api.discussion.statement.label/add))}
+      (assoc :body-params {:label ":check"
+                           :display-name "A. Schneider"
+                           :share-hash share-hash
+                           :statement-id statement-id})
+      toolbelt/add-csrf-header
+      (toolbelt/mock-authorization-header token)
+      test-app
+      :status))
+
+(deftest add-label-no-restriction-test
+  (let [share-hash "cat-dog-hash"
+        statement-id (:db/id (first (discussion-db/starting-statements share-hash)))]
+    (testing "Anonymous and registered users are allowed to add labels."
+      (is (= 200 (add-label-request share-hash statement-id nil)))
+      (is (= 200 (add-label-request share-hash statement-id toolbelt/token-schnaqqifant-user)))
+      (is (= 200 (add-label-request share-hash statement-id toolbelt/token-kangaroo-normal-user)))
+      (is (= 200 (add-label-request share-hash statement-id toolbelt/token-n2o-admin))))))
+
+(deftest add-label-mods-only-test
   (let [share-hash "cat-dog-hash"
         statement-id (:db/id (first (discussion-db/starting-statements share-hash)))
-        request #(-> {:request-method :put :uri "/discussion/statement/label/add"}
-                     (assoc :body-params {:label ":check"
-                                          :display-name "A. Schneider"
-                                          :share-hash share-hash
-                                          :statement-id statement-id})
-                     toolbelt/add-csrf-header
-                     (toolbelt/mock-authorization-header %))]
-    (testing "Only request with valid role shall be accepted."
-      @(discussion-db/mods-mark-only! share-hash true)
-      (is (= 200 (-> toolbelt/token-schnaqqifant-user request test-app :status)))
-      (is (= 403 (-> toolbelt/token-wegi-no-beta-user request test-app :status))))))
+        _ @(discussion-db/mods-mark-only! share-hash true)]
+    (testing "Only moderators are allowed to set labels."
+      (is (= 403 (add-label-request share-hash statement-id nil)))
+      (is (= 403 (add-label-request share-hash statement-id toolbelt/token-kangaroo-normal-user)))
+      (is (= 200 (add-label-request share-hash statement-id toolbelt/token-schnaqqifant-user))))))
 
 ;; -----------------------------------------------------------------------------
 
