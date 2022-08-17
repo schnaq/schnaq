90a226e12a3428c829562cfc8b9c0fee7615ca4a
Christian Meter <cmeter@googlemail.com>
2022-08-17 12:25:21 +0200

    Extract keycloak admin functions

---
+++ src/main/schnaq/keycloak.clj
@@ -0,0 +1,17 @@
+(ns schnaq.keycloak
+  (:require [com.fulcrologic.guardrails.core :refer [>defn =>]]
+            [keycloak.admin :as kc-admin]
+            [schnaq.config.keycloak :refer [kc-client realm]]))
+
+(>defn get-group-id
+  "Return the internal ID of the group in Keycloak."
+  [group]
+  [:hub/keycloak-name => number?]
+  (kc-admin/get-group-id kc-client realm group))
+
+(>defn add-user-to-group!
+  "Add user to a keycloak group. Returns an object  
+  `org.keycloak.representations.idm.GroupRepresentation`"
+  [group-id new-user-keycloak-id]
+  [number? :user.registered/keycloak-id => any?]
+  (kc-admin/add-user-to-group! kc-client realm group-id new-user-keycloak-id))
