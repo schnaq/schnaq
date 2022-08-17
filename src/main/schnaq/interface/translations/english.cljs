7a53647360587cf02bd15b283c5cd7d5f7f83393
Christian Meter <cmeter@googlemail.com>
2022-08-17 10:55:20 +0200

    Remove role management from keycloak

--- src/main/schnaq/interface/translations/english.cljs
+++ src/main/schnaq/interface/translations/english.cljs
@@ -625,14 +625,10 @@
    :admin.center.delete.user.identity/button "Delete identity"
 
    :admin.center.user/headline "User Management"
-   :admin.center.user/subheadline "First load a user to start editing."
+   :admin.center.user/subheadline "First load a user to start editing. Role management takes place via Keycloak."
    :admin.center.user.load/button "Load User"
    :admin.center.user.save/button "Save User"
    :admin.center.user.delete/confirmation "Do you really want to remove this attribute?"
-   :admin.center.user.role.delete/label "Remove role from user"
-   :admin.center.user.role.delete/button "Remove Role"
-   :admin.center.user.role.add/label "Add role to user"
-   :admin.center.user.role.add/button "Add role"
 
    :badges.filters/label "Display"
    :badges/sort "Sort the posts"
