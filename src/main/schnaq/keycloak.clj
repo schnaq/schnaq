467c67b1897f201dcbe8936082e4917de2a999ad
Christian Meter <cmeter@googlemail.com>
2022-08-17 12:30:25 +0200

    Use uuid-str spec

--- src/main/schnaq/keycloak.clj
+++ src/main/schnaq/keycloak.clj
@@ -1,17 +1,18 @@
 (ns schnaq.keycloak
-  (:require [com.fulcrologic.guardrails.core :refer [>defn =>]]
+  (:require [com.fulcrologic.guardrails.core :refer [=> >defn]]
             [keycloak.admin :as kc-admin]
-            [schnaq.config.keycloak :refer [kc-client realm]]))
+            [schnaq.config.keycloak :refer [kc-client realm]]
+            [schnaq.database.specs :as specs]))
 
 (>defn get-group-id
   "Return the internal ID of the group in Keycloak."
   [group]
-  [:hub/keycloak-name => number?]
+  [:hub/keycloak-name => ::specs/uuid-str]
   (kc-admin/get-group-id kc-client realm group))
 
 (>defn add-user-to-group!
   "Add user to a keycloak group. Returns an object  
   `org.keycloak.representations.idm.GroupRepresentation`"
   [group-id new-user-keycloak-id]
-  [number? :user.registered/keycloak-id => any?]
+  [::specs/uuid-str :user.registered/keycloak-id => any?]
   (kc-admin/add-user-to-group! kc-client realm group-id new-user-keycloak-id))
