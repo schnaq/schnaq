1fae2e20030b773b477da9b8ef48281dacc542b2
Christian Meter <cmeter@googlemail.com>
2022-08-18 12:39:25 +0200

    Add discussion tour and rf events

--- src/main/schnaq/interface/tour.cljs
+++ src/main/schnaq/interface/tour.cljs
@@ -1,8 +1,8 @@
 (ns schnaq.interface.tour
-  (:require ["react-joyride" :refer [STATUS]]
-            ["react-joyride$default" :as Joyride]
+  (:require ["react-bootstrap" :refer [Button]]
+            ["react-joyride" :refer [STATUS] :default Joyride]
             [oops.core :refer [oget]]
-            [reagent.core :as r]
+            [re-frame.core :as rf]
             [schnaq.interface.components.colors :refer [colors]]))
 
 (def ^:private finished
@@ -12,27 +12,52 @@
 
 (def styles {:options {:primaryColor (:secondary colors)}})
 
-(def ^:private steps
-  [{:target ".info-card"
-    :content "Aktuelles Thema / Aussage"}
-   {:target ".selection-card"
-    :content "Hier kannst du deine eigenen Beiträge auf oberster Ebene platzieren. Siehst du die Leiste über dem Eingabefeld? Dort kannst du deinen Beitrag passend stylen und sogar Zeichnungen, Bilder und Dateien einfügen."}
-   {:target ".statement-card"
-    :content "Das hier ist ein Beitrag. Du kannst darauf direkt reagieren oder Reaktionen auf diesen Beitrag anschauen."}
-   {:target ".schnaq-navbar"
-    :content "Das sind die Optionen und unterschiedlichen Ansichten für deinen schnaq. Dort kommst du auch zu deinem Profil, wenn du dir eins erstellst."}])
+(def ^:private tours
+  {:user []
+   :discussion
+   [{:target ".info-card"
+     :content "Hier findest du immer den Beitrag oder das Thema, um das es hier gerade geht."}
+    {:target ".selection-card"
+     :content "Hier kannst du deine eigenen Beiträge auf oberster Ebene platzieren. Siehst du die Leiste über dem Eingabefeld? Dort kannst du deinen Beitrag passend stylen und sogar Zeichnungen, Bilder und Dateien einfügen."}
+    {:target ".statement-card"
+     :content "Das hier ist ein Beitrag. Du kannst darauf direkt reagieren oder Reaktionen auf diesen Beitrag anschauen."}
+    {:target ".activation-card"
+     :content "In dieser Karte werden Umfragen, Wortwolken oder auch Aktivierungsphrasen angezeigt."}
+    {:target ".schnaq-navbar"
+     :content "Das sind die Optionen und unterschiedlichen Ansichten für deinen schnaq. Dort kommst du auch zu deinem Profil, wenn du dir eins erstellst."}]})
 
 (defn tour []
-  (let [run? (r/atom false)]
-    (fn []
-      (let [callback
-            (fn [data]
-              (let [{:keys [status]} (js->clj data :keywordize-keys true)]
-                (when (= status finished) (reset! run? false))))]
-        [:<>
-         [:> Joyride {:callback callback
-                      :continuous true
-                      :run @run?
-                      :steps steps
-                      :styles styles}]
-         [:button.btn.btn-primary {:on-click #(reset! run? true)} "Start"]]))))
+  (let [steps @(rf/subscribe [:tour/steps])
+        callback
+        (fn [data]
+          (let [{:keys [status]} (js->clj data :keywordize-keys true)]
+            (when (= status finished) (rf/dispatch [:tour/stop]))))]
+    [:<>
+     (when steps
+       [:> Joyride {:callback callback
+                    :continuous true
+                    :run true
+                    :steps steps
+                    :styles styles}])
+     [:> Button {:variant "primary"
+                 :on-click #(rf/dispatch [:tour/run :discussion])} "Start Discussion Tour"]
+     [:> Button {:variant "primary"
+                 :on-click #(rf/dispatch [:tour/run :user])} "Start User Tour"]
+     [:> Button {:variant "primary"
+                 :on-click #(rf/dispatch [:tour/run :feed])} "Start Overview Tour"]]))
+
+(rf/reg-sub
+ :tour/steps
+ (fn [db]
+   (let [tour-type (get-in db [:tour :type])]
+     (get tours tour-type))))
+
+(rf/reg-event-db
+ :tour/run
+ (fn [db [_ tour-type]]
+   (assoc-in db [:tour :type] tour-type)))
+
+(rf/reg-event-db
+ :tour/stop
+ (fn [db]
+   (update db :tour dissoc :type)))
