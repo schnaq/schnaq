7f2dd62f1f382c235f1ca3fde46d8f7534e08d12
Christian Meter <cmeter@googlemail.com>
2022-09-14 17:41:58 +0200

    Add fns to download svgs

--- src/main/schnaq/interface/utils/file_download.cljs
+++ src/main/schnaq/interface/utils/file_download.cljs
@@ -1,4 +1,5 @@
-(ns schnaq.interface.utils.file-download)
+(ns schnaq.interface.utils.file-download
+  (:require [oops.core :refer [oset!]]))
 
 (defn file-blob [data mimetype]
   (js/Blob. [data] {"type" mimetype}))
@@ -27,3 +28,20 @@
 
 (defn export-data [data]
   (download-data data "exported-discussion.txt" "text/plain"))
+
+(defn- trigger-download
+  "Create an anchor, set it's href and click it."
+  [uri filename]
+  (let [a (.createElement js/document "a")]
+    (oset! a :download filename)
+    (oset! a :href uri)
+    (oset! a :target :_blank)
+    (.click a)))
+
+(defn download-svg-node
+  "Download an svg DOM element."
+  [svg filename]
+  (let [data (.serializeToString (new js/XMLSerializer) svg)
+        svgBlob (new js/Blob #js [data] #js {:type "image/svg+xml;charset=utf-8"})
+        url (.createObjectURL js/URL svgBlob)]
+    (trigger-download url filename)))
