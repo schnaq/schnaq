1fab113616300fa0b885486b6cb6d227244b7757
Alexander Schneider <alexander@schneider.gg>
2022-09-14 11:24:28 +0200

    Add api route to create local wordcloud

--- src/main/schnaq/api/wordcloud.clj
+++ src/main/schnaq/api/wordcloud.clj
@@ -2,6 +2,7 @@
   (:require [ring.util.http-response :refer [ok]]
             [schnaq.api.toolbelt :as at]
             [schnaq.database.main :refer [set-activation-focus]]
+            [schnaq.database.specs :as specs]
             [schnaq.database.wordcloud :as wordcloud-db]))
 
 (defn- toggle-wordcloud
@@ -11,6 +12,13 @@
   (let [{:keys [db/id wordcloud/visible?] :as wordcloud} (wordcloud-db/wordcloud-by-share-hash share-hash)]
     (when visible?
       (set-activation-focus [:discussion/share-hash share-hash] id))
+    (ok {:wordcloud wordcloud})))
+
+(defn- create-local-wordcloud
+  "Creates a local, interactive wordcloud, when posted to."
+  [{{{:keys [share-hash title]} :body} :parameters}]
+  (let [wordcloud (wordcloud-db/create-local-wordcloud share-hash title)]
+    (set-activation-focus [:discussion/share-hash share-hash] (:db/id wordcloud))
     (ok {:wordcloud wordcloud})))
 
 (def wordcloud-routes
@@ -24,4 +32,14 @@
                     :parameters {:body {:share-hash :discussion/share-hash
                                         :edit-hash :discussion/edit-hash}}
                     :responses {200 {:body {:wordcloud :discussion/wordcloud}}
-                                400 at/response-error-body}}]]])
+                                400 at/response-error-body}}]
+    ["/local" {:post create-local-wordcloud
+               :description (at/get-doc #'create-local-wordcloud)
+               :middleware [:user/authenticated?
+                            :user/pro?
+                            :discussion/valid-credentials?]
+               :name :wordcloud.local/create
+               :parameters {:body {:share-hash :discussion/share-hash
+                                   :edit-hash :discussion/edit-hash}}
+               :responses {200 {:body {:wordcloud ::specs/wordcloud}}
+                           400 at/response-error-body}}]]])
