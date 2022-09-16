09afa53acf3ba94dcac7b8418d86654f0f095497
Alexander Schneider <alexander@schneider.gg>
2022-09-16 10:24:11 +0200

    Return moderators of a schnaq always

--- src/main/schnaq/database/patterns.clj
+++ src/main/schnaq/database/patterns.clj
@@ -96,6 +96,7 @@
    :discussion/created-at
    [:discussion/activation-focus :xform 'schnaq.database.xforms/pull-up-db-id]
    {:discussion/author public-user}
+   [:discussion/moderators :xform 'schnaq.database.xforms/maps->ids]
    {:discussion/theme theme}
    :discussion/wordcloud
    {[:discussion/mode :xform 'schnaq.database.xforms/pull-up-db-ident] [:db/ident]}
