45dc81c1a839458e008c0d503d4d518618ca322d
Christian Meter <cmeter@googlemail.com>
2022-09-16 12:19:21 +0200

    Add dot

--- src/main/schnaq/interface/translations/german.cljs
+++ src/main/schnaq/interface/translations/german.cljs
@@ -184,7 +184,7 @@
    :schnaq.wordcloud/hide "Wortwolke für schnaq Raum verbergen"
    :schnaq.wordcloud/title "Wortwolke"
    :schnaq.wordcloud/label "Zeige die häufigsten Wörter aus deinem schnaq als Wortwolke an."
-   :schnaq.wordcloud.local.create/heading "Lege eine interaktive Wortwolke an, die nur auf neue Eingaben reagiert"
+   :schnaq.wordcloud.local.create/heading "Lege eine interaktive Wortwolke an, die nur auf neue Eingaben reagiert."
    :schnaq.wordcloud.local.create/label "Frage oder Überschrift"
    :schnaq.wordcloud.local.create/button "Lege Wortwolke an"
    :schnaq.wordcloud.local.add-words/label "Wörter hier einfügen"
