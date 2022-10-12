af4c859bd77769e0b337951b7d36d240bc9a53a4
Christian Meter <cmeter@googlemail.com>
2022-10-12 13:54:51 +0200

    Display wordclouds correctly

--- src/main/schnaq/interface/views/schnaq/wordcloud_card.cljs
+++ src/main/schnaq/interface/views/schnaq/wordcloud_card.cljs
@@ -124,12 +124,11 @@
   "Displays all wordclouds of the current schnaq excluding the one in `exclude`."
   [exclude]
   [(? ::specs/wordcloud) :ret (? (s/coll-of :re-frame/component))]
-  (when exclude
-    (for [wordcloud (remove #(= (:db/id exclude) (:db/id %)) @(rf/subscribe [:schnaq.wordclouds/local]))]
-      [motion/fade-in-and-out
-       [:article
-        {:key (str "wordcloud-card-" (:db/id wordcloud))}
-        [local-wordcloud-card wordcloud]]])))
+  (for [wordcloud (remove #(= (:db/id exclude) (:db/id %)) @(rf/subscribe [:schnaq.wordclouds/local]))]
+    [motion/fade-in-and-out
+     [:article
+      {:key (str "wordcloud-card-" (:db/id wordcloud))}
+      [local-wordcloud-card wordcloud]]]))
 
 ;; -----------------------------------------------------------------------------
 
