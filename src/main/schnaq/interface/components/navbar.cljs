7a6d36c7fbbeef46e9ad02e55cdd54a3257ec7e3
Christian Meter <cmeter@googlemail.com>
2022-10-18 18:25:11 +0200

    Start with new transparent navbar

--- src/main/schnaq/interface/components/navbar.cljs
+++ src/main/schnaq/interface/components/navbar.cljs
@@ -1,5 +1,6 @@
 (ns schnaq.interface.components.navbar
-  (:require ["react-bootstrap" :refer [Button]]
+  (:require ["react-bootstrap/Button" :as Button]
+            ["react-bootstrap/ButtonGroup" :as ButtonGroup]
             ["react-bootstrap/Container" :as Container]
             ["react-bootstrap/Nav" :as Nav]
             ["react-bootstrap/Navbar" :as Navbar]
@@ -12,7 +13,7 @@
             [schnaq.interface.navigation :as navigation]
             [schnaq.interface.translations :refer [labels]]
             [schnaq.interface.utils.toolbelt :as toolbelt]
-            [schnaq.interface.views.discussion.share :refer [share-schnaq-button]]
+            [schnaq.interface.views.discussion.share :refer [share-schnaq-modal]]
             [schnaq.interface.views.navbar.elements :refer [LanguageDropdown
                                                             txt-export-request]]
             [schnaq.interface.views.navbar.user-management :refer [admin-dropdown
@@ -34,11 +35,12 @@
 
 (defn- upgrade-button
   "Show an upgrade button for non-pro users."
-  []
+  [{:keys [vertical?] :as props}]
   (when-not @(rf/subscribe [:user/pro?])
-    [:> NavLink {:bsPrefix "btn btn-secondary"
-                 :on-click #(rf/dispatch [:navigation.redirect/follow {:redirect "https://schnaq.com/pricing"}])}
-     [icon :star "me-1"] (labels :pricing.upgrade-nudge/button)]))
+    [:> NavLink (merge {:bsPrefix "btn btn-outline-secondary"
+                        :on-click #(rf/dispatch [:navigation.redirect/follow {:redirect "https://schnaq.com/pricing"}])}
+                       props)
+     [icon :star (if vertical? "d-block mx-auto" "me-1")] (labels :pricing.upgrade-nudge/button)]))
 
 (defn common-navigation-links
   "Show default navigation links."
@@ -54,6 +56,16 @@
    [:> NavLink {:href "https://schnaq.com/blog/"}
     (labels :nav/blog)]])
 
+(def ^:private discussion-views
+  {:routes.schnaq/start {:icon :icon-cards-dark
+                         :label :discussion.button/text}
+   :routes/graph-view {:icon :icon-graph-dark
+                       :label :graph.button/text}
+   :routes.schnaq/qanda {:icon :icon-qanda-dark
+                         :label :qanda.button/text}
+   :routes.schnaq/dashboard {:icon :icon-summary-dark
+                             :label :summary.link.button/text}})
+
 (defn links-to-discussion-views
   "Toggle between different views in a discussion."
   []
@@ -64,31 +76,76 @@
                                  :src (img-path img-key)}])]
     [:<>
      [:> NavLink {:disabled true} (labels :discussion.navbar/views)]
-     [:> NavLink {:class "ms-3" :href (href :routes.schnaq/start)}
-      [img :icon-cards-dark] (labels :discussion.button/text)]
-     [:> NavLink {:class "ms-3" :href (href :routes/graph-view)}
-      [img :icon-graph-dark] (labels :graph.button/text)]
-     [:> NavLink {:class "ms-3" :href (href :routes.schnaq/qanda)}
-      [img :icon-qanda-dark] (labels :qanda.button/text)]
-     [:> NavLink {:class "ms-3" :href (href :routes.schnaq/dashboard)}
-      [img :icon-summary-dark] (labels :summary.link.button/text)]]))
+     (doall
+      (for [[route {:keys [icon label]}] discussion-views]
+        [:> NavLink {:key (str "discussion-view-element-" route)
+                     :class "ms-3" :href (href route)}
+         [img icon] (labels label)]))]))
+
+(defn active-button? [current-route asked-route]
+  (if (= asked-route :routes.schnaq/start)
+    (or (= current-route asked-route) (= current-route :routes.schnaq.select/statement))
+    (= current-route asked-route)))
+
+(defn discussion-view-group
+  "Switch between different discussion views."
+  [props]
+  (let [share-hash @(rf/subscribe [:schnaq/share-hash])
+        current-route @(rf/subscribe [:navigation/current-route-name])
+        href #(navigation/href % {:share-hash share-hash})
+        img (fn [img-key] [:img {:height 20
+                                 :class "bg-white rounded-1"
+                                 :src (img-path img-key)}])]
+    [:> ButtonGroup (merge {:aria-label "TODO" :size :sm} props)
+     (doall
+      (for [[route {:keys [icon label]}] discussion-views]
+        [:> Button {:key (str "discussion-view-element-" route)
+                    :variant (if (active-button? current-route route) :primary :outline-primary)
+                    :className "clickable"
+                    :href (href route)}
+         [:div [img icon]] [:small (labels label)]]))]))
+
+(defn- stacked-icon
+  "Build a stacked icon."
+  [{:keys [vertical?] :as props} icon-key]
+  [:div.fa-stack.small (if vertical?
+                         (assoc props :className "d-block mx-auto")
+                         props)
+   [icon :square "fa-stack-2x text-white"]
+   [icon icon-key "fa-stack-1x text-dark"]])
+
+(defn download-schnaq-button
+  "Button to download a schnaq."
+  [{:keys [vertical?] :as props}]
+  (let [share-hash @(rf/subscribe [:schnaq/share-hash])]
+    [:> NavLink (merge {:on-click #(txt-export-request share-hash @(rf/subscribe [:schnaq/title]))}
+                       props)
+     [stacked-icon {:vertical? vertical?} :file-download] (labels :discussion.navbar/download)]))
+
+(defn share-schnaq-button
+  "Share schnaq button opening a modal."
+  [{:keys [vertical?] :as props}]
+  [share-schnaq-modal
+   (fn [modal-props]
+     [:> NavLink (merge props modal-props)
+      [stacked-icon {:vertical? vertical?} :share] (labels :discussion.navbar/share)])])
+
+(defn manage-schnaq-button
+  "Button to navigate to schnaq management page."
+  [{:keys [vertical?] :as props}]
+  (let [share-hash @(rf/subscribe [:schnaq/share-hash])]
+    [:> NavLink (merge {:href (navigation/href :routes.schnaq/moderation-center {:share-hash share-hash})}
+                       props)
+     [stacked-icon {:vertical? vertical?} :sliders-h] (labels :schnaq.moderation.edit/administrate-short)]))
 
 (defn schnaq-settings
   "Show the schnaq settings, export and share links."
   []
-  (let [share-hash @(rf/subscribe [:schnaq/share-hash])
-        href #(navigation/href % {:share-hash share-hash})
-        stacked-icon (fn [icon-key] [:div.fa-stack.small
-                                     [icon :square "fa-stack-2x"]
-                                     [icon icon-key "fa-stack-1x text-primary"]])]
-    [:<>
-     [:> NavLink {:disabled true} (labels :discussion.navbar/settings)]
-     [share-schnaq-button (fn [props] [:> NavLink (merge props {:class "ms-2"})
-                                       [stacked-icon :share] (labels :discussion.navbar/share)])]
-     [:> NavLink {:class "ms-2" :on-click #(txt-export-request share-hash @(rf/subscribe [:schnaq/title]))}
-      [stacked-icon :file-download] (labels :discussion.navbar/download)]
-     [:> NavLink {:class "ms-2" :href (href :routes.schnaq/moderation-center)}
-      [stacked-icon :sliders-h] (labels :schnaq.moderation.edit/administrate-short)]]))
+  [:<>
+   [:> NavLink {:disabled true} (labels :discussion.navbar/settings)]
+   [share-schnaq-button {:className "ms-2"}]
+   [download-schnaq-button {:className "ms-2"}]
+   [manage-schnaq-button {:className "ms-2"}]])
 
 (defn- page-title [props]
   [:> NavbarText
@@ -101,7 +158,7 @@
   [:> Navbar {:bg :primary :variant :dark :expand false}
    [:> Container {:fluid true}
     [:> NavbarBrand {:href (toolbelt/current-overview-link)}
-     [schnaqqi-white {:class "img-fluid" :width "50"}]]
+     [schnaqqi-white {:class "img-fluid" :width 50}]]
     [page-title]
     [:> NavbarToggle {:aria-controls "mobile-navbar"}]
     [:> NavbarCollapse {:id "mobile-navbar"}
@@ -114,6 +171,32 @@
          [:div.col-6 [links-to-discussion-views]]
          [:div.col-6 [schnaq-settings]]]
         [common-navigation-links])]]]])
+
+(defn split-navbar []
+  (let [share-hash @(rf/subscribe [:schnaq/share-hash])]
+    [:> Navbar {:bg :transparent :variant :light :expand :lg}
+     [:> Container {:fluid true}
+      [:div.d-flex.align-items-center.panel-white.py-0.ps-0
+       [:> NavbarBrand {:className "p-0" :href (toolbelt/current-overview-link)}
+        [:div.schnaq-logo-container
+         [schnaq-logo-white {:class "img-fluid" :width 150}]]]
+       [page-title]]
+      [:> NavbarToggle {:aria-controls "schnaq-navbar-big"}]
+      [:> NavbarCollapse {:id "schnaq-navbar-big"
+                          :className "justify-content-end"}
+       (when share-hash
+         [:> Nav {:className "panel-white-sm me-2"}
+          [discussion-view-group]])
+       [:> Nav {:className "panel-white-sm"}
+        (when share-hash
+          [:<>
+           [share-schnaq-button {:vertical? true :className "p-0 me-2"}]
+           [download-schnaq-button {:vertical? true :className "p-0 me-2"}]
+           [manage-schnaq-button {:vertical? true :className "p-0 me-2"}]])
+        [LanguageDropdown {:vertical? true :className "p-0 me-2"}]
+        [upgrade-button {:vertical? true}]
+        [admin-dropdown]
+        [user-navlink-dropdown {:vertical? true}]]]]]))
 
 (defn page-navbar []
   [:> Navbar {:bg :primary :variant :dark :expand :lg}
@@ -130,24 +213,6 @@
       [admin-dropdown]
       [user-navlink-dropdown]]]]])
 
-(defn split-navbar []
-  [:> Navbar {:bg :transparent :variant :light :expand :lg}
-   [:> Container {:fluid true}
-    [:div.d-flex.align-items-center.panel-white.py-0.ps-0
-     [:> NavbarBrand {:className "p-0" :href (toolbelt/current-overview-link)}
-      [:div.schnaq-logo-container
-       [schnaq-logo-white {:class "img-fluid" :width 150}]]]
-     [page-title]]
-    [:> NavbarToggle {:aria-controls "schnaq-navbar"}]
-    [:> NavbarCollapse {:id "schnaq-navbar"
-                        :className "justify-content-end"}
-     [:> Nav {:className "panel-white"}
-      [common-navigation-links]
-      [LanguageDropdown]
-      [upgrade-button]
-      [admin-dropdown]
-      [user-navlink-dropdown]]]]])
-
 ;; -----------------------------------------------------------------------------
 
 (defn collapsible-navbar
@@ -156,20 +221,21 @@
   (when-not @(rf/subscribe [:ui/setting :hide-navbar])
     [:<>
      [:div.d-xl-none [mobile-navigation]]
+     #_[split-navbar]
      [:div.d-none.d-xl-block [split-navbar]]
-     #_[:div.d-none.d-xl-block
-        [:nav.navbar.navbar-expand-lg.navbar-light.schnaq-navbar-dynamic-padding
-         {:class navbar-bg-class}
-         [:div.container-fluid
-          [:div.navbar-brand.pt-0 brand-content]
-          [:button.navbar-toggler.mx-2.panel-white
-           {:type "button" :data-bs-toggle "collapse"
-            :data-bs-target (str "#" collapse-content-id)
-            :aria-controls collapse-content-id
-            :aria-expanded "false"
-            :aria-label "Toggle navigation"}
-           [:span.navbar-toggler-icon]]
-          [:div.d-md-none [common-components/theme-logo {:style {:max-width "100px"}}]]
-          [:div.ms-auto.d-none.d-lg-block
-           top-right-content]]]
-        collapsible-content]]))
+     [:div.d-none.d-xl-block
+      [:nav.navbar.navbar-expand-lg.navbar-light.schnaq-navbar-dynamic-padding
+       {:class navbar-bg-class}
+       [:div.container-fluid
+        [:div.navbar-brand.pt-0 brand-content]
+        [:button.navbar-toggler.mx-2.panel-white
+         {:type "button" :data-bs-toggle "collapse"
+          :data-bs-target (str "#" collapse-content-id)
+          :aria-controls collapse-content-id
+          :aria-expanded "false"
+          :aria-label "Toggle navigation"}
+         [:span.navbar-toggler-icon]]
+        [:div.d-md-none [common-components/theme-logo {:style {:max-width "100px"}}]]
+        [:div.ms-auto.d-none.d-lg-block
+         top-right-content]]]
+      collapsible-content]]))
