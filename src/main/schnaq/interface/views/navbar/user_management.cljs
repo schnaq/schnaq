5d4b0f1064ab3540b9e11985266651493c12a5ee
Christian Meter <cmeter@googlemail.com>
2022-10-19 13:35:37 +0200

    Reduce avatar size

--- src/main/schnaq/interface/views/navbar/user_management.cljs
+++ src/main/schnaq/interface/views/navbar/user_management.cljs
@@ -92,7 +92,7 @@
   (let [username @(rf/subscribe [:user/display-name])
         authenticated? @(rf/subscribe [:user/authenticated?])
         pro? @(rf/subscribe [:user/pro?])
-        icon-size 32]
+        icon-size 25]
     [:span props
      (if authenticated?
        [common/avatar
