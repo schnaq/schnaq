35087a4cc1229d3c18469be5a7b3fe252c707086
Christian Meter <cmeter@googlemail.com>
2022-12-14 22:09:40 +0100

    Use react chartjs component

--- src/main/schnaq/interface/views/discussion/pie_chart.cljs
+++ src/main/schnaq/interface/views/discussion/pie_chart.cljs
@@ -1,7 +1,6 @@
 (ns schnaq.interface.views.discussion.pie-chart
-  (:require ["chart.js/auto" :refer [Chart]]
-            [reagent.core :as reagent]
-            [reagent.dom :as rdom]
+  (:require ["chart.js/auto"]
+            ["react-chartjs-2" :refer [Chart]]
             [schnaq.interface.components.colors :refer [colors]]))
 
 (defn create-vote-chart-data
@@ -10,28 +9,12 @@
   (let [up-votes (get statement :statement/upvotes 0)
         down-votes (get statement :statement/downvotes 0)
         neutral-votes (if (and (zero? up-votes) (zero? down-votes)) 1 0)]
-    {:type "doughnut"
-     :options {:events [] :responsive true}
-     :data {:datasets [{:label "Pie Chart Data",
-                        :data [neutral-votes, up-votes, down-votes]
-                        :backgroundColor [(:neutral/medium colors)
-                                          (:positive/default colors)
-                                          (:negative/default colors)]
-                        :hoverOffset 4
-                        :cutout "70%"}]}}))
-
-(defn pie-chart-component
-  [_data]
-  (let [pie-chart (reagent/atom nil)]
-    (reagent/create-class
-     {:display-name "chart-js-component"
-      :component-did-mount
-      (fn [comp]
-        (let [[_ chart-data] (reagent/argv comp)]
-          (reset! pie-chart (Chart. (rdom/dom-node comp) (clj->js chart-data)))))
-      :component-did-update
-      (fn [comp]
-        (let [[_ chart-data] (reagent/argv comp)]
-          (.destroy @pie-chart)
-          (reset! pie-chart (js/Chart. (rdom/dom-node comp) (clj->js chart-data)))))
-      :reagent-render (fn [_data] [:canvas {:style {:margin-top -10}}])})))
+    [:> Chart {:type "doughnut"
+               :options {:events [] :responsive true}
+               :data {:datasets [{:label "Statement votes",
+                                  :data [neutral-votes, up-votes, down-votes]
+                                  :backgroundColor [(:neutral/medium colors)
+                                                    (:positive/default colors)
+                                                    (:negative/default colors)]
+                                  :hoverOffset 4
+                                  :cutout "70%"}]}}]))
