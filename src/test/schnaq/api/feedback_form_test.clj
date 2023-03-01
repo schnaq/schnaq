876f5653ae4526673fd97c72760836d82275d0f1
Alexander Schneider <alexander@schneider.gg>
2023-03-01 15:20:02 +0100

    Fix broken tests

--- src/test/schnaq/api/feedback_form_test.clj
+++ src/test/schnaq/api/feedback_form_test.clj
@@ -24,21 +24,22 @@
         :body-params {:items items
                       :share-hash "cat-dog-hash"}}
        toolbelt/add-csrf-header
-       (toolbelt/mock-authorization-header user-token))))
+       (toolbelt/mock-authorization-header user-token)
+       test-app)))
 
 (deftest create-form-test
   (testing "Creating a valid feedback form."
     (testing "Pro user, that has no moderation rights cannot create items."
-      (is (= 403 (-> toolbelt/token-schnaqqifant-user create-test-form test-app :status))))
+      (is (= 403 (-> toolbelt/token-schnaqqifant-user create-test-form :status))))
     (testing "Adding items as a moderator is okay"
-      (let [response (-> toolbelt/token-n2o-admin create-test-form test-app)]
+      (let [response (-> toolbelt/token-n2o-admin create-test-form)]
         (is (= 200 (:status response)))
         ;; Returns an id
         (is (pos-int? (:feedback-form-id (m/decode-response-body response))))))))
 
 (deftest create-form-test-empty-items
-  (testing "Creating a valid feedback form  wit no items is not allowed."
-    (is (= 400 (-> toolbelt/token-n2o-admin (create-test-form '()) test-app :status)))))
+  (testing "Creating a valid feedback form with no items is not allowed."
+    (is (= 400 (-> toolbelt/token-n2o-admin (create-test-form '()) :status)))))
 
 (defn- answer-feedback
   [answers]
@@ -47,7 +48,8 @@
        :body-params {:answers answers
                      :share-hash "cat-dog-hash"}}
       toolbelt/add-csrf-header
-      (toolbelt/mock-authorization-header toolbelt/token-n2o-admin)))
+      (toolbelt/mock-authorization-header toolbelt/token-n2o-admin)
+      test-app))
 
 (deftest answer-feedback-test
   (testing "Accept all valid answers without problem."
@@ -63,6 +65,6 @@
                       :db/id)
           valid-answer {:feedback.answer/item item-id
                         :feedback.answer/text "Alles!"}]
-      (is (= 200 (:status (test-app (answer-feedback [valid-answer])))))
-      (is (:saved? (m/decode-response-body (test-app (answer-feedback [valid-answer])))))
-      (is (= 400 (:status (test-app (answer-feedback [(assoc valid-answer :feedback.answer/item 123)]))))))))
+      (is (= 200 (:status (answer-feedback [valid-answer]))))
+      (is (:saved? (m/decode-response-body (answer-feedback [valid-answer]))))
+      (is (= 400 (:status (answer-feedback [(assoc valid-answer :feedback.answer/item 123)])))))))
