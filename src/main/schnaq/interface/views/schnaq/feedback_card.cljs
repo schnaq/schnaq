7d9f9b220feb08853a52bf689bf64a1ab326da7b
Mike Birkhoff <michael.birkhoff@hhu.de>
2023-03-24 10:44:36 +0100

    Use react bootstrap components

--- src/main/schnaq/interface/views/schnaq/feedback_card.cljs
+++ src/main/schnaq/interface/views/schnaq/feedback_card.cljs
@@ -1,5 +1,7 @@
 (ns schnaq.interface.views.schnaq.feedback-card
-  (:require [oops.core :refer [oget]]
+  (:require ["react-bootstrap/Button" :as Button]
+            ["react-bootstrap/Form" :as Form]
+            [oops.core :refer [oget oget+]]
             [re-frame.core :as rf]
             [schnaq.interface.components.icons :refer [icon]]
             [schnaq.interface.components.inputs :as inputs]
@@ -7,6 +9,8 @@
             [schnaq.interface.translations :refer [labels]]
             [schnaq.interface.utils.http :as http]
             [schnaq.interface.utils.toolbelt :as tools]))
+
+(def ^:private FormCheck (oget Form :Check))
 
 (defn- feedback-entry [item-count]
   (let [radio-name (keyword (str "feedback-item-type-" item-count))
@@ -16,23 +20,21 @@
     [:div.my-4
      [inputs/floating placeholder (str "feedback-item-label-" item-count) {:required true}]
      [:p.me-3.d-inline (labels :schnaq.feedback.create/which-type)]
-     [:div.form-check.form-check-inline
-      [:input.form-check-input
-       {:id id-text
-        :type "radio"
-        :name radio-name
-        :value :text
-        :defaultChecked true}]
-      [:label.form-check-label
-       {:for (keyword id-text)} (labels :schnaq.feedback.create/text)]]
-     [:div.form-check.form-check-inline
-      [:input.form-check-input
-       {:id id-scale-5
-        :type "radio"
-        :name radio-name
-        :value :scale-five}]
-      [:label.form-check-label
-       {:for (keyword id-scale-5)} (labels :schnaq.feedback.create/rating)]]]))
+     [:> FormCheck
+      {:id id-text
+       :inline true
+       :type "radio"
+       :name radio-name
+       :value :text
+       :defaultChecked true
+       :label (labels :schnaq.feedback.create/text)}]
+     [:> FormCheck
+      {:id id-scale-5
+       :inline true
+       :type "radio"
+       :name radio-name
+       :value :scale-five
+       :label (labels :schnaq.feedback.create/rating)}]]))
 
 (defn- feedback-tab-entries []
   (let [feedback-count @(rf/subscribe [:feedback.create/item-count])]
@@ -45,23 +47,24 @@
 (defn- feedback-tab-add-remove-entry-buttons []
   (let [feedback-count @(rf/subscribe [:feedback.create/item-count])]
     [:<>
-     [:button.btn.btn-dark.me-2
-      {:type :button
-       :on-click #(rf/dispatch [:feedback.create/set-item-count (inc feedback-count)])}
-      [icon :plus] " " (labels :feedback.create/add-button)]
+     [:> Button {:variant "dark"
+                 :className "me-2"
+                 :type :button
+                 :on-click #(rf/dispatch [:feedback.create/set-item-count (inc feedback-count)])}
+      [icon :plus "me-1"] (labels :feedback.create/add-button)]
      (when (> feedback-count 1)
-       [:button.btn.btn-dark.me-2
-        {:type :button
-         :on-click #(rf/dispatch [:feedback.create/set-item-count (dec feedback-count)])}
-        [icon :minus] " " (labels :feedback.create/remove-button)])]))
+       [:> Button {:variant "dark"
+                   :type :button
+                   :on-click #(rf/dispatch [:feedback.create/set-item-count (dec feedback-count)])}
+        [icon :minus "me-1"] (labels :feedback.create/remove-button)])]))
 
 (defn- extract-feedback-from-form
   "Extract information from a feedback form."
   [form ordinal]
-  (let [type (case (oget form (str "feedback-item-type-" ordinal) :value)
+  (let [type (case (oget+ form (str "feedback-item-type-" ordinal) :value)
                "text" :feedback.item.type/text
                "scale-five" :feedback.item.type/scale-five)
-        label (oget form (str "feedback-item-label-" ordinal) :value)]
+        label (oget+ form (str "feedback-item-label-" ordinal) :value)]
     {:feedback.item/label label
      :feedback.item/type type
      :feedback.item/ordinal ordinal}))
@@ -75,19 +78,18 @@
   "Feedback tab menu to create a Feedback form."
   []
   (let [feedback-count @(rf/subscribe [:feedback.create/item-count])]
-    [:form.pt-2
-     {:on-submit (fn [event]
-                   (.preventDefault event)
-                   (let [form (oget event [:target :elements])]
-                     (rf/dispatch [:schnaq.feedback/create (extract-all-feedback-from-form form feedback-count)])
-                     #_(rf/dispatch [:form/should-clear form])))}
+    [:> Form {:on-submit (fn [event]
+                           (.preventDefault event)
+                           (let [form (oget event [:target :elements])]
+                             (rf/dispatch [:schnaq.feedback/create (extract-all-feedback-from-form form feedback-count)])
+                             #_(rf/dispatch [:form/should-clear form])))}
      [feedback-tab-entries]
-     [:div.text-center.mb-3
-      [feedback-tab-add-remove-entry-buttons]]
-     [:div.text-center.pt-2
-      [:button.btn.btn-primary.w-75
-       {:type "submit"
-        :on-click #(matomo/track-event "Active User" "Action" "Create FeedbackForm")}
+     [:div.text-center
+      [feedback-tab-add-remove-entry-buttons]
+      [:> Button {:variant "primary"
+                  :className "w-75 mt-3 mx-auto d-block"
+                  :type :submit
+                  :on-click #(matomo/track-event "Active User" "Action" "Create FeedbackForm")}
        (labels :feedback.create/submit-button)]]]))
 
 (rf/reg-sub
