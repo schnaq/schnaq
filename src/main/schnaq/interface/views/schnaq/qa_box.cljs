cf83bdb24872b91f2f56b841a0b601ba94b75782
Alexander Schneider <alexander@schneider.gg>
2023-07-21 13:33:05 +0200

    WIP: Put input box into qa-card
    Signed-off-by: Alexander Schneider <alexander@schneider.gg>

--- src/main/schnaq/interface/views/schnaq/qa_box.cljs
+++ src/main/schnaq/interface/views/schnaq/qa_box.cljs
@@ -1,7 +1,17 @@
 (ns schnaq.interface.views.schnaq.qa-box
-  (:require [com.fulcrologic.guardrails.core :refer [=> >defn-]]
+  (:require ["react-bootstrap/Button" :as Button]
+            ["react-bootstrap/Form" :as Form]
+            ["react-bootstrap/InputGroup" :as InputGroup]
+            [com.fulcrologic.guardrails.core :refer [=> >defn-]]
+            [oops.core :refer [oget]]
             [re-frame.core :as rf]
-            [schnaq.database.specs :as specs]))
+            [schnaq.database.specs :as specs]
+            [schnaq.interface.components.icons :refer [icon]]
+            [schnaq.interface.translations :refer [labels]]
+            [schnaq.interface.utils.toolbelt :as tools]))
+
+(def ^:private FormGroup (oget Form :Group))
+(def ^:private FormControl (oget Form :Control))
 
 (rf/reg-sub
  :qa-boxes
@@ -16,13 +26,25 @@
     (filter #(= (:db/id %) qa-box-id))
     first)))
 
+;; TODO i18n
+
 (>defn- qa-box-card
   "Show a qa box card, where users can ask questions of the presenter."
   [qa-box]
   [::specs/poll => :re-frame/component]
-  [:section.statement-card
+  [:section.statement-card.activation-card
    [:div.mx-4.my-2
     [:div.d-flex
      [:h6.pb-2.text-center.mx-auto (:qa-box/label qa-box)]
-     [:p "TODO dropdown menu"]]
-    [:h3 "Any content"]]])
+     [:p "TODO…"]]
+    (let [input-name (str "question-" (:db/id qa-box))]
+      [:> Form {:on-submit (fn [e]
+                             (.preventDefault e)
+                             (rf/dispatch [:TODO (oget e [:target :elements input-name :value])]))
+                :on-key-down (fn [e]
+                               (when (tools/ctrl-press? e "Enter")
+                                 (rf/dispatch [:TODO (oget e [:target :elements input-name :value])])))} ;;TODO
+       [:> FormGroup
+        [:> InputGroup
+         [:> FormControl {:name input-name :placeholder "Gib hier deine Frage ein"}]
+         [:> Button {:variant "primary" :type :submit} [icon :plane]]]]])]])
