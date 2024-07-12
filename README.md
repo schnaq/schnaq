f9eb7f39a7bcf50388b96885d6fae3ec2aca7bb5
Christian Meter <cmeter@googlemail.com>
2024-07-12 16:30:48 +0200

    Replace some s3 routes

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 <p align="center">
   <a href="https://app.schnaq.com">
     <picture>
-      <source media="(prefers-color-scheme: dark)" srcset="https://s3.schnaq.com/schnaq-common/logos/schnaq_white.webp">
-      <img src="https://s3.schnaq.com/schnaq-common/logos/schnaq.webp" height="80">
+      <source media="(prefers-color-scheme: dark)" srcset="https://snq-common.s3.nl-ams.scw.cloud/logos/schnaq_white.webp">
+      <img src="https://snq-common.s3.nl-ams.scw.cloud/logos/schnaq.webp" height="80">
     </picture>
     <div align="center">Education as interactive, as it's supposed to be!</div>
   </a>
--- src/main/schnaq/interface/components/videos.cljs
+++ src/main/schnaq/interface/components/videos.cljs
@@ -4,7 +4,7 @@
   "Returns a video path."
   [identifier]
   (identifier
-   {:register.point-right/webm "https://s3.schnaq.com/schnaq-schnaqqifanten/schnaqqi_point_right.webm"
-    :register.point-right/mp4 "https://s3.schnaq.com/schnaq-schnaqqifanten/schnaqqi_point_right.mp4"
-    :register.point-right-christmas/webm "https://s3.schnaq.com/schnaq-schnaqqifanten/events/schnaqqi_point_right_christmas.webm"
-    :register.point-right-christmas/mp4 "https://s3.schnaq.com/schnaq-schnaqqifanten/events/schnaqqi_point_right_christmas.mp4"}))
+   {:register.point-right/webm "https://snq-common.s3.nl-ams.scw.cloud/schnaqqifanten/schnaqqi_point_right.webm"
+    :register.point-right/mp4 "https://snq-common.s3.nl-ams.scw.cloud/schnaqqifanten/schnaqqi_point_right.mp4"
+    :register.point-right-christmas/webm "https://snq-common.s3.nl-ams.scw.cloud/schnaqqifanten/events/schnaqqi_point_right_christmas.webm"
+    :register.point-right-christmas/mp4 "https://snq-common.s3.nl-ams.scw.cloud/schnaqqifanten/events/schnaqqi_point_right_christmas.mp4"}))
